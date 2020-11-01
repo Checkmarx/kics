0d149248af5ab13e6fa4a65009448e104f59ca39
Adar Weidman <Adar.Weidman@checkmarx.com>
2020-11-01 13:08:47 +0200

    add query reme_logRetensionGraterThan90Days [why so many typos in query name??]

---
+++ assets/queries/terraform/azure/reme_logRetensionGraterThan90Days/test/negative.tf
@@ -0,0 +1,25 @@
+resource "azurerm_network_watcher_flow_log" "test1" {
+    network_watcher_name = azurerm_network_watcher.test.name
+    resource_group_name  = azurerm_resource_group.test.name
+    network_security_group_id = azurerm_network_security_group.test.id
+    storage_account_id        = azurerm_storage_account.test.id
+    enabled                   = true
+
+    retention_policy {
+    enabled = true
+    days    = 90
+    }
+}
+
+resource "azurerm_network_watcher_flow_log" "test2" {
+    network_watcher_name = azurerm_network_watcher.test.name
+    resource_group_name  = azurerm_resource_group.test.name
+    network_security_group_id = azurerm_network_security_group.test.id
+    storage_account_id        = azurerm_storage_account.test.id
+    enabled                   = true
+
+    retention_policy {
+    enabled = true
+    days    = 900
+    }
+}
---
+++ assets/queries/terraform/azure/reme_logRetensionGraterThan90Days/test/positive.tf
@@ -0,0 +1,46 @@
+resource "azurerm_network_watcher_flow_log" "test1" {
+    network_watcher_name = azurerm_network_watcher.test.name
+    resource_group_name  = azurerm_resource_group.test.name
+    network_security_group_id = azurerm_network_security_group.test.id
+    storage_account_id        = azurerm_storage_account.test.id
+    enabled                   = true
+
+    retention_policy {
+    enabled = true
+    days    = 89
+    }
+}
+
+resource "azurerm_network_watcher_flow_log" "test2" {
+    network_watcher_name = azurerm_network_watcher.test.name
+    resource_group_name  = azurerm_resource_group.test.name
+    network_security_group_id = azurerm_network_security_group.test.id
+    storage_account_id        = azurerm_storage_account.test.id
+    enabled                   = true
+
+    retention_policy {
+    enabled = true
+    days    = 3
+    }
+}
+
+resource "azurerm_network_watcher_flow_log" "test3" {
+    network_watcher_name = azurerm_network_watcher.test.name
+    resource_group_name  = azurerm_resource_group.test.name
+    network_security_group_id = azurerm_network_security_group.test.id
+    storage_account_id        = azurerm_storage_account.test.id
+    enabled                   = true
+}
+
+resource "azurerm_network_watcher_flow_log" "test4" {
+    network_watcher_name = azurerm_network_watcher.test.name
+    resource_group_name  = azurerm_resource_group.test.name
+    network_security_group_id = azurerm_network_security_group.test.id
+    storage_account_id        = azurerm_storage_account.test.id
+    enabled                   = true
+
+    retention_policy {
+    enabled = false
+    days    = 900
+    }
+}
