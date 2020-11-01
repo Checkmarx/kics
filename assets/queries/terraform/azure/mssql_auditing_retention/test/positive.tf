adc4b45cc8761919e4ed45e14fed511484312e0d
Adar Weidman <Adar.Weidman@checkmarx.com>
2020-11-01 15:27:44 +0200

    add query mssql_auditing_retention to Azure

---
+++ assets/queries/terraform/azure/mssql_auditing_retention/test/positive.tf
@@ -0,0 +1,69 @@
+resource "azurerm_mssql_database" "example1" {
+  name                = "myexamplesqldatabase"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = "West US"
+  server_name         = azurerm_mssql_server.example.name
+
+  extended_auditing_policy {
+    storage_endpoint                        = azurerm_storage_account.example.primary_blob_endpoint
+    storage_account_access_key              = azurerm_storage_account.example.primary_access_key
+    storage_account_access_key_is_secondary = true
+    retention_in_days                       = 6
+  }
+
+  tags = {
+    environment = "production"
+  }
+}
+
+resource "azurerm_mssql_database" "example2" {
+  name                = "myexamplesqldatabase"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = "West US"
+  server_name         = azurerm_mssql_server.example.name
+
+  extended_auditing_policy {
+    storage_endpoint                        = azurerm_storage_account.example.primary_blob_endpoint
+    storage_account_access_key              = azurerm_storage_account.example.primary_access_key
+    storage_account_access_key_is_secondary = true
+    retention_in_days                       = 90
+  }
+
+  tags = {
+    environment = "production"
+  }
+}
+
+resource "azurerm_mssql_database" "example3" {
+  name                = "myexamplesqldatabase"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = "West US"
+  server_name         = azurerm_mssql_server.example.name
+
+  extended_auditing_policy {
+    storage_endpoint                        = azurerm_storage_account.example.primary_blob_endpoint
+    storage_account_access_key              = azurerm_storage_account.example.primary_access_key
+    storage_account_access_key_is_secondary = true
+    retention_in_days                       = 0
+  }
+
+  tags = {
+    environment = "production"
+  }
+}
+
+resource "azurerm_mssql_server" "example5" {
+    name                         = "mssqlserver"
+    resource_group_name          = azurerm_resource_group.example.name
+    location                     = azurerm_resource_group.example.location
+    version                      = "12.0"
+    administrator_login          = "mradministrator"
+    administrator_login_password = "thisIsDog11"
+
+    extended_auditing_policy {
+      storage_endpoint            = azurerm_storage_account.example.primary_blob_endpoint
+      storage_account_access_key  = azurerm_storage_account.example.primary_access_key
+      storage_account_access_key_is_secondary = true
+      retention_in_days                       = 20
+    }
+}
