499aabdfee313960325be75d6303e9da41a14337
Adar Weidman <Adar.Weidman@checkmarx.com>
2020-11-02 11:29:18 +0200

    add query reme_check_key_expiration_is_set

---
+++ assets/queries/terraform/azure/reme_check_key_expiration_is_set/test/negative.tf
@@ -0,0 +1,16 @@
+resource "azurerm_key_vault_key" "generated" {
+    name         = "generated-certificate"
+    key_vault_id = azurerm_key_vault.example.id
+    key_type     = "RSA"
+    key_size     = 2048
+
+    key_opts = [
+    "decrypt",
+    "encrypt",
+    "sign",
+    "unwrapKey",
+    "verify",
+    "wrapKey",
+    ]
+  expiration_date = "2020-12-30T20:00:00Z"
+}
---
+++ assets/queries/terraform/azure/reme_check_key_expiration_is_set/test/positive.tf
@@ -0,0 +1,15 @@
+resource "azurerm_key_vault_key" "generated" {
+    name         = "generated-certificate"
+    key_vault_id = azurerm_key_vault.example.id
+    key_type     = "RSA"
+    key_size     = 2048
+
+    key_opts = [
+    "decrypt",
+    "encrypt",
+    "sign",
+    "unwrapKey",
+    "verify",
+    "wrapKey",
+    ]
+}
