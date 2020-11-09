e93c712b8d35db81db6d617e134dc5f8abe039f6
Adar Weidman <Adar.Weidman@checkmarx.com>
2020-11-09 11:00:46 +0200

    Add rdp_is_exposed_to_the_internet and ssh_is_exposed_to_the_internet to Azure

---
+++ assets/queries/terraform/azure/rdp_is_exposed_to_the_internet/query.rego
@@ -0,0 +1,59 @@
+package Cx
+
+
+isRelevantProtocol(protocol) = allow {
+    upper(protocol) != "UDP"
+    upper(protocol) != "ICMP"
+    allow = true
+}
+
+isRelevantPort(port) = allow {
+	regex.match("(^|\\s|,)3389(-|,|$|\\s)", port)
+    allow = true
+}
+else = allow {
+	ports = split(port, ",")
+    sublist = split(ports[var],"-")
+    to_number(trim(sublist[0]," ")) <= 3389
+    to_number(trim(sublist[1]," ")) >= 3389
+	allow = true
+}
+
+isRelevantAddressPrefix(prefix) = allow {
+	prefix == "*"
+    allow = true
+}
+else = allow {
+	prefix == "0.0.0.0"
+    allow = true
+}
+else = allow {
+	endswith(prefix, "/0")
+    allow = true
+}
+else = allow {
+	prefix == "internet"
+    allow = true
+}
+else = allow {
+	prefix == "any"
+    allow = true
+}
+
+
+CxPolicy [ result ] {
+    resource := input.document[i].resource.azurerm_network_security_rule[var0]
+    upper(resource.access) == "ALLOW"
+
+	  isRelevantProtocol(resource.protocol)
+    isRelevantPort(resource.destination_port_range)
+    isRelevantAddressPrefix(resource.source_address_prefix)
+    
+    result := {
+                "documentId": 		input.document[i].id,
+                "searchKey": 	    sprintf("azurerm_network_security_rule[%s].destination_port_range", [var0]),
+                "issueType":		"IncorrectValue",
+                "keyExpectedValue": sprintf("'azurerm_network_security_rule.%s.destination_port_range' cannot be 3389", [var0]),
+                "keyActualValue": 	sprintf("'azurerm_network_security_rule.%s.destination_port_range' might be 3389", [var0])
+              }
+}
---
+++ assets/queries/terraform/azure/rdp_is_exposed_to_the_internet/test/positive.tf
@@ -0,0 +1,139 @@
+resource "azurerm_network_security_rule" "example1" {
+     name                        = "example"
+     priority                    = 100
+     direction                   = "Inbound"
+     access                      = "Allow"
+     protocol                    = "TCP"
+     source_port_range           = "*"
+     destination_port_range      = "3389"
+     source_address_prefix       = "*"
+     destination_address_prefix  = "*"
+     resource_group_name         = azurerm_resource_group.example.name
+     network_security_group_name = azurerm_network_security_group.example.name
+}
+
+resource "azurerm_network_security_rule" "example2" {
+     name                        = "example"
+     priority                    = 100
+     direction                   = "Inbound"
+     access                      = "Allow"
+     protocol                    = "TCP"
+     source_port_range           = "*"
+     destination_port_range      = "3389-3390"
+     source_address_prefix       = "*"
+     destination_address_prefix  = "*"
+     resource_group_name         = azurerm_resource_group.example.name
+     network_security_group_name = azurerm_network_security_group.example.name
+}
+
+resource "azurerm_network_security_rule" "example3" {
+     name                        = "example"
+     priority                    = 100
+     direction                   = "Inbound"
+     access                      = "Allow"
+     protocol                    = "TCP"
+     source_port_range           = "*"
+     destination_port_range      = "3388-3389"
+     source_address_prefix       = "*"
+     destination_address_prefix  = "*"
+     resource_group_name         = azurerm_resource_group.example.name
+     network_security_group_name = azurerm_network_security_group.example.name
+}
+
+resource "azurerm_network_security_rule" "example4" {
+     name                        = "example"
+     priority                    = 100
+     direction                   = "Inbound"
+     access                      = "Allow"
+     protocol                    = "TCP"
+     source_port_range           = "*"
+     destination_port_range      = "3389"
+     source_address_prefix       = "0.0.0.0"
+     destination_address_prefix  = "*"
+     resource_group_name         = azurerm_resource_group.example.name
+     network_security_group_name = azurerm_network_security_group.example.name
+}
+
+resource "azurerm_network_security_rule" "example5" {
+     name                        = "example"
+     priority                    = 100
+     direction                   = "Inbound"
+     access                      = "Allow"
+     protocol                    = "TCP"
+     source_port_range           = "*"
+     destination_port_range      = "3389,3391"
+     source_address_prefix       = "34.15.11.3/0"
+     destination_address_prefix  = "*"
+     resource_group_name         = azurerm_resource_group.example.name
+     network_security_group_name = azurerm_network_security_group.example.name
+}
+
+resource "azurerm_network_security_rule" "example6" {
+     name                        = "example"
+     priority                    = 100
+     direction                   = "Inbound"
+     access                      = "Allow"
+     protocol                    = "TCP"
+     source_port_range           = "*"
+     destination_port_range      = "3389"
+     source_address_prefix       = "/0"
+     destination_address_prefix  = "*"
+     resource_group_name         = azurerm_resource_group.example.name
+     network_security_group_name = azurerm_network_security_group.example.name
+}
+
+resource "azurerm_network_security_rule" "example7" {
+     name                        = "example"
+     priority                    = 100
+     direction                   = "Inbound"
+     access                      = "Allow"
+     protocol                    = "TCP"
+     source_port_range           = "*"
+     destination_port_range      = "3388-3390, 23000"
+     source_address_prefix       = "internet"
+     destination_address_prefix  = "*"
+     resource_group_name         = azurerm_resource_group.example.name
+     network_security_group_name = azurerm_network_security_group.example.name
+}
+
+resource "azurerm_network_security_rule" "example8" {
+     name                        = "example"
+     priority                    = 100
+     direction                   = "Inbound"
+     access                      = "Allow"
+     protocol                    = "TCP"
+     source_port_range           = "*"
+     destination_port_range      = "3387, 3389 , 3391 "
+     source_address_prefix       = "any"
+     destination_address_prefix  = "*"
+     resource_group_name         = azurerm_resource_group.example.name
+     network_security_group_name = azurerm_network_security_group.example.name
+}
+
+resource "azurerm_network_security_rule" "example9" {
+     name                        = "example"
+     priority                    = 100
+     direction                   = "Inbound"
+     access                      = "Allow"
+     protocol                    = "*"
+     source_port_range           = "*"
+     destination_port_range      = "3388, 3389-3390,2250"
+     source_address_prefix       = "/0"
+     destination_address_prefix  = "*"
+     resource_group_name         = azurerm_resource_group.example.name
+     network_security_group_name = azurerm_network_security_group.example.name
+}
+
+resource "azurerm_network_security_rule" "example10" {
+     name                        = "example"
+     priority                    = 100
+     direction                   = "Inbound"
+     access                      = "Allow"
+     protocol                    = "*"
+     source_port_range           = "*"
+     destination_port_range      = "111-211, 2000-4430, 1-2 , 3"
+     source_address_prefix       = "internet"
+     destination_address_prefix  = "*"
+     resource_group_name         = azurerm_resource_group.example.name
+     network_security_group_name = azurerm_network_security_group.example.name
+}
