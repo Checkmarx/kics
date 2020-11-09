1302c37fa2b38fce78b8078bebd2c6ab760fd205
Adar Weidman <Adar.Weidman@checkmarx.com>
2020-11-09 14:26:57 +0200

    Update query ssl_connection_enabled

---
+++ assets/queries/terraform/azure/ssl_connection_is_enabled/test/negative.tf
@@ -0,0 +1,18 @@
+resource "azurerm_mysql_server" "example1" {
+  name                = "webflux-mysql-${var.environment}${random_integer.rnd_int.result}"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+
+  administrator_login          = "webflux-${var.environment}"
+  administrator_login_password = random_string.password.result
+
+  sku_name   = "B_Gen5_2"
+  storage_mb = 5120
+  version    = "5.7"
+
+  auto_grow_enabled                 = true
+  backup_retention_days             = 7
+  infrastructure_encryption_enabled = true
+  public_network_access_enabled     = true
+  ssl_enforcement_enabled           = true
+}
