3258a06de3b30917cb7134ccf79bc301e142f159
Ruben <ruben.silva@checkmarx.com>
2020-11-16 15:28:19 +0000

    Add Network Policy is Disabled query for Terraform

---
+++ assets/queries/terraform/gcp/network_policy_is_disabled/test/negative.tf
@@ -0,0 +1,20 @@
+#this code is a correct code for which the query should not find any result
+resource "google_container_cluster" "primary" {
+  name               = "marcellus-wallace"
+  location           = "us-central1-a"
+  initial_node_count = 3
+  network_policy {
+      enabled = true
+  }
+  addons_config {
+    network_policy_config {
+        disabled = false
+    }
+  }
+  networking_mode = "VPC_NATIVE"
+
+  timeouts {
+    create = "30m"
+    update = "40m"
+  }
+}
