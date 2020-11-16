5d6ef4f3b4ddb7ec3793a4b0ece5d32697805875
Ruben-Silva <74045324+Ruben-Silva@users.noreply.github.com>
2020-11-16 18:45:16 +0000

    Add ClusterLabels are Disabled query for Terraform (#153)

---
+++ assets/queries/terraform/gcp/cluster_labels_are_disabled/test/positive.tf
@@ -0,0 +1,11 @@
+#this is a problematic code where the query should report a result(s)
+resource "google_container_cluster" "primary" {
+  name               = "marcellus-wallace"
+  location           = "us-central1-a"
+  initial_node_count = 3
+
+  timeouts {
+    create = "30m"
+    update = "40m"
+  }
+}
