37221b1e1f7f31867ce643ca985654b6c28e86f1
pedro-mimoso <74053374+pedro-mimoso@users.noreply.github.com>
2020-11-16 18:42:42 +0000

    Added query aws_redshift_cluster_logging_disabled disabled (#149)
    * Added query redshift logging #117

---
+++ assets/queries/terraform/aws/aws_redshift_logging_disabled/test/positive.tf
@@ -0,0 +1,20 @@
+resource "aws_redshift_cluster" "default" {
+  cluster_identifier = "tf-redshift-cluster"
+  database_name      = "mydb"
+  master_username    = "foo"
+  master_password    = "Mustbe8characters"
+  node_type          = "dc1.large"
+  cluster_type       = "single-node"
+  logging {
+      enable = false
+  }
+}
+
+resource "aws_redshift_cluster" "default2" {
+  cluster_identifier = "tf-redshift-cluster"
+  database_name      = "mydb"
+  master_username    = "foo"
+  master_password    = "Mustbe8characters"
+  node_type          = "dc1.large"
+  cluster_type       = "single-node"
+}
