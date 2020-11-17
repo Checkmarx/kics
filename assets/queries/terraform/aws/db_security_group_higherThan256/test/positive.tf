dc68eab4d21241a9a4b01f2c444b343fbe670f5c
fabioGoncalvesCx <fabio.goncalves@checkmarx.com>
2020-11-17 18:39:21 +0000

    Feature/db instance iam database authentication enabled (#187)
    Added Terraform queries:
    * db instance iam database authentication enabled
    * db security group is public for Aws Terraform
    * aws db security group public scope
    * aws db security group higher than256
    * aws ebs encryption enable by default
    * aws_ebs_volume_snapshot encrypted

---
+++ assets/queries/terraform/aws/db_security_group_higherThan256/test/positive.tf
@@ -0,0 +1,7 @@
+resource "aws_db_security_group" "default" {
+  name = "rds_sg"
+
+  ingress {
+    cidr = "10.0.0.0/24"
+  }
+}
---
+++ assets/queries/terraform/aws/ebs_encryption_enable_by_default/test/negative.tf
@@ -0,0 +1,7 @@
+resource "aws_ebs_encryption_by_default" "example" {
+  enabled = true
+}
+
+resource "aws_ebs_encryption_by_default" "example2" {
+  
+}
