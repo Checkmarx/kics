00b5ed5d756ddd628b50bdcb4e890fa800af6eaf
joelcarvalhocheckmarx <73828702+joelcarvalhocheckmarx@users.noreply.github.com>
2020-11-17 13:03:27 +0000

    added API Gateway Name query for Terraform #121 (#123)

---
+++ assets/queries/terraform/aws/api_gateway_name/test/positive.tf
@@ -0,0 +1,9 @@
+resource "aws_api_gateway_stage" "example" {
+  depends_on = [aws_cloudwatch_log_group.example]
+
+  stage_name = "prod"
+}
+
+resource "aws_cloudwatch_log_group" "example2" {
+  name              = "Xpto"
+}
