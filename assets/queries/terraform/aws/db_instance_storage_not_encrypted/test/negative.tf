8f370dc944097850938cac371afd77bb1f833faa
fabioGoncalvesCx <fabio.goncalves@checkmarx.com>
2020-11-17 18:40:26 +0000

    Add DB Instance Storage Not Encrypted (#217)
    Closes #212

---
+++ assets/queries/terraform/aws/db_instance_storage_not_encrypted/test/negative.tf
@@ -0,0 +1,26 @@
+resource "aws_db_instance" "default" {
+  allocated_storage    = 20
+  storage_type         = "gp2"
+  engine               = "mysql"
+  engine_version       = "5.7"
+  instance_class       = "db.t2.micro"
+  name                 = "mydb"
+  username             = "foo"
+  password             = "foobarbaz"
+  parameter_group_name = "default.mysql5.7"
+  iam_database_authentication_enabled = true
+  storage_encrypted = true
+}
+resource "aws_db_instance" "default2" {
+  allocated_storage    = 20
+  storage_type         = "gp2"
+  engine               = "mysql"
+  engine_version       = "5.7"
+  instance_class       = "db.t2.micro"
+  name                 = "mydb"
+  username             = "foo"
+  password             = "foobarbaz"
+  parameter_group_name = "default.mysql5.7"
+  iam_database_authentication_enabled = false
+  ms_key_id = "${aws_kms_key.my_key.key_id}"
+}
