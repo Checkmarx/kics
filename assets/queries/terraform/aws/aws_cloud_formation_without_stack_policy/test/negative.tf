03523279491ebea640625fee2a9e9f80be1f0606
rafaela-soares <rafaela.soares@checkmarx.com>
2020-11-17 14:36:01 +0000

    requested changes done #138

--- assets/queries/terraform/aws/aws_cloud_formation_without_stack_policy/test/negative.tf
+++ assets/queries/terraform/aws/aws_cloud_formation_without_stack_policy/test/negative.tf
@@ -8,3 +8,16 @@ resource "aws_cloudformation_stack" "network" {
 
      policy_url = ""
 }
+
+
+
+resource "aws_cloudformation_stack" "network2" {
+
+     name = "networking-stack"
+
+     parameters = {
+     VPCCidr = "10.0.0.0/16"
+     }
+
+     policy_body = ""
+}
