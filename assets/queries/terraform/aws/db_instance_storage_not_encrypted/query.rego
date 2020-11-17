b25078cf7940d731ae265c68513926e410e50089
Fabio Gonçalves <fabio.goncalves@checkmarx.com>
2020-11-17 17:34:10 +0000

    Add db_instance_storage_not_encrypted query Issue #212

---
+++ assets/queries/terraform/aws/db_instance_storage_not_encrypted/query.rego
@@ -0,0 +1,15 @@
+package Cx
+
+CxPolicy [ result ] {
+	resource := input.document[i].resource.aws_db_instance[name]
+    not resource.storage_encrypted
+    not resource.ms_key_id 
+
+	result := {
+                "documentId": 		input.document[i].id,
+                "searchKey": 	    sprintf("aws_db_instance[%s].storage_encrypted", [name]),
+                "issueType":		"IncorrectValue",
+                "keyExpectedValue": "One of 'aws_db_instance.storage_encrypted'  is 'true'",
+                "keyActualValue": 	"One of 'aws_db_instance.storage_encrypted' is 'false'"
+              }
+}
---
+++ assets/queries/terraform/aws/db_instance_storage_not_encrypted/test/positive.tf
@@ -0,0 +1,25 @@
+resource "aws_db_instance" "default" {
+  allocated_storage    = 20
+  storage_type         = "gp2"
+  engine               = "mysql"
+  engine_version       = "5.7"
+  instance_class       = "db.t2.micro"
+  name                 = "mydb"
+  username             = "foo"
+  password             = "foobarbaz"
+  parameter_group_name = "default.mysql5.7"
+  iam_database_authentication_enabled = false
+  storage_encrypted = false
+}
+
+resource "aws_db_instance" "default2" {
+  allocated_storage    = 20
+  storage_type         = "gp2"
+  engine               = "mysql"
+  engine_version       = "5.7"
+  instance_class       = "db.t2.micro"
+  name                 = "mydb"
+  username             = "foo"
+  password             = "foobarbaz"
+  parameter_group_name = "default.mysql5.7"
+}
