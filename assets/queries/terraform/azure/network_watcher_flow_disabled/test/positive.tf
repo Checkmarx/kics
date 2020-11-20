d3dd663eedbbbbdbf28f53b14bf7feaa71cd61d7
Diogo Lemos <diogo.lemos@checkmarx.com>
2020-11-20 16:09:52 +0000

    Add Network Watcher Flow Disabled for Terraform

---
+++ assets/queries/terraform/azure/network_watcher_flow_disabled/test/positive.tf
@@ -0,0 +1,13 @@
+resource "azurerm_network_watcher_flow_log" "test" {
+  network_watcher_name = azurerm_network_watcher.test.name
+  resource_group_name  = azurerm_resource_group.test.name
+
+  network_security_group_id = azurerm_network_security_group.test.id
+  storage_account_id        = azurerm_storage_account.test.id
+  enabled                   = false
+
+  retention_policy {
+    enabled = true
+    days    = 7
+  }
+}
