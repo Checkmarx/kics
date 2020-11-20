ec5d70bd323203a7c364c96cb749ce91c051c44f
Ruben <ruben.silva@checkmarx.com>
2020-11-20 15:33:36 +0000

    Add SQLServerPredActiveDirectoryLogin query Tf#286

---
+++ assets/queries/terraform/azure/sql_server_predictable_active_directory_admin_account_name/test/negative.tf
@@ -0,0 +1,24 @@
+#this code is a correct code for which the query should not find any result
+data "azurerm_client_config" "current" {}
+
+resource "azurerm_resource_group" "example" {
+  name     = "acceptanceTestResourceGroup1"
+  location = "West US"
+}
+
+resource "azurerm_sql_server" "example" {
+  name                         = "mysqlserver"
+  resource_group_name          = azurerm_resource_group.example.name
+  location                     = azurerm_resource_group.example.location
+  version                      = "12.0"
+  administrator_login          = "4dm1n157r470r"
+  administrator_login_password = "4-v3ry-53cr37-p455w0rd"
+}
+
+resource "azurerm_sql_active_directory_administrator" "example" {
+  server_name         = azurerm_sql_server.example.name
+  resource_group_name = azurerm_resource_group.example.name
+  login               = "NotEasyToPredictAdmin"
+  tenant_id           = data.azurerm_client_config.current.tenant_id
+  object_id           = data.azurerm_client_config.current.object_id
+}
