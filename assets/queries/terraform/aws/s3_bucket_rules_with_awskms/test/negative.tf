ec59cb6a962e83f7fe006cdbde442137b548140a
Joel Carvalho <Joel.Carvalho@checkmarx.com>
2020-11-20 17:23:57 +0000

    Add S3 Bucket Rules W/Master Key Null query #302

---
+++ assets/queries/terraform/aws/s3_bucket_rules_with_awskms/test/negative.tf
@@ -0,0 +1,11 @@
+resource "aws_s3_bucket" "mybucket" {
+  bucket = "mybucket"
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm     = "AES256"
+      }
+    }
+  }
+}
