99f9e3189c64a526813deb2f73aee5aaf3d8c2fb
rafaela-soares <rafaela.soares@checkmarx.com>
2020-11-20 09:28:01 +0000

    requested changes

--- assets/queries/terraform/aws/ami_shared_to_multiple_accounts/test/negative.tf
+++ assets/queries/terraform/aws/ami_shared_to_multiple_accounts/test/negative.tf
@@ -2,3 +2,9 @@ resource "aws_ami_launch_permission" "example" {
   image_id   = "ami-12345678"
   account_id = "123456789012"
 }
+
+
+resource "aws_ami_launch_permission" "example" {
+  image_id   = "ami-12345680"
+  account_id = "12345672"
+}
--- assets/queries/terraform/aws/ami_shared_to_multiple_accounts/test/positive.tf
+++ assets/queries/terraform/aws/ami_shared_to_multiple_accounts/test/positive.tf
@@ -8,7 +8,7 @@ resource "aws_ami_launch_permission" "example" {
 
 resource "aws_ami_launch_permission" "example2" {
 
-  image_id   = "ami-1967678"
+  image_id   = "ami-1235678"
   account_id = "123456789012"
 
 }
