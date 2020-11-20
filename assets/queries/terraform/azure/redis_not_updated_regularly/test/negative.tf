c30fea6967bf5b1a07964dac192e29706c345f69
Jorge Cruz <=>
2020-11-20 18:29:08 +0000

    Add redis_not_updated_regularly query for Terraform #314

---
+++ assets/queries/terraform/azure/redis_not_updated_regularly/test/negative.tf
@@ -0,0 +1,24 @@
+resource "azurerm_redis_cache" "example_redis" {
+    name                = "timeout-redis"
+    location            = "West Europe"
+    resource_group_name = azurerm_resource_group.example_rg.name
+    subnet_id           = azurerm_subnet.example_redis_snet.id
+
+    family              = "P"
+    capacity            = 1
+    sku_name            = "Premium"
+    shard_count         = 1
+
+    enable_non_ssl_port = false
+    minimum_tls_version = "1.2"
+
+    redis_configuration {
+        enable_authentication   = true
+        maxmemory_policy        = "volatile-lru"
+    }
+
+    patch_schedule {
+        day_of_week     = "Thursday"
+        start_hour_utc  = 7
+    }
+}
