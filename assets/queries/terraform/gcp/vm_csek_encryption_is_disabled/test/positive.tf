66738941c91a1ca0fdd0714db27fa212285b810a
Ruben <ruben.silva@checkmarx.com>
2020-11-20 11:35:03 +0000

    Add VM CSEK Encryption Disabled query for Tf #254

---
+++ assets/queries/terraform/gcp/vm_csek_encryption_is_disabled/test/positive.tf
@@ -0,0 +1,40 @@
+#this is a problematic code where the query should report a result(s)
+resource "google_compute_disk" "default1" {
+  name  = "test-disk"
+  type  = "pd-ssd"
+  zone  = "us-central1-a"
+  image = "debian-9-stretch-v20200805"
+  labels = {
+    environment = "dev"
+  }
+  physical_block_size_bytes = 4096
+}
+
+resource "google_compute_disk" "default2" {
+  name  = "test-disk"
+  type  = "pd-ssd"
+  zone  = "us-central1-a"
+  image = "debian-9-stretch-v20200805"
+  labels = {
+    environment = "dev"
+  }
+  physical_block_size_bytes = 4096
+
+  disk_encryption_key {
+  }
+}
+
+resource "google_compute_disk" "default3" {
+  name  = "test-disk"
+  type  = "pd-ssd"
+  zone  = "us-central1-a"
+  image = "debian-9-stretch-v20200805"
+  labels = {
+    environment = "dev"
+  }
+  physical_block_size_bytes = 4096
+
+  disk_encryption_key {
+      sha256 = ""
+  }
+}
