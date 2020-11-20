b2c22c4f3cc7b38ec672e0db3db22f0cd18a3fbc
Diogo Lemos <diogo.lemos@checkmarx.com>
2020-11-20 14:34:46 +0000

    Add SQLServer with Public Access for Terraform

---
+++ assets/queries/terraform/azure/sqlserver_with_public_access/test/negative.tf
@@ -0,0 +1,7 @@
+resource "azurerm_sql_firewall_rule" "example" {
+  name                = "FirewallRule1"
+  resource_group_name = azurerm_resource_group.example.name
+  server_name         = azurerm_sql_server.example.name
+  start_ip_address    = "10.0.17.62"
+  end_ip_address      = "10.0.17.62"
+}
