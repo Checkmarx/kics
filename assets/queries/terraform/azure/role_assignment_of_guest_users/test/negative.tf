76f98ae385cb5a1e36276ad3e95ee786660361ac
Jorge Cruz <=>
2020-11-20 12:16:40 +0000

    Add role_assignment_of_guest_users query for Terraform #262

---
+++ assets/queries/terraform/azure/role_assignment_of_guest_users/test/negative.tf
@@ -0,0 +1,5 @@
+resource "azurerm_role_assignment" "example" {
+  scope                = data.azurerm_subscription.primary.id
+  role_definition_name = "Reader"
+  principal_id         = data.azurerm_client_config.example.object_id
+}
