28d548ffef5c1b0bf3be894c94a6f70c77030638
rafaela-soares <rafaela.soares@checkmarx.com>
2020-11-24 14:34:56 +0000

    Add no_password_policy_enabled query Closes #347

---
+++ assets/queries/terraform/aws/no_password_policy_enabled/test/negative.tf
@@ -0,0 +1,8 @@
+resource "aws_iam_user_login_profile" "example8" {
+  user    = aws_iam_user.example.name
+  pgp_key = "keybase:some_person_that_exists"
+
+  password_reset_required = true
+
+  password_length = 15
+}
