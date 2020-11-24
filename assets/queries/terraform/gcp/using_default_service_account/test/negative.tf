4d4d94bc71942cfadba4a13d89aa8d8f708f5dd2
Ruben <ruben.silva@checkmarx.com>
2020-11-24 15:16:40 +0000

    Add Using Default Service Account query for Tf#310

---
+++ assets/queries/terraform/gcp/using_default_service_account/test/negative.tf
@@ -0,0 +1,32 @@
+#this code is a correct code for which the query should not find any result
+resource "google_compute_instance" "default" {
+  name         = "test"
+  machine_type = "e2-medium"
+  zone         = "us-central1-a"
+
+  tags = ["foo", "bar"]
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+
+  // Local SSD disk
+  scratch_disk {
+    interface = "SCSI"
+  }
+
+  network_interface {
+    network = "default"
+
+    access_config {
+      // Ephemeral IP
+    }
+  }
+
+  service_account {
+    email = "email@email.com"
+    scopes = ["userinfo-email", "compute-ro", "storage-ro"]
+  }
+}
