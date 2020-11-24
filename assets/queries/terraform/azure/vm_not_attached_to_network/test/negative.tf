d27c284ca4d64fb723c29fe2542a3732a5514a05
Jorge Cruz <jorge.cruz@checkmarx.com>
2020-11-24 17:29:09 +0000

    Add vm_not_attached_to_network query for Terraform #401 (#404)

---
+++ assets/queries/terraform/azure/vm_not_attached_to_network/test/negative.tf
@@ -0,0 +1,23 @@
+resource "azurerm_network_interface" "main" {
+  name                = "${var.prefix}-nic"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+
+  ip_configuration {
+    name                          = "testconfiguration1"
+    subnet_id                     = azurerm_subnet.internal.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_virtual_machine" "main" {
+  name                  = "${var.prefix}-vm"
+  location              = azurerm_resource_group.main.location
+  resource_group_name   = azurerm_resource_group.main.name
+  network_interface_ids = [azurerm_network_interface.main.id]
+  vm_size               = "Standard_DS1_v2"
+
+  os_profile_linux_config {
+    disable_password_authentication = false
+  }
+}
