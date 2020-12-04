68599b06678812b0efe62635bdc852e0c23d2f7b
Nuno Araújo <74066205+NunoAraujoCX@users.noreply.github.com>
2020-12-04 11:09:57 +0000

    Add log_duration_is_not_set query Closes #578 (#579)

---
+++ assets/queries/terraform/azure/log_duration_is_not_set/test/positive.tf
@@ -0,0 +1,21 @@
+#this is a problematic code where the query should report a result(s)
+resource "azurerm_postgresql_configuration" "example1" {
+    name                = "log_duration"
+    resource_group_name = "example1_resource_group_name"
+    server_name         = "example1_server_name"
+    value               = "off"
+}
+
+resource "azurerm_postgresql_configuration" "example2" {
+    name                = "log_duration"
+    resource_group_name = "example2_resource_group_name"
+    server_name         = "example2_server_name"
+    value               = "Off"
+}
+
+resource "azurerm_postgresql_configuration" "example3" {
+    name                = "log_duration"
+    resource_group_name = "example3_resource_group_name"
+    server_name         = "example3_server_name"
+    value               = "OFF"
+}
