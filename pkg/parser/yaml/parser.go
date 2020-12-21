b322607aed0558baadff769cd43d59485f52ae57
João Reigota <joao.Reigota@checkmarx.com>
2020-12-21 10:33:22 +0000

    Linted yaml ansible parser

--- pkg/parser/yaml/parser.go
+++ pkg/parser/yaml/parser.go
@@ -34,7 +34,6 @@ func (p *Parser) Parse(_ string, fileContent []byte) ([]model.Document, error) {
 		if err != nil {
 			return nil, errors.Wrap(err, "Failed to Parse Dockerfile")
 		}
-
 	}
 
 	return documents, nil
@@ -56,10 +55,7 @@ func playbookParser(fileContent []byte) ([]model.Document, error) {
 	var documents []model.Document
 	for dec.Decode(&arr) == nil {
 		if doc != nil {
-			for _, key := range arr {
-				playBooks.Tasks = append(playBooks.Tasks, key)
-			}
-
+			playBooks.Tasks = append(playBooks.Tasks, arr...)
 			j, err := json.Marshal(playBooks)
 			if err != nil {
 				return nil, errors.Wrap(err, "Failed to Marshal Dockerfile")
