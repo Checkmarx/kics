b1dda601f5d4fdd897ea30cb1f05edec8f7a3732
AdarWeidman <adar.weidman@gmail.com>
2020-12-24 15:47:00 +0200

    Add the new query - ports in private network terraform azure

---
+++ assets/queries/terraform/azure/sensitive_port_is_exposed_to_wide_private_network/test/positive.tf
@@ -0,0 +1,139 @@
+resource "azurerm_network_security_rule" "example1" {
+     name                        = "example"
+     priority                    = 100
+     direction                   = "Inbound"
+     access                      = "Allow"
+     protocol                    = "UDP"
+     source_port_range           = "*"
+     destination_port_range      = "61621"
+     source_address_prefix       = "172.16.0.0/12"
+     destination_address_prefix  = "*"
+     resource_group_name         = azurerm_resource_group.example.name
+     network_security_group_name = azurerm_network_security_group.example.name
+}
+
+resource "azurerm_network_security_rule" "example2" {
+     name                        = "example"
+     priority                    = 100
+     direction                   = "Inbound"
+     access                      = "Allow"
+     protocol                    = "TCP"
+     source_port_range           = "*"
+     destination_port_range      = "23-34"
+     source_address_prefix       = "172.16.0.0/12"
+     destination_address_prefix  = "*"
+     resource_group_name         = azurerm_resource_group.example.name
+     network_security_group_name = azurerm_network_security_group.example.name
+}
+
+resource "azurerm_network_security_rule" "example3" {
+     name                        = "example"
+     priority                      = 100
+     direction                   = "Inbound"
+     access                      = "Allow"
+     protocol                    = "*"
+     source_port_range           = "*"
+     destination_port_range      = "21-23"
+     source_address_prefix       = "172.16.0.0/12"
+     destination_address_prefix  = "*"
+     resource_group_name         = azurerm_resource_group.example.name
+     network_security_group_name = azurerm_network_security_group.example.name
+}
+
+resource "azurerm_network_security_rule" "example4" {
+     name                        = "example"
+     priority                    = 100
+     direction                   = "Inbound"
+     access                      = "Allow"
+     protocol                    = "*"
+     source_port_range           = "*"
+     destination_port_range      = "23"
+     source_address_prefix       = "192.168.0.0/16"
+     destination_address_prefix  = "*"
+     resource_group_name         = azurerm_resource_group.example.name
+     network_security_group_name = azurerm_network_security_group.example.name
+}
+
+resource "azurerm_network_security_rule" "example5" {
+     name                        = "example"
+     priority                    = 100
+     direction                   = "Inbound"
+     access                      = "Allow"
+     protocol                    = "UDP"
+     source_port_range           = "*"
+     destination_port_range      = "23,245"
+     source_address_prefix       = "192.168.0.0/16"
+     destination_address_prefix  = "*"
+     resource_group_name         = azurerm_resource_group.example.name
+     network_security_group_name = azurerm_network_security_group.example.name
+}
+
+resource "azurerm_network_security_rule" "example6" {
+     name                        = "example"
+     priority                    = 100
+     direction                   = "Inbound"
+     access                      = "Allow"
+     protocol                    = "TCP"
+     source_port_range           = "*"
+     destination_port_range      = "23"
+     source_address_prefix       = "192.168.0.0/16"
+     destination_address_prefix  = "*"
+     resource_group_name         = azurerm_resource_group.example.name
+     network_security_group_name = azurerm_network_security_group.example.name
+}
+
+resource "azurerm_network_security_rule" "example7" {
+     name                        = "example"
+     priority                    = 100
+     direction                   = "Inbound"
+     access                      = "Allow"
+     protocol                    = "UDP"
+     source_port_range           = "*"
+     destination_port_range      = "22-64, 94"
+     source_address_prefix       = "10.0.0.0/8"
+     destination_address_prefix  = "*"
+     resource_group_name         = azurerm_resource_group.example.name
+     network_security_group_name = azurerm_network_security_group.example.name
+}
+
+resource "azurerm_network_security_rule" "example8" {
+     name                        = "example"
+     priority                    = 100
+     direction                   = "Inbound"
+     access                      = "Allow"
+     protocol                    = "TCP"
+     source_port_range           = "*"
+     destination_port_range      = "14, 23, 48"
+     source_address_prefix       = "10.0.0.0/8"
+     destination_address_prefix  = "*"
+     resource_group_name         = azurerm_resource_group.example.name
+     network_security_group_name = azurerm_network_security_group.example.name
+}
+
+resource "azurerm_network_security_rule" "example9" {
+     name                        = "example"
+     priority                    = 100
+     direction                   = "Inbound"
+     access                      = "Allow"
+     protocol                    = "*"
+     source_port_range           = "*"
+     destination_port_range      = "12, 23-24,46"
+     source_address_prefix       = "10.0.0.0/8"
+     destination_address_prefix  = "*"
+     resource_group_name         = azurerm_resource_group.example.name
+     network_security_group_name = azurerm_network_security_group.example.name
+}
+
+resource "azurerm_network_security_rule" "example10" {
+     name                        = "example"
+     priority                    = 100
+     direction                   = "Inbound"
+     access                      = "Allow"
+     protocol                    = "*"
+     source_port_range           = "*"
+     destination_port_range      = "46-146, 18-36, 1-2, 3"
+     source_address_prefix       = "10.0.0.0/8"
+     destination_address_prefix  = "*"
+     resource_group_name         = azurerm_resource_group.example.name
+     network_security_group_name = azurerm_network_security_group.example.name
+}
