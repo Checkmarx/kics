ca056e659ea019ca3ca301ba4cdfa614b57712f2
Felipe Avelar <felipe.avelar@checkmarx.com>
2021-01-06 15:12:02 +0000

    Add msk_cluster_logging_enabled query for terraform

---
+++ assets/queries/terraform/aws/msk_cluster_logging_enabled/test/positive.tf
@@ -0,0 +1,23 @@
+resource "aws_msk_cluster" "example1" {  
+  logging_info {
+    broker_logs {
+      cloudwatch_logs {
+        enabled   = false
+        log_group = aws_cloudwatch_log_group.test.name
+      }
+      firehose {
+        delivery_stream = aws_kinesis_firehose_delivery_stream.test_stream.name
+      }
+      s3 {
+        enabled = true
+        bucket  = aws_s3_bucket.bucket.id
+        prefix  = "logs/msk-"
+      }
+    }
+  }
+}
+
+resource "aws_msk_cluster" "example2" {  
+  logging_info {
+  }
+}
