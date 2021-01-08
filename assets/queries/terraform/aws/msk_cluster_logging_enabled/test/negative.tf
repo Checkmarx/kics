2b24341c8de3b57694f7547867224a9327c56973
felipe-avelar <felipe.avelar@checkmarx.com>
2021-01-08 17:01:19 +0000

    Add msk_cluster_logging_enabled query for terraform (#1744) (#1764)

---
+++ assets/queries/terraform/aws/msk_cluster_logging_enabled/test/negative.tf
@@ -0,0 +1,10 @@
+resource "aws_msk_cluster" "example" {  
+  logging_info {
+    broker_logs {
+      cloudwatch_logs {
+        enabled   = true
+        log_group = aws_cloudwatch_log_group.test.name
+      }
+    }
+  }
+}
