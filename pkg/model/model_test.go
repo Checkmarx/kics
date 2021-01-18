5a869884b208fc02256d1165ac99c8045584dca5
João Reigota <joao.reigota@checkmarx.com>
2021-01-18 16:47:38 +0000

    Increased model.go coverage to 100%

--- pkg/model/model_test.go
+++ pkg/model/model_test.go
@@ -24,3 +24,51 @@ func TestExtensions_MatchedFilesRegex(t *testing.T) {
 	}
 	require.Equal(t, "(.*)(\\.tf|\\.txt)$", e.MatchedFilesRegex())
 }
+
+func TestInclude(t *testing.T) {
+	e := Extensions{
+		".txt": struct{}{},
+		".tf":  struct{}{},
+	}
+	require.Equal(t, true, e.Include(".txt"))
+}
+
+func TestFileMetadatas(t *testing.T) {
+	m := FileMetadatas{
+		{
+			ID:           "id",
+			ScanID:       "scan_id",
+			OriginalData: "orig_data",
+			FileName:     "file_name",
+			Document: Document{
+				"id": "",
+			},
+		},
+	}
+
+	mEmptyDocuments := FileMetadatas{
+		{
+			ID:           "id",
+			ScanID:       "scan_id",
+			OriginalData: "orig_data",
+			FileName:     "file_name",
+			Document:     nil,
+		},
+	}
+
+	t.Run("to_map", func(t *testing.T) {
+		result := m.ToMap()
+		require.Len(t, result, 1)
+		require.Equal(t, m[0], result["id"])
+	})
+
+	t.Run("combine", func(t *testing.T) {
+		result := m.Combine()
+		require.Equal(t, Documents{Documents: []Document{{"file": "file_name", "id": "id"}}}, result)
+	})
+
+	t.Run("combine_empty_documents", func(t *testing.T) {
+		result := mEmptyDocuments.Combine()
+		require.Equal(t, Documents{Documents: []Document{}}, result)
+	})
+}
