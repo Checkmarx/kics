c976e2f5e232c1a1a45a81f140b5f6e05d5f7a1c
João Reigota <joao.reigota@checkmarx.com>
2021-01-21 10:12:34 +0000

    Increased inspector.go coverage to 2.4%

--- pkg/engine/inspector_test.go
+++ pkg/engine/inspector_test.go
@@ -1,1 +1,97 @@
 package engine
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/Checkmarx/kics/internal/tracker"
+	"github.com/open-policy-agent/opa/cover"
+)
+
+func TestInspector_EnableCoverageReport(t *testing.T) {
+	type fields struct {
+		queries              []*preparedQuery
+		vb                   VulnerabilityBuilder
+		tracker              Tracker
+		enableCoverageReport bool
+		coverageReport       cover.Report
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		want   bool
+	}{
+		{
+			name: "enable_coverage_report_1",
+			fields: fields{
+				queries:              []*preparedQuery{},
+				vb:                   DefaultVulnerabilityBuilder,
+				tracker:              &tracker.CITracker{},
+				enableCoverageReport: false,
+				coverageReport:       cover.Report{},
+			},
+			want: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			c := &Inspector{
+				queries:              tt.fields.queries,
+				vb:                   tt.fields.vb,
+				tracker:              tt.fields.tracker,
+				enableCoverageReport: tt.fields.enableCoverageReport,
+				coverageReport:       tt.fields.coverageReport,
+			}
+			c.EnableCoverageReport()
+			if !reflect.DeepEqual(c.enableCoverageReport, tt.want) {
+				t.Errorf("Inspector.enableCoverageReport() = %v, want %v", c.enableCoverageReport, tt.want)
+			}
+		})
+	}
+}
+
+func TestInspector_GetCoverageReport(t *testing.T) {
+	coverageReports := cover.Report{
+		Coverage: 75.5,
+		Files:    map[string]*cover.FileReport{},
+	}
+
+	type fields struct {
+		queries              []*preparedQuery
+		vb                   VulnerabilityBuilder
+		tracker              Tracker
+		enableCoverageReport bool
+		coverageReport       cover.Report
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		want   cover.Report
+	}{
+		{
+			name: "get_coverage_report_1",
+			fields: fields{
+				queries:              []*preparedQuery{},
+				vb:                   DefaultVulnerabilityBuilder,
+				tracker:              &tracker.CITracker{},
+				enableCoverageReport: true,
+				coverageReport:       coverageReports,
+			},
+			want: coverageReports,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			c := &Inspector{
+				queries:              tt.fields.queries,
+				vb:                   tt.fields.vb,
+				tracker:              tt.fields.tracker,
+				enableCoverageReport: tt.fields.enableCoverageReport,
+				coverageReport:       tt.fields.coverageReport,
+			}
+			if got := c.GetCoverageReport(); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Inspector.GetCoverageReport() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
