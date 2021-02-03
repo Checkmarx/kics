a38996b821b5efcbc585d9c57aefca4871019e16
Rogério Peixoto <rogerio.peixoto@checkmarx.com>
2021-02-03 16:02:54 +0000

    adressing codacy issue in shell script
    Signed-off-by: Rogério Peixoto <rogerio.peixoto@checkmarx.com>

--- .github/validate-dockerfile-syntax.sh
+++ .github/validate-dockerfile-syntax.sh
@@ -5,5 +5,5 @@ MOUNT_ROOT=${MOUNT_ROOT:-'/project'}
 echo assets/queries/dockerfile/**/*.dockerfile
 for filepath in $(find assets/queries/dockerfile -type f -name '*.dockerfile'); do
   echo "Validating ${filepath}"
-  docker run -v $(pwd):${MOUNT_ROOT} hadolint/hadolint hadolint -c "${MOUNT_ROOT}/.github/hadolint.yml" "${MOUNT_ROOT}/$filepath";
+  docker run -v $(pwd):"${MOUNT_ROOT}" hadolint/hadolint hadolint -c "${MOUNT_ROOT}/.github/hadolint.yml" "${MOUNT_ROOT}/$filepath";
 done
