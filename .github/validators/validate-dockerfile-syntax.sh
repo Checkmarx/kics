e91d0b7ef66bcfff4cefe37ffef8fefdec64f29b
Rogério Peixoto <rogerio.peixoto@checkmarx.com>
2021-02-04 12:57:15 +0000

    replacing bash with python script for validator
    Signed-off-by: Rogério Peixoto <rogerio.peixoto@checkmarx.com>

--- .github/validators/validate-dockerfile-syntax.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -eo pipefail;
-HADOLINT_PATH=${HADOLINT_PATH:-'.bin/hadolint'}
-
-for filepath in $(find assets/queries/dockerfile -type f -name '*.dockerfile'); do
-  echo "Validating ${filepath}"
-  command "${HADOLINT_PATH}" -c ".github/hadolint.yml" "${filepath}";
-done
