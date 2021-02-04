33b4083270ca7ce4a22f689fb4ff4796676febe4
Rogério Peixoto <rogerio.peixoto@checkmarx.com>
2021-02-04 10:00:09 +0000

    adding python script for validation
    Signed-off-by: Rogério Peixoto <rogerio.peixoto@checkmarx.com>

--- .github/validate-k8s-syntax.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -eo pipefail;
-KUBEVAL_PATH=${KUBEVAL_PATH:-'.bin/kubeval'}
-
-for filepath in $(find assets/queries/k8s -type f -name '*.yaml' -or -name '*.yml' $(printf "! -path %s " $(cat .github/validate-k8s-skip-list))); do
-  echo "Validating ${filepath}"
-  #kubectl apply --dry-run=client --validate=true -f "${filepath}"
-  command "${KUBEVAL_PATH}" "${filepath}"
-done
---
+++ .github/validators/validate-syntax.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+import glob
+import subprocess
+from subprocess import CalledProcessError
+import os
+
+LINTER_PATH = os.getenv('LINTER_PATH')
+SKIP_LIST = os.getenv('SKIP_LIST_PATH')
+FILES_GLOB = os.getenv('FILES_GLOB')
+
+##############################
+#  get files and skip list   #
+##############################
+all_files = glob.glob(FILES_GLOB)
+ignore_list = []
+
+with open(SKIP_LIST, 'r') as reader:
+  ignore_list = [line.rstrip() for line in reader]
+
+files = [file for file in all_files if file not in ignore_list]
+error_files = []
+
+##############################
+#         run linter         #
+##############################
+for file in files:
+  try:
+    result = subprocess.check_output([LINTER_PATH, file]).decode('utf-8').rstrip()
+  except CalledProcessError as e:
+    error_files.append(e)
+  for line in result.split('\n'):
+    print(f"{line}")
+
+################################
+# show errors and exit code 1  #
+################################
+if len(error_files) > 0:
+  print("\n--- errors ---")
+  for error in error_files:
+    print(error)
+    error_result = error.output.decode('utf-8').rstrip()
+    for line in error_result.split('\n'):
+      print(line)
+  exit(1)
