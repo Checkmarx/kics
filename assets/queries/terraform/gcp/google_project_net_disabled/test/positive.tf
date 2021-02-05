e7ac4e97adcfbc690dcc7bf9139aeca8e53e0262
Nuno Araújo <74066205+NunoAraujoCX@users.noreply.github.com>
2021-02-05 15:24:39 +0000

    delete duplicated query google_project_net_disabled #1925 (#1926)

--- assets/queries/terraform/gcp/google_project_net_disabled/test/positive.tf
+++ assets/queries/terraform/gcp/google_project_net_disabled/test/positive.tf
@@ -2,7 +2,7 @@ resource "google_project" "my_project" {
   name       = "My Project"
   project_id = "your-project-id"
   org_id     = "1234567"
-  auto_create_network = false
+  auto_create_network = true
 }
 
 resource "google_project" "my_project2" {
