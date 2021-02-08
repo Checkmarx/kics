c55748597946507839e7c7f8a20c931d021b6dc2
Nuno Araujo <nuno.araujo@checkmarx.com>
2021-02-08 10:14:57 +0000

    Update logging_disabled_for_key_vault query Closes #1984

--- assets/queries/terraform/azure/logging_disabled_for_key_vault/test/positive.tf
+++ assets/queries/terraform/azure/logging_disabled_for_key_vault/test/positive.tf
@@ -1,13 +1,8 @@
-// Example 1
-
-resource "azurerm_key_vault" "example" {
-  name                        = "example-vault"
+resource "azurerm_key_vault" "example1" {
+  name                        = "example1-vault"
   location                    = "West US"
   resource_group_name         = "example-resources"
 }
-
-// Example 2
-
 resource "azurerm_key_vault" "example2" {
   name                        = "example2-vault"
   location                    = "West US"
@@ -18,9 +13,6 @@ data "azurerm_key_vault" "example2" {
   name                = "example2-vault"
   resource_group_name = "example-resources"
 }
-
-// Example 3
-
 resource "azurerm_key_vault" "example3" {
   name                        = "example3-vault"
   location                    = "West US"
@@ -37,8 +29,6 @@ resource "azurerm_monitor_diagnostic_setting" "example3" {
   target_resource_id = data.azurerm_key_vault.example3.id
   storage_account_id = data.azurerm_storage_account.example.id
 }
-
-// Example 4
 
 resource "azurerm_key_vault" "example4" {
   name                        = "example4-vault"
