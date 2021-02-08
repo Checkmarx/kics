10c8fb79b471e850734dda81531361ba39bcdfca
Adrien Frediani <frediani.adrien@gmail.com>
2021-02-08 13:48:15 +0100

    fix: terraform/aws kinesis firehose ss3 rule issue (#1879)
    When an encrypted Kinesis Data Stream is used as datasource to a Kinesis Firehose, then it can't and must not need to have sse enabled
    
    cf AWS details: https://docs.aws.amazon.com/firehose/latest/dev/encryption.html
    
    cf terraform details:
    https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/kinesis_firehose_delivery_stream
    
    Co-authored-by: Adrien Frediani <frediani.adrien@fairway.digital>

--- assets/queries/terraform/aws/kinesis_sse_not_configured/test/positive.tf
+++ assets/queries/terraform/aws/kinesis_sse_not_configured/test/positive.tf
@@ -4,64 +4,43 @@ resource "aws_kinesis_firehose_delivery_stream" "example" {
 
   kinesis_source_configuration {
     kinesis_stream_arn = "${aws_kinesis_stream.cloudwatch-logs.arn}"
-    role_arn = "${aws_iam_role.firehose_role.arn}"
-  }
-
-  server_side_encryption {
-
-    enabled = true
-    key_type = "AWS_OWNED_CMK"
+    role_arn           = "${aws_iam_role.firehose_role.arn}"
   }
 }
-
-
-
 
 
 resource "aws_kinesis_firehose_delivery_stream" "example2" {
   name        = "${aws_s3_bucket.logs.bucket}-firehose"
   destination = "extended_s3"
-
 }
-
 
 
 resource "aws_kinesis_firehose_delivery_stream" "example3" {
   name        = "${aws_s3_bucket.logs.bucket}-firehose"
   destination = "extended_s3"
 
-
   server_side_encryption {
-
     enabled = false
   }
 }
-
-
 
 
 resource "aws_kinesis_firehose_delivery_stream" "example4" {
   name        = "${aws_s3_bucket.logs.bucket}-firehose"
   destination = "extended_s3"
 
-
   server_side_encryption {
-
-    enabled = true
+    enabled  = true
     key_type = "AWS_OWN"
   }
 }
-
-
 
 resource "aws_kinesis_firehose_delivery_stream" "example5" {
   name        = "${aws_s3_bucket.logs.bucket}-firehose"
   destination = "extended_s3"
 
-
   server_side_encryption {
-
-    enabled = true
+    enabled  = true
     key_type = "CUSTOMER_MANAGED_CMK"
   }
 }
