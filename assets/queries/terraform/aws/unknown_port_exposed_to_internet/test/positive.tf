797bc3ab1e52e761e4f91d3185809b302850d36b
Nuno Araujo <nuno.araujo@checkmarx.com>
2021-02-08 16:57:01 +0000

    Update unknown_port_exposed_to_internet Closes #2017

--- assets/queries/terraform/aws/unknown_port_exposed_to_internet/test/positive.tf
+++ assets/queries/terraform/aws/unknown_port_exposed_to_internet/test/positive.tf
@@ -37,11 +37,11 @@ resource "aws_security_group" "allow_tls2" {
     cidr_blocks = ["192.168.0.0/24", "0.0.0.0/0"]
   }
 
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["192.168.0.0/24", "0.0.0.0/0"]
   }
 
   tags = {
