dc731a8ac049092f202bde942c2151abc6b914c5
Felipe Avelar <felipe.avelar@checkmarx.com>
2021-02-24 14:49:16 +0000

    Minor correction
    Signed-off-by: Felipe Avelar <felipe.avelar@checkmarx.com>

--- .github/generators/docs_generator.py
+++ .github/generators/docs_generator.py
@@ -9,16 +9,16 @@ template_dict = {}
 platforms = []
 
 arg_parser = argparse.ArgumentParser(description='Create query docs')
-arg_parser.add_argument('-p', nargs=1, type=Path, dest='intput_path', help='Path to read metadata.json')
+arg_parser.add_argument('-p', nargs=1, type=Path, dest='input_path', help='Path to read metadata.json')
 arg_parser.add_argument('-o', nargs=1, type=Path, dest='output_path', help='Path to output documentation files')
 arg_parser.add_argument('-t', nargs=1, type=Path, dest='templates_path', help='Path to template files')
-arg_parser.add_argument('-f', nargs='+', type=str, dest='formats', help='Formats to be created documentation, must exists on template folder')
+arg_parser.add_argument('-f', nargs='+', type=str, dest='formats', help='Documentation formats to be created, the extension must exists in the template folder')
 parsed_args = vars(arg_parser.parse_args())
 
 parsed_args['formats'] = ['.%s' % f.lstrip('.') for f in parsed_args['formats']]
 parsed_args['output_path'][0].mkdir(parents=True, exist_ok=True)
 
-for path in parsed_args['intput_path'][0].rglob('metadata.json'):
+for path in parsed_args['input_path'][0].rglob('metadata.json'):
   if 'assets/queries/template/metadata.json' in str(path):
     continue
   with open(path) as file:
@@ -42,13 +42,14 @@ for path in parsed_args['intput_path'][0].rglob('metadata.json'):
 for platform in template_dict:
   for file_format in parsed_args['formats']:
     with open(os.path.join(parsed_args['templates_path'][0], 'template' + file_format)) as template:
-      # sort to be ordered by category
+      # sort by category
       data = {}
       for severity in severities:
         if severities[severity] in template_dict[platform]:
           data[severity] = {}
           for category in sorted(template_dict[platform][severities[severity]].keys()):
-            data[severity] = {**data[severity], **template_dict[platform][severities[severity]][category]}
+            metadata_info = template_dict[platform][severities[severity]][category]
+            data[severity] = {**data[severity], **metadata_info}
       template_string = template.read()
       template_jinja = Template(template_string)
       result = template_jinja.render({
@@ -57,4 +58,4 @@ for platform in template_dict:
         'colors': colors
       })
     with open(os.path.join(parsed_args['output_path'][0], platform.lower() + '-queries' + file_format), 'w') as output:
-      print (result, file=output)
+      print(result, file=output)
