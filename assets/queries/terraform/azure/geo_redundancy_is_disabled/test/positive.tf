60c052a8b434b3fa9b07f5c95365b1399246d81e
Rogério Peixoto <rogerio.peixoto@checkmarx.com>
2021-03-09 13:17:00 +0000

    adding samples with parser errors to ignore, adding redirect stderr flag, fixing interpolation in tf samples

--- assets/queries/terraform/azure/geo_redundancy_is_disabled/test/positive.tf
+++ assets/queries/terraform/azure/geo_redundancy_is_disabled/test/positive.tf
@@ -2,39 +2,38 @@
 resource "azurerm_postgresql_server" "positive1" {
     name                = "dbserver"
     location            = "usgovvirginia"
-    resource_group_name = "${azurerm_resource_group.jira_rg.name}"
+    resource_group_name = azurerm_resource_group.jira_rg.name
 
     sku_name   = "GP_Gen5_4"
     version    = "9.6"
     storage_mb = 640000
 
-    backup_retention_days        = "${var.jira_postgre_data_retention}"
+    backup_retention_days        = var.jira_postgre_data_retention
     auto_grow_enabled            = true
 
-    administrator_login          = "${var.mp_db_username}"
-    administrator_login_password = "${azurerm_key_vault_secret.db_pswd.value}"
+    administrator_login          = var.mp_db_username
+    administrator_login_password = azurerm_key_vault_secret.db_pswd.value
     ssl_enforcement_enabled      = true
 
-    tags                         = "${local.postgresqlserver_tags}"
+    tags                         = local.postgresqlserver_tags
 }
 
 resource "azurerm_postgresql_server" "positive2" {
     name                = "dbserver"
     location            = "usgovvirginia"
-    resource_group_name = "${azurerm_resource_group.jira_rg.name}"
+    resource_group_name = azurerm_resource_group.jira_rg.name
 
     sku_name   = "GP_Gen5_4"
     version    = "9.6"
     storage_mb = 640000
 
-    backup_retention_days        = "${var.jira_postgre_data_retention}"
+    backup_retention_days        = var.jira_postgre_data_retention
     geo_redundant_backup_enabled = false
     auto_grow_enabled            = true
 
-    administrator_login          = "${var.mp_db_username}"
-    administrator_login_password = "${azurerm_key_vault_secret.db_pswd.value}"
+    administrator_login          = var.mp_db_username
+    administrator_login_password = azurerm_key_vault_secret.db_pswd.value
     ssl_enforcement_enabled      = false
 
-    tags                         = "${local.postgresqlserver_tags}"
-
+    tags                         = local.postgresqlserver_tags
 }
