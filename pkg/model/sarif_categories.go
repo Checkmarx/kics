98636911c7465d0d7fbcd438ba42cb1b3ea55d40
Felipe Avelar <felipe.avelar@checkmarx.com>
2021-03-09 15:15:43 +0000

    Corrected sarif categories and some metadata categories
    Signed-off-by: Felipe Avelar <felipe.avelar@checkmarx.com>

--- pkg/model/sarif_categories.go
+++ pkg/model/sarif_categories.go
@@ -10,394 +10,134 @@ var noCategory = sarifTaxanomyDefinition{
 }
 
 var categories = map[string]sarifTaxanomyDefinition{
-	"Access control": {
+	"Access Control": {
 		DefinitionID:   categoryIdentifier + "001",
-		DefinitionName: "Access control",
+		DefinitionName: "Access Control",
 		DefinitionShortDescription: sarifMessage{
-			Text: "Description of category",
+			Text: "Service permission and identity management",
 		},
 		DefinitionFullDescription: sarifMessage{
-			Text: "Description of category",
+			Text: "Service permission and identity management",
 		},
 	},
-	"Azure container services": {
+	"Availability": {
 		DefinitionID:   categoryIdentifier + "002",
-		DefinitionName: "Azure container services",
+		DefinitionName: "Availability",
 		DefinitionShortDescription: sarifMessage{
-			Text: "Description of category",
+			Text: "Reliability and Scalability",
 		},
 		DefinitionFullDescription: sarifMessage{
-			Text: "Description of category",
+			Text: "Reliability and Scalability",
 		},
 	},
-	"Backup and disaster recovery": {
+	"Backup": {
 		DefinitionID:   categoryIdentifier + "003",
-		DefinitionName: "Backup and disaster recovery",
+		DefinitionName: "Backup",
 		DefinitionShortDescription: sarifMessage{
-			Text: "Description of category",
+			Text: "Survivability and Recovery",
 		},
 		DefinitionFullDescription: sarifMessage{
-			Text: "Description of category",
+			Text: "Survivability and Recovery",
 		},
 	},
-	"Best practices": {
+	"Best Practices": {
 		DefinitionID:   categoryIdentifier + "004",
-		DefinitionName: "Best practices",
+		DefinitionName: "Best Practices",
 		DefinitionShortDescription: sarifMessage{
-			Text: "Description of category",
+			Text: "Metadata management",
 		},
 		DefinitionFullDescription: sarifMessage{
-			Text: "Description of category",
+			Text: "Metadata management",
 		},
 	},
-	"Cloud assets management": {
+	"Build Process": {
 		DefinitionID:   categoryIdentifier + "005",
-		DefinitionName: "Cloud assets management",
+		DefinitionName: "Build Process",
 		DefinitionShortDescription: sarifMessage{
-			Text: "Description of category",
+			Text: "Insecure configurations when building/deploying Docker images",
 		},
 		DefinitionFullDescription: sarifMessage{
-			Text: "Description of category",
+			Text: "Insecure configurations when building/deploying Docker images",
 		},
 	},
-	"Compute": {
+	"Encryption": {
 		DefinitionID:   categoryIdentifier + "006",
-		DefinitionName: "Compute",
+		DefinitionName: "Encryption",
 		DefinitionShortDescription: sarifMessage{
-			Text: "Description of category",
+			Text: "Data Security and Encryption configuration",
 		},
 		DefinitionFullDescription: sarifMessage{
-			Text: "Description of category",
+			Text: "Data Security and Encryption configuration",
 		},
 	},
-	"Container": {
+	"Insecure Configurations": {
 		DefinitionID:   categoryIdentifier + "007",
-		DefinitionName: "Container",
+		DefinitionName: "Insecure Configurations",
 		DefinitionShortDescription: sarifMessage{
-			Text: "Description of category",
+			Text: "Configurations which expose the application unnecessarily",
 		},
 		DefinitionFullDescription: sarifMessage{
-			Text: "Description of category",
+			Text: "Configurations which expose the application unnecessarily",
 		},
 	},
-	"Data security": {
+	"Insecure Defaults": {
 		DefinitionID:   categoryIdentifier + "008",
-		DefinitionName: "Data security",
+		DefinitionName: "Insecure Defaults",
 		DefinitionShortDescription: sarifMessage{
-			Text: "Description of category",
+			Text: "Configurations that are insecure by default",
 		},
 		DefinitionFullDescription: sarifMessage{
-			Text: "Description of category",
+			Text: "Configurations that are insecure by default",
 		},
 	},
-	"Defaults": {
+	"Networking and Firewall": {
 		DefinitionID:   categoryIdentifier + "009",
-		DefinitionName: "Defaults",
+		DefinitionName: "Networking and Firewall",
 		DefinitionShortDescription: sarifMessage{
-			Text: "Description of category",
+			Text: "Network port exposure and firewall configuration",
 		},
 		DefinitionFullDescription: sarifMessage{
-			Text: "Description of category",
+			Text: "Network port exposure and firewall configuration",
 		},
 	},
-	"DNS Management": {
+	"Observability": {
 		DefinitionID:   categoryIdentifier + "010",
-		DefinitionName: "DNS Management",
+		DefinitionName: "Observability",
 		DefinitionShortDescription: sarifMessage{
-			Text: "Description of category",
+			Text: "Logging and Monitoring",
 		},
 		DefinitionFullDescription: sarifMessage{
-			Text: "Description of category",
+			Text: "Logging and Monitoring",
 		},
 	},
-	"Encryption & Key Management": {
+	"Resource Management": {
 		DefinitionID:   categoryIdentifier + "011",
-		DefinitionName: "Encryption & Key Management",
+		DefinitionName: "Resource Management",
 		DefinitionShortDescription: sarifMessage{
-			Text: "Description of category",
+			Text: "Resource and privilege limit configuration",
 		},
 		DefinitionFullDescription: sarifMessage{
-			Text: "Description of category",
+			Text: "Resource and privilege limit configuration",
 		},
 	},
-	"Encryption &KeyManagement": {
+	"Secret Management": {
 		DefinitionID:   categoryIdentifier + "012",
-		DefinitionName: "Encryption &KeyManagement",
+		DefinitionName: "Secret Management",
 		DefinitionShortDescription: sarifMessage{
-			Text: "Description of category",
+			Text: "Secret and Key management",
 		},
 		DefinitionFullDescription: sarifMessage{
-			Text: "Description of category",
+			Text: "Secret and Key management",
 		},
 	},
-	"Encryption and Key Management": {
+	"Supply-Chain": {
 		DefinitionID:   categoryIdentifier + "013",
-		DefinitionName: "Encryption and Key Management",
+		DefinitionName: "Supply-Chain",
 		DefinitionShortDescription: sarifMessage{
-			Text: "Description of category",
+			Text: "Dependency version management",
 		},
 		DefinitionFullDescription: sarifMessage{
-			Text: "Description of category",
-		},
-	},
-	"Filesystem": {
-		DefinitionID:   categoryIdentifier + "014",
-		DefinitionName: "Filesystem",
-		DefinitionShortDescription: sarifMessage{
-			Text: "Description of category",
-		},
-		DefinitionFullDescription: sarifMessage{
-			Text: "Description of category",
-		},
-	},
-	"High Availability": {
-		DefinitionID:   categoryIdentifier + "015",
-		DefinitionName: "High Availability",
-		DefinitionShortDescription: sarifMessage{
-			Text: "Description of category",
-		},
-		DefinitionFullDescription: sarifMessage{
-			Text: "Description of category",
-		},
-	},
-	"Identity & Access Management": {
-		DefinitionID:   categoryIdentifier + "016",
-		DefinitionName: "Identity & Access Management",
-		DefinitionShortDescription: sarifMessage{
-			Text: "Description of category",
-		},
-		DefinitionFullDescription: sarifMessage{
-			Text: "Description of category",
-		},
-	},
-	"Identity and Access Management": {
-		DefinitionID:   categoryIdentifier + "017",
-		DefinitionName: "Identity and Access Management",
-		DefinitionShortDescription: sarifMessage{
-			Text: "Description of category",
-		},
-		DefinitionFullDescription: sarifMessage{
-			Text: "Description of category",
-		},
-	},
-	"Image": {
-		DefinitionID:   categoryIdentifier + "018",
-		DefinitionName: "Image",
-		DefinitionShortDescription: sarifMessage{
-			Text: "Description of category",
-		},
-		DefinitionFullDescription: sarifMessage{
-			Text: "Description of category",
-		},
-	},
-	"Key management": {
-		DefinitionID:   categoryIdentifier + "019",
-		DefinitionName: "Key management",
-		DefinitionShortDescription: sarifMessage{
-			Text: "Description of category",
-		},
-		DefinitionFullDescription: sarifMessage{
-			Text: "Description of category",
-		},
-	},
-	"Logging": {
-		DefinitionID:   categoryIdentifier + "020",
-		DefinitionName: "Logging",
-		DefinitionShortDescription: sarifMessage{
-			Text: "Description of category",
-		},
-		DefinitionFullDescription: sarifMessage{
-			Text: "Description of category",
-		},
-	},
-	"Metadata": {
-		DefinitionID:   categoryIdentifier + "021",
-		DefinitionName: "Metadata",
-		DefinitionShortDescription: sarifMessage{
-			Text: "Description of category",
-		},
-		DefinitionFullDescription: sarifMessage{
-			Text: "Description of category",
-		},
-	},
-	"Monitoring": {
-		DefinitionID:   categoryIdentifier + "022",
-		DefinitionName: "Monitoring",
-		DefinitionShortDescription: sarifMessage{
-			Text: "Description of category",
-		},
-		DefinitionFullDescription: sarifMessage{
-			Text: "Description of category",
-		},
-	},
-	"Multi-Stage": {
-		DefinitionID:   categoryIdentifier + "023",
-		DefinitionName: "Multi-Stage",
-		DefinitionShortDescription: sarifMessage{
-			Text: "Description of category",
-		},
-		DefinitionFullDescription: sarifMessage{
-			Text: "Description of category",
-		},
-	},
-	"Network and Security": {
-		DefinitionID:   categoryIdentifier + "024",
-		DefinitionName: "Network and Security",
-		DefinitionShortDescription: sarifMessage{
-			Text: "Description of category",
-		},
-		DefinitionFullDescription: sarifMessage{
-			Text: "Description of category",
-		},
-	},
-	"Network Ports Security": {
-		DefinitionID:   categoryIdentifier + "025",
-		DefinitionName: "Network Ports Security",
-		DefinitionShortDescription: sarifMessage{
-			Text: "Description of category",
-		},
-		DefinitionFullDescription: sarifMessage{
-			Text: "Description of category",
-		},
-	},
-	"Network Security": {
-		DefinitionID:   categoryIdentifier + "026",
-		DefinitionName: "Network Security",
-		DefinitionShortDescription: sarifMessage{
-			Text: "Description of category",
-		},
-		DefinitionFullDescription: sarifMessage{
-			Text: "Description of category",
-		},
-	},
-	"Networking": {
-		DefinitionID:   categoryIdentifier + "027",
-		DefinitionName: "Networking",
-		DefinitionShortDescription: sarifMessage{
-			Text: "Description of category",
-		},
-		DefinitionFullDescription: sarifMessage{
-			Text: "Description of category",
-		},
-	},
-	"Operational": {
-		DefinitionID:   categoryIdentifier + "028",
-		DefinitionName: "Operational",
-		DefinitionShortDescription: sarifMessage{
-			Text: "Description of category",
-		},
-		DefinitionFullDescription: sarifMessage{
-			Text: "Description of category",
-		},
-	},
-	"Operational Efficiency": {
-		DefinitionID:   categoryIdentifier + "029",
-		DefinitionName: "Operational Efficiency",
-		DefinitionShortDescription: sarifMessage{
-			Text: "Description of category",
-		},
-		DefinitionFullDescription: sarifMessage{
-			Text: "Description of category",
-		},
-	},
-	"Package Management": {
-		DefinitionID:   categoryIdentifier + "030",
-		DefinitionName: "Package Management",
-		DefinitionShortDescription: sarifMessage{
-			Text: "Description of category",
-		},
-		DefinitionFullDescription: sarifMessage{
-			Text: "Description of category",
-		},
-	},
-	"Privileges": {
-		DefinitionID:   categoryIdentifier + "031",
-		DefinitionName: "Privileges",
-		DefinitionShortDescription: sarifMessage{
-			Text: "Description of category",
-		},
-		DefinitionFullDescription: sarifMessage{
-			Text: "Description of category",
-		},
-	},
-	"Resources Limiting": {
-		DefinitionID:   categoryIdentifier + "032",
-		DefinitionName: "Resources Limiting",
-		DefinitionShortDescription: sarifMessage{
-			Text: "Description of category",
-		},
-		DefinitionFullDescription: sarifMessage{
-			Text: "Description of category",
-		},
-	},
-	"Runtime": {
-		DefinitionID:   categoryIdentifier + "033",
-		DefinitionName: "Runtime",
-		DefinitionShortDescription: sarifMessage{
-			Text: "Description of category",
-		},
-		DefinitionFullDescription: sarifMessage{
-			Text: "Description of category",
-		},
-	},
-	"S3": {
-		DefinitionID:   categoryIdentifier + "034",
-		DefinitionName: "S3",
-		DefinitionShortDescription: sarifMessage{
-			Text: "Description of category",
-		},
-		DefinitionFullDescription: sarifMessage{
-			Text: "Description of category",
-		},
-	},
-	"Secrets": {
-		DefinitionID:   categoryIdentifier + "035",
-		DefinitionName: "Secrets",
-		DefinitionShortDescription: sarifMessage{
-			Text: "Description of category",
-		},
-		DefinitionFullDescription: sarifMessage{
-			Text: "Description of category",
-		},
-	},
-	"Storage": {
-		DefinitionID:   categoryIdentifier + "036",
-		DefinitionName: "Storage",
-		DefinitionShortDescription: sarifMessage{
-			Text: "Description of category",
-		},
-		DefinitionFullDescription: sarifMessage{
-			Text: "Description of category",
-		},
-	},
-	"User Management": {
-		DefinitionID:   categoryIdentifier + "037",
-		DefinitionName: "User Management",
-		DefinitionShortDescription: sarifMessage{
-			Text: "Description of category",
-		},
-		DefinitionFullDescription: sarifMessage{
-			Text: "Description of category",
-		},
-	},
-	"Versioning": {
-		DefinitionID:   categoryIdentifier + "038",
-		DefinitionName: "Versioning",
-		DefinitionShortDescription: sarifMessage{
-			Text: "Description of category",
-		},
-		DefinitionFullDescription: sarifMessage{
-			Text: "Description of category",
-		},
-	},
-	"Vulnerability and Threat Management": {
-		DefinitionID:   categoryIdentifier + "039",
-		DefinitionName: "Vulnerability and Threat Management",
-		DefinitionShortDescription: sarifMessage{
-			Text: "Description of category",
-		},
-		DefinitionFullDescription: sarifMessage{
-			Text: "Description of category",
+			Text: "Dependency version management",
 		},
 	},
 }
