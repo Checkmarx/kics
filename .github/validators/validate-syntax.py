d146020b1aeac5aff224b8ccd95615c49de7952f
Rogério Peixoto <rogerio.peixoto@checkmarx.com>
2021-03-14 23:36:27 +0000

    adding --diff flag to validate-syntax.py

--- .github/validators/validate-syntax.py
+++ .github/validators/validate-syntax.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """
 usage: validate-syntax.py [-h] -p LINTER_PATH [-e OPTIONS] [-s FILE_LIST] [-t]
-                          [-r] [-v]
+                          [-r] [-d CHANGES_PATH] [-v]
                           FILES [FILES ...]
 
 Execute linter against sample files
@@ -20,10 +20,13 @@ optional arguments:
                         Skip files list
   -t, --tmp             Copy to tmp directory before executing
   -r, --redirect        Redirect linter subprocess stderr to stdout
+  -d CHANGES_PATH, --diff CHANGES_PATH
+                        Run only on provided changed samples list
   -v, --verbose         Increase verbosity, default:ERROR v:INFO vv:DEBUG
 """
 import argparse
 import glob
+import json
 import os
 import shutil
 import subprocess
@@ -44,6 +47,8 @@ parser.add_argument('-t', '--tmp', default=False, action='store_true',
                     help='Copy to tmp directory before executing')
 parser.add_argument('-r', '--redirect', default=False, action='store_true',
                     help='Redirect linter subprocess stderr to stdout')
+parser.add_argument('-d', '--diff', type=str, metavar='CHANGES_PATH', default='',
+                    help='Run only on provided changed samples list')
 parser.add_argument('-v', '--verbose', action='count', default=0,
                     help='Increase verbosity, default:ERROR v:INFO vv:DEBUG')
 
@@ -78,6 +83,24 @@ def expand_files():
     for file_entry in args.files:
         all_files.extend(glob.glob(file_entry))
     return all_files
+
+
+def changed_sample_files(all_files):
+    if args.verbose:
+        print("--diff option enabled... filtering changed samples")
+
+    with open(args.diff, 'r') as fp:
+        changes_list = json.load(fp)
+    if args.verbose > 1:
+      print('detected git commit changes:')
+      print(changes_list)
+
+    changed_samples = [file for file in all_files if file in changes_list]
+    if args.verbose > 1:
+      print('changesd samples:')
+      print(changed_samples)
+
+    return changed_samples
 
 
 def read_skip_list():
@@ -124,50 +147,60 @@ def run_linter(file):
     return result
 
 
-print('starting validator')
+def main():
+    print('starting validator')
 
-skip_list = read_skip_list()
+    skip_list = read_skip_list()
 
-if args.verbose:
-    print(f"Ignore list is:{os.linesep}{os.linesep.join(skip_list)}\n")
+    if args.verbose:
+        print(f"Ignore list is:{os.linesep}{os.linesep.join(skip_list)}\n")
 
-############################################
-#  filter out test files and ignored files #
-############################################
-files = [
-    file for file in expand_files()
-    if file not in skip_list
-    and 'positive_expected_result.json' not in file
-]
+    ############################################
+    #  filter out test files and ignored files #
+    ############################################
+    files = [
+        file for file in expand_files()
+        if file not in skip_list
+        and 'positive_expected_result.json' not in file
+    ]
+    if args.verbose:
+      print(f'found {len(files)} samples in glob pattern')
 
-error_files = []
+    if args.diff:
+        files = changed_sample_files(files)
 
-print(f'found {len(files)} files to check')
+    error_files = []
 
-###########################################
-#                run linter               #
-###########################################
-for file in files:
-    result = ''
-    try:
-        result = run_linter(file)
-    except CalledProcessError as e:
-        error_files.append({'err': e, 'file': file})
-    if result:
-        for line in result.split('\n'):
-            if line and args.verbose:
-                print(f"{line}")
+    print(f'found {len(files)} files to check')
 
-###########################################
-#                list errors              #
-###########################################
-if len(error_files) > 0:
-    print("\n###### ERRORS #######")
-    for error_obj in error_files:
-        print(error_obj)
-        error_result = error_obj['err'].output.decode('utf-8').rstrip()
-        for line in error_result.split('\n'):
-            print(line)
-        print('----\n')
+    ###########################################
+    #                run linter               #
+    ###########################################
+    for file in files:
+        result = ''
+        try:
+            result = run_linter(file)
+        except CalledProcessError as e:
+            error_files.append({'err': e, 'file': file})
+        if result:
+            for line in result.split('\n'):
+                if line and args.verbose:
+                    print(f"{line}")
 
-summary(files, error_files)
+    ###########################################
+    #                list errors              #
+    ###########################################
+    if len(error_files) > 0:
+        print("\n###### ERRORS #######")
+        for error_obj in error_files:
+            print(error_obj)
+            error_result = error_obj['err'].output.decode('utf-8').rstrip()
+            for line in error_result.split('\n'):
+                print(line)
+            print('----\n')
+
+    summary(files, error_files)
+
+
+if __name__ == "__main__":
+    main()
