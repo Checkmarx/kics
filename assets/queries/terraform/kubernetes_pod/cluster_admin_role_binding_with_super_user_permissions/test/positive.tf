d85af8310cb5bb3d4e7c8d80656796ba22e87af1
Nuno Araujo <nuno.araujo@checkmarx.com>
2021-03-25 11:36:24 +0000

    update

--- assets/queries/terraform/kubernetes_pod/cluster_admin_role_binding_with_super_user_permissions/test/positive.tf
+++ assets/queries/terraform/kubernetes_pod/cluster_admin_role_binding_with_super_user_permissions/test/positive.tf
@@ -1,6 +1,6 @@
-resource "kubernetes_cluster_role_binding" "example" {
+resource "kubernetes_cluster_role_binding" "example2" {
   metadata {
-    name = "terraform-example"
+    name = "terraform-example2"
   }
   role_ref {
     api_group = "rbac.authorization.k8s.io"
