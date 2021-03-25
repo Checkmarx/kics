151b2d4c43f56094583c01ecfedcf6a97dfa67cd
Nuno Araujo <nuno.araujo@checkmarx.com>
2021-03-25 11:20:12 +0000

    Add Cluster Admin Rolebinding With Superuser Permissions query Closes #2391

---
+++ assets/queries/terraform/kubernetes_pod/cluster_admin_role_binding_with_super_user_permissions/test/positive.tf
@@ -0,0 +1,25 @@
+resource "kubernetes_cluster_role_binding" "example" {
+  metadata {
+    name = "terraform-example"
+  }
+  role_ref {
+    api_group = "rbac.authorization.k8s.io"
+    kind      = "ClusterRole"
+    name      = "cluster-admin"
+  }
+  subject {
+    kind      = "User"
+    name      = "admin"
+    api_group = "rbac.authorization.k8s.io"
+  }
+  subject {
+    kind      = "ServiceAccount"
+    name      = "default"
+    namespace = "kube-system"
+  }
+  subject {
+    kind      = "Group"
+    name      = "system:masters"
+    api_group = "rbac.authorization.k8s.io"
+  }
+}
