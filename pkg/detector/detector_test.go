2a3d05ff9685584c5a458f39e107e8df062e08b3
João Reigota <joao.reigota@checkmarx.com>
2021-03-29 11:53:51 +0100

    Added unit tests to detector package
    Signed-off-by: João Reigota <joao.reigota@checkmarx.com>

--- pkg/detector/detector_test.go
+++ pkg/detector/detector_test.go
@@ -1,1 +1,151 @@
 package detector
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/Checkmarx/kics/pkg/model"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+)
+
+type mockkindDetectLine struct {
+}
+
+type mockDefaultDetector struct {
+}
+
+func (m mockkindDetectLine) DetectLine(file *model.FileMetadata, searchKey string,
+	logWithFields *zerolog.Logger, outputLines int) model.VulnerabilityLines {
+	return model.VulnerabilityLines{
+		Line: 1,
+	}
+}
+
+func (m mockDefaultDetector) DetectLine(file *model.FileMetadata, searchKey string,
+	logWithFields *zerolog.Logger, outputLines int) model.VulnerabilityLines {
+	return model.VulnerabilityLines{
+		Line: 5,
+	}
+}
+
+func TestDetector_Add(t *testing.T) {
+	var mock mockkindDetectLine
+	det := initDetector()
+	type args struct {
+		kindDetector kindDetectLine
+		fileKind     model.FileKind
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		{
+			name: "test_add",
+			args: args{
+				kindDetector: mock,
+				fileKind:     model.KindDOCKER,
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			det = det.Add(tt.args.kindDetector, tt.args.fileKind)
+			got, ok := det.detectors[tt.args.fileKind]
+			if !ok {
+				t.Errorf("Add(), mockKindDetectLine is not in detectors")
+			}
+			if !reflect.DeepEqual(got, mock) {
+				t.Errorf("Add() = %v, want = %v", got, mock)
+			}
+		})
+	}
+}
+
+func TestDetector_SetupLogs(t *testing.T) {
+	det := initDetector()
+	type args struct {
+		log zerolog.Logger
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		{
+			name: "test_setup_logs",
+			args: args{
+				log: log.With().
+					Str("scanID", "Test").
+					Str("fileName", "Test_file_name").
+					Str("queryName", "Test_Query_name").
+					Logger(),
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			det.SetupLogs(&tt.args.log)
+			got := det.logWithFields
+			if !reflect.DeepEqual(*got, tt.args.log) {
+				t.Errorf("SetupLogs() = %v, want = %v", got, tt.args.log)
+			}
+		})
+	}
+}
+
+func TestDetector_DetectLine(t *testing.T) {
+	var mock mockkindDetectLine
+	var defaultmock mockDefaultDetector
+	det := initDetector().Add(mock, model.KindCOMMON)
+	det.defaultDetector = defaultmock
+
+	type args struct {
+		file      *model.FileMetadata
+		searchKey string
+	}
+	tests := []struct {
+		name string
+		args args
+		want model.VulnerabilityLines
+	}{
+		{
+			name: "test_kind_detect_line",
+			args: args{
+				file: &model.FileMetadata{
+					Kind: model.KindCOMMON,
+				},
+				searchKey: "",
+			},
+			want: model.VulnerabilityLines{
+				Line: 1,
+			},
+		},
+		{
+			name: "test_default_detect_line",
+			args: args{
+				file: &model.FileMetadata{
+					Kind: model.KindTerraform,
+				},
+				searchKey: "",
+			},
+			want: model.VulnerabilityLines{
+				Line: 5,
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := det.DetectLine(tt.args.file, tt.args.searchKey)
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("DetectLine() = %v, want = %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func initDetector() *DetectLine {
+	return NewDetectLine(3)
+}
