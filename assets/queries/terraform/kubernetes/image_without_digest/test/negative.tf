08221f395555d62b4db524dfc599aafc555f177a
rafaela-soares <rafaela.soares@checkmarx.com>
2021-03-31 14:18:27 +0100

    "Image Without Digest" query for TF Closes #2648

---
+++ assets/queries/terraform/kubernetes/image_without_digest/test/negative.tf
@@ -0,0 +1,52 @@
+resource "kubernetes_pod" "negative" {
+  metadata {
+    name = "terraform-example"
+  }
+
+  spec {
+    container {
+      image = "image@sha256:45b23dee08af5e43a7fea6c4cf9c25ccf269ee113168c19722f87876677c5cb"
+      name  = "example"
+
+      env {
+        name  = "environment"
+        value = "test"
+      }
+
+      port {
+        container_port = 8080
+      }
+
+      liveness_probe {
+        http_get {
+          path = "/nginx_status"
+          port = 80
+
+          http_header {
+            name  = "X-Custom-Header"
+            value = "Awesome"
+          }
+        }
+
+        initial_delay_seconds = 3
+        period_seconds        = 3
+      }
+    }
+
+    dns_config {
+      nameservers = ["1.1.1.1", "8.8.8.8", "9.9.9.9"]
+      searches    = ["example.com"]
+
+      option {
+        name  = "ndots"
+        value = 1
+      }
+
+      option {
+        name = "use-vc"
+      }
+    }
+
+    dns_policy = "None"
+  }
+}
