3dc97bf8a31152359e7b5e5c22d0bcdfbaa3b58f
Felipe Avelar <felipe.avelar@checkmarx.com>
2021-04-01 16:26:18 +0100

    Resolved codacy issue
    Signed-off-by: Felipe Avelar <felipe.avelar@checkmarx.com>

--- pkg/parser/terraform/converter/default.go
+++ pkg/parser/terraform/converter/default.go
@@ -14,6 +14,7 @@ import (
 	ctyjson "github.com/zclconf/go-cty/cty/json"
 )
 
+// InputVariableMap represents a set of terraform input variables
 type InputVariableMap map[string]cty.Value
 
 var inputVarMap = make(InputVariableMap)
