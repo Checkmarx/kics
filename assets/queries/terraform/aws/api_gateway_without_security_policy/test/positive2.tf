cf21226c6343801787233d824196d4c9a300f3f1
rafaela-soares <rafaela.soares@checkmarx.com>
2021-04-09 10:17:00 +0100

    requested changes

--- assets/queries/terraform/aws/api_gateway_without_security_policy/test/positive2.tf
+++ assets/queries/terraform/aws/api_gateway_without_security_policy/test/positive2.tf
@@ -1,7 +1,3 @@
-resource "aws_api_gateway_domain_name" "example" {
-  domain_name              = "api.example.com"
-}
-
 resource "aws_api_gateway_domain_name" "example2" {
   domain_name              = "api.example.com"
   security_policy = "TLS_1_0"
