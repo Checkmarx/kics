623390a29eda71ea18cf45dd7a3948e1149e0e6d
Rogério Peixoto <rogerio.peixoto@checkmarx.com>
2021-04-23 23:05:22 +0100

    feat(ci): docker image release improvements

--- .github/scripts/get-digest.sh
+++ .github/scripts/get-digest.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker inspect --format='{{.RepoDigests}}' $1 | sed -r 's/\[checkmarx\/kics@(sha256:)(.*)\]/\1\2/g'
+docker inspect --format='{{.RepoDigests}}' "$1" | sed -r 's/\[checkmarx\/kics@(sha256:)(.*)\]/\1\2/g'
