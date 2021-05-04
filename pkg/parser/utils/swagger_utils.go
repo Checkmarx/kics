e7f6d3c479846f905f012dfe4e079c6bb85c8a23
Rogério Peixoto <rogerio.peixoto@checkmarx.com>
2021-05-04 14:54:39 +0100

    fix(sast): unhandled errors flagged by cxSAST

--- pkg/parser/utils/swagger_utils.go
+++ pkg/parser/utils/swagger_utils.go
@@ -3,6 +3,8 @@ package utils
 import (
 	"os"
 	"path/filepath"
+
+	"github.com/rs/zerolog/log"
 )
 
 // AddSwaggerInfo gets and adds the content of a swagger file
@@ -10,6 +12,7 @@ func AddSwaggerInfo(path, swaggerPath string) map[string]interface{} {
 	var filePath string
 
 	if _, err := os.Stat(swaggerPath); err != nil { // content is not a full valid path or is an incomplete path
+		log.Trace().Msgf("path to the swagger specification is not a valid: %s", err)
 		filePath = filepath.Join(filepath.Dir(path), swaggerPath)
 	} else { // content is a full valid path
 		filePath = swaggerPath
