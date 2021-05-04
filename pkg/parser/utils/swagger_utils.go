581739f6ad935e284f8bb512e8f037fb5f90209f
Rogerio Peixoto <rogerio.peixoto@checkmarx.com>
2021-05-04 15:03:21 +0100

    Update swagger_utils.go
    Signed-off-by: Rogério Peixoto <rogerio.peixoto@checkmarx.com>

--- pkg/parser/utils/swagger_utils.go
+++ pkg/parser/utils/swagger_utils.go
@@ -11,8 +11,10 @@ import (
 func AddSwaggerInfo(path, swaggerPath string) map[string]interface{} {
 	var filePath string
 
-	if _, err := os.Stat(swaggerPath); err != nil { // content is not a full valid path or is an incomplete path
-		log.Trace().Msgf("path to the swagger specification is not a valid: %s", err)
+	_, err := os.Stat(swaggerPath)
+
+	if err != nil { // content is not a full valid path or is an incomplete path
+		log.Trace().Msgf("path to the swagger content specification is not a valid: %s", swaggerPath)
 		filePath = filepath.Join(filepath.Dir(path), swaggerPath)
 	} else { // content is a full valid path
 		filePath = swaggerPath
