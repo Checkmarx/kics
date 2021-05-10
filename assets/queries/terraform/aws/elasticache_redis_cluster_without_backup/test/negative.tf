eabab1796cbc600d8a06679f8a2358f3562d98dc
rafaela-soares <rafaela.soares@checkmarx.com>
2021-05-10 17:58:23 +0100

    Add "ElastiCache Redis Cluster Without Backup"

---
+++ assets/queries/terraform/aws/elasticache_redis_cluster_without_backup/test/negative.tf
@@ -0,0 +1,9 @@
+resource "aws_elasticache_cluster" "negative1" {
+  cluster_id           = "cluster"
+  engine               = "redis"
+  node_type            = "cache.m5.large"
+  num_cache_nodes      = 1
+  parameter_group_name = "default.redis6.x"
+
+  snapshot_retention_limit = 5
+}
---
+++ assets/queries/terraform/aws/elasticache_redis_cluster_without_backup/test/positive.tf
@@ -0,0 +1,17 @@
+resource "aws_elasticache_cluster" "positive1" {
+  cluster_id           = "cluster"
+  engine               = "redis"
+  node_type            = "cache.m5.large"
+  num_cache_nodes      = 1
+  parameter_group_name = "default.redis6.x"
+}
+
+resource "aws_elasticache_cluster" "positive2" {
+  cluster_id           = "cluster"
+  engine               = "redis"
+  node_type            = "cache.m5.large"
+  num_cache_nodes      = 1
+  parameter_group_name = "default.redis6.x"
+
+  snapshot_retention_limit = 0
+}
