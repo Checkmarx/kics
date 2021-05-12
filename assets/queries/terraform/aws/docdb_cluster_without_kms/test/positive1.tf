f2d81a6a93cbbf9ff033d3bbcd1fc11b31b66b88
João Reigota <joao.reigota@checkmarx.com>
2021-05-12 10:24:23 +0100

    Added DOCDB Cluster Without KMS query for Terraform #3269
    Signed-off-by: João Reigota <joao.reigota@checkmarx.com>

---
+++ assets/queries/terraform/aws/docdb_cluster_without_kms/test/positive1.tf
@@ -0,0 +1,9 @@
+resource "aws_docdb_cluster" "docdb" {
+  cluster_identifier      = "my-docdb-cluster"
+  engine                  = "docdb"
+  master_username         = "foo"
+  master_password         = "mustbeeightchars"
+  backup_retention_period = 5
+  preferred_backup_window = "07:00-09:00"
+  skip_final_snapshot     = true
+}
