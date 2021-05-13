456365bdc7dd1becb526d34f65d0c94b0394d8b6
Felipe Avelar <felipe.avelar@checkmarx.com>
2021-05-13 12:10:37 +0100

    Added "Global Accelerator Flow Logs Disabled" query for Terraform
    Signed-off-by: Felipe Avelar <felipe.avelar@checkmarx.com>

---
+++ assets/queries/terraform/aws/global_accelerator_flow_logs_disabled/test/negative1.tf
@@ -0,0 +1,11 @@
+resource "aws_globalaccelerator_accelerator" "negative1" {
+  name            = "Example"
+  ip_address_type = "IPV4"
+  enabled         = true
+
+  attributes {
+    flow_logs_enabled   = true
+    flow_logs_s3_bucket = "example-bucket"
+    flow_logs_s3_prefix = "flow-logs/"
+  }
+}
---
+++ assets/queries/terraform/aws/global_accelerator_flow_logs_disabled/test/positive1.tf
@@ -0,0 +1,26 @@
+resource "aws_globalaccelerator_accelerator" "positive1" {
+  name            = "Example"
+  ip_address_type = "IPV4"
+  enabled         = true
+}
+
+resource "aws_globalaccelerator_accelerator" "positive2" {
+  name            = "Example"
+  ip_address_type = "IPV4"
+  enabled         = true
+
+  attributes {
+    flow_logs_s3_bucket = "example-bucket"
+    flow_logs_s3_prefix = "flow-logs/"
+  }
+}
+
+resource "aws_globalaccelerator_accelerator" "positive3" {
+  name            = "Example"
+  ip_address_type = "IPV4"
+  enabled         = true
+
+  attributes {
+    flow_logs_enabled   = false
+  }
+}
