f0dc4cbe90a3ea9337871ebfaa4f47fef18e7e59
Felipe Avelar <felipe.avelar@checkmarx.com>
2021-05-13 17:16:24 +0100

    Splitted samples
    Signed-off-by: Felipe Avelar <felipe.avelar@checkmarx.com>

--- assets/queries/terraform/aws/docdb_logging_disabled/test/positive4.tf
+++ assets/queries/terraform/aws/docdb_logging_disabled/test/positive4.tf
@@ -1,4 +1,4 @@
-resource "aws_docdb_cluster" "positive1" {
+resource "aws_docdb_cluster" "positive4" {
   cluster_identifier      = "my-docdb-cluster"
   engine                  = "docdb"
   master_username         = "foo"
@@ -6,4 +6,6 @@ resource "aws_docdb_cluster" "positive1" {
   backup_retention_period = 5
   preferred_backup_window = "07:00-09:00"
   skip_final_snapshot     = true
+
+  enabled_cloudwatch_logs_exports = ["audit"]
 }
--- assets/queries/terraform/aws/neptune_logging_disabled/test/positive3.tf
+++ assets/queries/terraform/aws/neptune_logging_disabled/test/positive3.tf
@@ -1,4 +1,4 @@
-resource "aws_neptune_cluster" "postive1" {
+resource "aws_neptune_cluster" "postive3" {
   cluster_identifier                  = "neptune-cluster"
   engine                              = "neptune"
   backup_retention_period             = 5
@@ -6,4 +6,5 @@ resource "aws_neptune_cluster" "postive1" {
   skip_final_snapshot                 = true
   iam_database_authentication_enabled = true
   apply_immediately                   = true
+  enable_cloudwatch_logs_exports      = ["error"]
 }
