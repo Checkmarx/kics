6e577a9cc5b3962c12677223e08a56f88e238bff
Felipe Avelar <felipe.avelar@checkmarx.com>
2021-05-13 17:09:25 +0100

    Splitted samples
    Signed-off-by: Felipe Avelar <felipe.avelar@checkmarx.com>

--- assets/queries/terraform/aws/elasticsearch_logs_disabled/test/positive1.tf
+++ assets/queries/terraform/aws/elasticsearch_logs_disabled/test/positive1.tf
@@ -6,20 +6,3 @@ resource "aws_elasticsearch_domain" "positive1" {
     enabled                  = false
   }
 }
-
-resource "aws_elasticsearch_domain" "positive2" {
-  domain_name           = "example"
-  elasticsearch_version = "1.5"
-
-  cluster_config {
-    instance_type = "r4.large.elasticsearch"
-  }
-
-  snapshot_options {
-    automated_snapshot_start_hour = 23
-  }
-
-  tags = {
-    Domain = "TestDomain"
-  }
-}
