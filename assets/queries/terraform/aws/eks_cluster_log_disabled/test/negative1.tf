8e9889da28413ede8f9bb1f91ef97403f81a7ad2
Felipe Avelar <felipe.avelar@checkmarx.com>
2021-05-13 16:55:17 +0100

    Corrected issues
    Signed-off-by: Felipe Avelar <felipe.avelar@checkmarx.com>

--- assets/queries/terraform/aws/eks_cluster_log_disabled/test/negative1.tf
+++ assets/queries/terraform/aws/eks_cluster_log_disabled/test/negative1.tf
@@ -8,13 +8,4 @@ resource "aws_eks_cluster" "negative1" {
 
   enabled_cluster_log_types = ["api", "audit", "authenticator", "controllerManager", "scheduler"]
   name                      = var.cluster_name
-
-  # ... other configuration ...
-}
-
-resource "aws_cloudwatch_log_group" "negative2" {
-  name              = "/aws/eks/${var.cluster_name}/cluster"
-  retention_in_days = 7
-
-  # ... potentially other configuration ...
 }
