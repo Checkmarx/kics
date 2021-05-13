810479ee93ad10cf40c4fcf9e95219c734cd9f95
Felipe Avelar <felipe.avelar@checkmarx.com>
2021-05-13 17:06:32 +0100

    Splitted samples
    Signed-off-by: Felipe Avelar <felipe.avelar@checkmarx.com>

--- assets/queries/terraform/aws/global_accelerator_flow_logs_disabled/test/positive1.tf
+++ assets/queries/terraform/aws/global_accelerator_flow_logs_disabled/test/positive1.tf
@@ -3,24 +3,3 @@ resource "aws_globalaccelerator_accelerator" "positive1" {
   ip_address_type = "IPV4"
   enabled         = true
 }
-
-resource "aws_globalaccelerator_accelerator" "positive2" {
-  name            = "Example"
-  ip_address_type = "IPV4"
-  enabled         = true
-
-  attributes {
-    flow_logs_s3_bucket = "example-bucket"
-    flow_logs_s3_prefix = "flow-logs/"
-  }
-}
-
-resource "aws_globalaccelerator_accelerator" "positive3" {
-  name            = "Example"
-  ip_address_type = "IPV4"
-  enabled         = true
-
-  attributes {
-    flow_logs_enabled   = false
-  }
-}
