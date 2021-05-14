39d8de45cde4d6f42a6e9f69523a8e2bfd259ec1
João Reigota <joao.reigota@checkmarx.com>
2021-05-14 09:59:35 +0100

    Added ECS Task Definition Volume Not Encrypted query for Terraform
    Signed-off-by: João Reigota <joao.reigota@checkmarx.com>

---
+++ assets/queries/terraform/aws/ecs_task_definition_volume_not_encrypted/test/negative1.tf
@@ -0,0 +1,19 @@
+resource "aws_ecs_task_definition" "service" {
+  family                = "service"
+  container_definitions = file("task-definitions/service.json")
+
+  volume {
+    name = "service-storage"
+
+    efs_volume_configuration {
+      file_system_id          = aws_efs_file_system.fs.id
+      root_directory          = "/opt/data"
+      transit_encryption      = "ENABLED"
+      transit_encryption_port = 2999
+      authorization_config {
+        access_point_id = aws_efs_access_point.test.id
+        iam             = "ENABLED"
+      }
+    }
+  }
+}
