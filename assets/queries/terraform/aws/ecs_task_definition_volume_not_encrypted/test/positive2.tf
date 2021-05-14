bd68d1c621e0d5fa27f05692d0ccf1f72fe995af
João Reigota <joao.reigota@checkmarx.com>
2021-05-14 11:15:49 +0100

    Splited postive file
    Signed-off-by: João Reigota <joao.reigota@checkmarx.com>

--- assets/queries/terraform/aws/ecs_task_definition_volume_not_encrypted/test/positive2.tf
+++ assets/queries/terraform/aws/ecs_task_definition_volume_not_encrypted/test/positive2.tf
@@ -1,4 +1,4 @@
-resource "aws_ecs_task_definition" "service" {
+resource "aws_ecs_task_definition" "service_2" {
   family                = "service"
   container_definitions = file("task-definitions/service.json")
 
@@ -8,7 +8,6 @@ resource "aws_ecs_task_definition" "service" {
     efs_volume_configuration {
       file_system_id          = aws_efs_file_system.fs.id
       root_directory          = "/opt/data"
-      transit_encryption      = "DISABLED"
       transit_encryption_port = 2999
       authorization_config {
         access_point_id = aws_efs_access_point.test.id
