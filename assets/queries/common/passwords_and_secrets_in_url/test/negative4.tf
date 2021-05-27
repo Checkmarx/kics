bf8309096230a8a65b0af7db41a19bf569dd425a
João Reigota <joao.reigota@checkmarx.com>
2021-05-27 17:11:48 +0100

    Added Passwords And Secrets In URL common query #2785
    Signed-off-by: João Reigota <joao.reigota@checkmarx.com>

---
+++ assets/queries/common/passwords_and_secrets_in_url/test/negative4.tf
@@ -0,0 +1,26 @@
+data "http" "example" {
+  url = "https://checkpoint-api.hashicorp.com/v1/check/terraform"
+
+  # Optional request headers
+  request_headers = {
+    Accept = "application/json"
+  }
+}
+
+data "http" "example_2" {
+  url = "https://checkpoint-api.hashicorp.com/v1/check/terraform"
+
+  # Optional request headers
+  request_headers = {
+    Accept = "application/json"
+  }
+}
+
+data "http" "example_3" {
+  url = "https://checkpoint-api.hashicorp.com/v1/check/terraform"
+
+  # Optional request headers
+  request_headers = {
+    Accept = "application/json"
+  }
+}
