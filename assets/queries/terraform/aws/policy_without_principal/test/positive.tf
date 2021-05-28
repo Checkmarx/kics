185f03c10e4dc450cf5b972d596ee662ac6ff151
Rafaela Soares <rafaela.soares@checkmarx.com>
2021-05-28 11:37:08 +0100

    feat(query): "Policy Without Principal" for Terraform (#3469)

---
+++ assets/queries/terraform/aws/policy_without_principal/test/positive.tf
@@ -0,0 +1,37 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_kms_key" "secure_policy" {
+  description             = "KMS key + secure_policy"
+  deletion_window_in_days = 7
+
+  policy = <<EOF
+{
+    "Version": "2008-10-17",
+    "Statement": [
+        {
+            "Sid": "Secure Policy",
+            "Effect": "Allow",
+            "Resource": "*",
+            "Action": [
+            "kms:Create*",
+            "kms:Describe*",
+            "kms:Enable*",
+            "kms:List*",
+            "kms:Put*",
+            "kms:Update*",
+            "kms:Revoke*",
+            "kms:Disable*",
+            "kms:Get*",
+            "kms:Delete*",
+            "kms:TagResource",
+            "kms:UntagResource",
+            "kms:ScheduleKeyDeletion",
+            "kms:CancelKeyDeletion"
+            ]
+        }
+    ]
+}
+EOF
+}
