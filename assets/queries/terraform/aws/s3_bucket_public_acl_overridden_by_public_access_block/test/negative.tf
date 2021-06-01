5a836170497977566aaaa4fc09ce3ab1d8f3bc6a
Rafaela Soares <rafaela.soares@checkmarx.com>
2021-06-01 12:45:30 +0100

    feat(query): add "S3 Bucket Public ACL Overridden By Public Access Block" for Terraform #3499

---
+++ assets/queries/terraform/aws/s3_bucket_public_acl_overridden_by_public_access_block/test/negative.tf
@@ -0,0 +1,16 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "public-bucket2" {
+  bucket = "bucket-with-public-acl-32"
+  acl = "public-read-write"
+}
+
+resource "aws_s3_bucket_public_access_block" "block_public_bucket_32" {
+  bucket = aws_s3_bucket.public-bucket2.id
+  block_public_acls = false
+  block_public_policy = true
+  ignore_public_acls = false
+  restrict_public_buckets = true
+}
