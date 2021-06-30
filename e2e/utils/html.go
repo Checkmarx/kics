422d19da79656d08973474d01941c5a9ceafcca9
Lucas Mendes <Lucas.Mendes@checkmarx.com>
2021-06-30 15:02:28 +0100

    fix: html nil nodes

--- e2e/utils/html.go
+++ e2e/utils/html.go
@@ -11,99 +11,125 @@ import (
 	"golang.org/x/net/html"
 )
 
+var availableCategories = map[string]string{
+	"Access Control":          "CAT001",
+	"Availability":            "CAT002",
+	"Backup":                  "CAT003",
+	"Best Practices":          "CAT004",
+	"Build Process":           "CAT005",
+	"Encryption":              "CAT006",
+	"Insecure Configurations": "CAT007",
+	"Insecure Defaults":       "CAT008",
+	"Networking and Firewall": "CAT009",
+	"Observability":           "CAT010",
+	"Resource Management":     "CAT011",
+	"Secret Management":       "CAT012",
+	"Supply-Chain":            "CAT013",
+	"Structure and Semantics": "CAT014",
+}
+
+var availablePlatforms = map[string]string{
+	"Ansible":        "ansible",
+	"CloudFormation": "cloudFormation",
+	"Common":         "common",
+	"Dockerfile":     "dockerfile",
+	"Kubernetes":     "k8s",
+	"OpenAPI":        "openAPI",
+	"Terraform":      "terraform",
+}
+
 func HTMLValidation(t *testing.T, file string) {
-	availableCategories := map[string]string{
-		"Access Control":          "CAT001",
-		"Availability":            "CAT002",
-		"Backup":                  "CAT003",
-		"Best Practices":          "CAT004",
-		"Build Process":           "CAT005",
-		"Encryption":              "CAT006",
-		"Insecure Configurations": "CAT007",
-		"Insecure Defaults":       "CAT008",
-		"Networking and Firewall": "CAT009",
-		"Observability":           "CAT010",
-		"Resource Management":     "CAT011",
-		"Secret Management":       "CAT012",
-		"Supply-Chain":            "CAT013",
-		"Structure and Semantics": "CAT014",
-	}
-
-	availablePlatforms := map[string]string{
-		"Ansible":        "ansible",
-		"CloudFormation": "cloudFormation",
-		"Common":         "common",
-		"Dockerfile":     "dockerfile",
-		"Kubernetes":     "k8s",
-		"OpenAPI":        "openAPI",
-		"Terraform":      "terraform",
-	}
-
 	// Read & Parse Expected HTML Report
-	expectHTMLString, err := os.ReadFile(filepath.Join("fixtures", file))
-	require.NoError(t, err)
-	expectedHTML, err := html.Parse(strings.NewReader(string(expectHTMLString)))
-	require.NoError(t, err)
+	expectHTMLString, errExpStr := os.ReadFile(filepath.Join("fixtures", file))
+	require.NoError(t, errExpStr, "Openning Expected HTML File should not yield an error")
+	expectedHTML, errExp := html.Parse(strings.NewReader(string(expectHTMLString)))
+	require.NoError(t, errExp, "Openning Expected HTML File should not yield an error")
 
 	// Read & Parse Output HTML Report
-	actualHTMLString, err := os.ReadFile(filepath.Join("output", file))
-	require.NoError(t, err)
-	actualHTML, err := html.Parse(strings.NewReader(string(actualHTMLString)))
-	require.NoError(t, err)
+	actualHTMLString, errActStr := os.ReadFile(filepath.Join("output", file))
+	require.NoError(t, errActStr, "Openning Actual HTML File should not yield an error")
+	actualHTML, errAct := html.Parse(strings.NewReader(string(actualHTMLString)))
+	require.NoError(t, errAct, "Openning Actual HTML File should not yield an error")
 
 	// Compare Header Data (Paths, Platforms)
 	headerIds := []string{"scan-paths", "scan-platforms"}
 	for arg := range headerIds {
-		expectedValue := getElementByID(expectedHTML, headerIds[arg]).LastChild.Data
-		actualValue := getElementByID(actualHTML, headerIds[arg]).LastChild.Data
+		expectedValue := getElementByID(expectedHTML, headerIds[arg])
+		actualValue := getElementByID(actualHTML, headerIds[arg])
 
-		require.Equal(t, expectedValue, actualValue,
-			"[HTML Report] HTML Element <%s>:\n- Expected value: %s\n- Actual value: %s\n", headerIds[arg], expectedValue, actualValue)
+		require.NotNil(t, actualValue.LastChild,
+			"[%s] Invalid value in Element ID <%s>", file, headerIds[arg])
+
+		require.Equal(t, expectedValue.LastChild.Data, actualValue.LastChild.Data,
+			"[%s] HTML Element <%s>:\n- Expected value: %s\n- Actual value: %s\n",
+			file, headerIds[arg], expectedValue.LastChild.Data, actualValue.LastChild.Data)
 	}
 
 	// Compare Severity Values (High, Medium, Total...)
 	severityIds := []string{"info", "low", "medium", "high", "total"}
 	for arg := range severityIds {
 		nodeIdentificator := "severity-count-" + severityIds[arg]
-		expectedSeverityValue := getElementByID(expectedHTML, nodeIdentificator).FirstChild.Data
-		actualSeverityValue := getElementByID(actualHTML, nodeIdentificator).FirstChild.Data
+		expectedSeverityValue := getElementByID(expectedHTML, nodeIdentificator)
+		actualSeverityValue := getElementByID(actualHTML, nodeIdentificator)
 
-		require.Equal(t, expectedSeverityValue, actualSeverityValue,
-			"[HTML Report] HTML Element <%s>:\n- Expected value: %s\n- Actual value: %s\n",
-			nodeIdentificator, expectedSeverityValue, actualSeverityValue)
+		require.NotNil(t, actualSeverityValue.FirstChild,
+			"[%s] Invalid value in Element ID <%s>", file, nodeIdentificator)
+
+		require.Equal(t, expectedSeverityValue.FirstChild.Data, actualSeverityValue.FirstChild.Data,
+			"[%s] HTML Element <%s>:\n- Expected value: %s\n- Actual value: %s\n",
+			file, nodeIdentificator, expectedSeverityValue.FirstChild.Data, actualSeverityValue.FirstChild.Data)
 
 		classIdentificator := "severity-partial-count-" + severityIds[arg]
 		expectedSeverityClassValues := getAndSumElementsByClass(expectedHTML, classIdentificator)
 		actualSeverityClassValues := getAndSumElementsByClass(actualHTML, classIdentificator)
 
 		require.Equal(t, expectedSeverityClassValues, actualSeverityClassValues,
-			"[HTML Report] Expected Sum of HTML classes <%s>:\n- Expected Value: %d\n- Actual value: %d\n",
-			classIdentificator, expectedSeverityClassValues, actualSeverityClassValues)
+			"[%s] Expected Sum of HTML classes <%s>:\n- Expected Value: %d\n- Actual value: %d\n",
+			file, classIdentificator, expectedSeverityClassValues, actualSeverityClassValues)
 	}
 
 	// Validate Query Names
-	for _, node := range getElementsByClass(actualHTML, "query-name") {
+	queriesClassname := "query-name"
+	queriesClasses := getElementsByClass(actualHTML, queriesClassname)
+	for _, node := range queriesClasses {
 		require.NotNil(t, node.FirstChild,
-			"[HTML Report] Invalid query in class element < query-info-platform >")
+			"[%s] Invalid query in class element <%s>", file, queriesClassname)
 	}
 
 	// Validate Platforms
-	for _, node := range getElementsByClass(actualHTML, "query-info-platform") {
+	platformClassname := "query-info-platform"
+	platformsClasses := getElementsByClass(actualHTML, platformClassname)
+	for _, node := range platformsClasses {
 		require.NotNil(t, node.FirstChild,
-			"[HTML Report] Invalid plataform in class element < query-info-platform >")
+			"[%s] Invalid plataform in class element <%s>", file, platformClassname)
 
 		require.NotEmpty(t, availablePlatforms[node.FirstChild.Data],
-			"[HTML Report] Invalid plataform in class element < query-info-platform >: %s\n", node.FirstChild.Data)
+			"[%s] Invalid plataform in class element <%s>: %s\n", file, platformClassname, node.FirstChild.Data)
 	}
 
 	// Validate Categories
-	for _, node := range getElementsByClass(actualHTML, "query-info-category") {
+	categoriesClassname := "query-info-category"
+	categoriesClasses := getElementsByClass(actualHTML, categoriesClassname)
+	for _, node := range categoriesClasses {
 		require.NotNil(t, node.FirstChild,
-			"[HTML Report] Invalid category in class element < query-info-category >")
+			"[%s] Invalid category in class element <%s>", file, categoriesClassname)
 
 		require.NotEmpty(t, availableCategories[node.FirstChild.Data],
-			"[HTML Report] Invalid category in class element < query-info-category >: %s\n", node.FirstChild.Data)
+			"[%s] Invalid category in class element <%s>: %s\n", file, categoriesClassname, node.FirstChild.Data)
 	}
+
+	// Validate Total Number of Results and Code-Boxes
+	totalClassname := "severity-count-total"
+	total, err := strconv.Atoi(getElementByID(actualHTML, totalClassname).FirstChild.Data)
+	require.NoError(t, err, "Getting Total Results should not yield an error")
+
+	codeBoxClassname := "code-box"
+	codeBoxClasses := getElementsByClass(actualHTML, codeBoxClassname)
+
+	require.Equal(t, total, len(codeBoxClasses),
+		"[%s] The Value of Element ID <%s> is not equal the number of <%s> classes in the HTML File"+
+			"\n- <%s>: %d\n- <%s>: %d\n",
+		file, totalClassname, codeBoxClassname, totalClassname, total, codeBoxClassname, len(codeBoxClasses))
 }
 
 func findAttribute(node *html.Node, key string) (string, bool) {
