d610927f00723dbea52e55706958b6ccaafb2700
Felipe Avelar <felipe.avelar@checkmarx.com>
2021-07-05 14:32:40 +0100

    feat(query): Added Security Group Not Used query for terraform
    Signed-off-by: Felipe Avelar <felipe.avelar@checkmarx.com>

---
+++ assets/queries/terraform/aws/security_groups_not_used/test/negative1.tf
@@ -0,0 +1,34 @@
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = [aws_vpc.main.cidr_block]
+    ipv6_cidr_blocks = [aws_vpc.main.ipv6_cidr_block]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_tls"
+  }
+}
+
+resource "aws_lb" "test" {
+  name = "test"
+  load_balancer_type = "application"
+  subnets = [aws_subnet.subnet1.id, aws_subnet.subnet2.id]
+  internal = true
+  security_groups = [aws_security_group.allow_tls.id]
+}
