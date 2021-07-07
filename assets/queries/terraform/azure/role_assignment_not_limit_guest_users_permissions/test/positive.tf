35e2afec6f7c55caade7c2b7468ef61f563708a1
Rafaela Soares <a79034@alunos.uminho.pt>
2021-07-07 15:28:32 +0100

    feat(query): "Role Assignment Not Limit Guest User Permissions" for Terraform (Closes #3804) (#3805)
    Co-authored-by: Rogerio Peixoto <rogerio.peixoto@checkmarx.com>

---
+++ assets/queries/terraform/azure/role_assignment_not_limit_guest_users_permissions/test/positive.tf
@@ -0,0 +1,22 @@
+resource "azurerm_role_definition" "example" {
+  name        = "my-custom-role"
+  scope       = data.azurerm_subscription.primary.id
+  description = "This is a custom role created via Terraform"
+
+  permissions {
+    actions     = ["*"]
+    not_actions = []
+  }
+
+  assignable_scopes = [
+    data.azurerm_subscription.primary.id, 
+  ]
+}
+
+resource "azurerm_role_assignment" "example" {
+  name               = "00000000-0000-0000-0000-000000000000"
+  scope              = data.azurerm_subscription.primary.id
+  role_definition_name = "Guest"
+  role_definition_id = azurerm_role_definition.example.role_definition_resource_id
+  principal_id       = data.azurerm_client_config.example.object_id
+}
