db1969366f4c41010b1f37bc0791135d3aa8d273
Felipe Avelar <felipe.avelar@checkmarx.com>
2021-07-13 10:57:27 +0100

    feat(query): Added Variable Without Type for terraform
    Signed-off-by: Felipe Avelar <felipe.avelar@checkmarx.com>

---
+++ assets/queries/terraform/general/variable_without_type/test/negative1.tf
@@ -0,0 +1,12 @@
+variable "cluster_name" {
+  default = "example"
+  description = "cluster name"
+  type    = string
+}
+
+resource "aws_eks_cluster" "negative1" {
+  depends_on = [aws_cloudwatch_log_group.example]
+
+  enabled_cluster_log_types = ["api", "audit", "authenticator", "controllerManager", "scheduler"]
+  name                      = var.cluster_name
+}
