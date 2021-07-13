5974366327cfcb21abbb6476c2cdb00be4c96aea
Felipe Avelar <felipe.avelar@checkmarx.com>
2021-07-13 11:20:18 +0100

    feat(query): Added Output Without Description for terraform
    Signed-off-by: Felipe Avelar <felipe.avelar@checkmarx.com>

---
+++ assets/queries/terraform/general/output_without_description/test/negative1.tf
@@ -0,0 +1,11 @@
+output "cluster_name" {
+  value = "example"
+  description = "cluster name"
+  type    = string
+}
+
+resource "aws_eks_cluster" "negative1" {
+  depends_on = [aws_cloudwatch_log_group.example]
+
+  enabled_cluster_log_types = ["api", "audit", "authenticator", "controllerManager", "scheduler"]
+}
---
+++ assets/queries/terraform/general/output_without_description/test/positive3.tf
@@ -0,0 +1,9 @@
+output "cluster_name" {
+  value = "example"
+  type    = string
+  description = ""
+}
+
+resource "aws_eks_cluster" "positive1" {
+  depends_on = [aws_cloudwatch_log_group.example]
+}
