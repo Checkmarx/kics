5b79d7009314f4655386a4cb55f25a92586889cd
rafaela-soares <rafaela.soares@checkmarx.com>
2021-08-11 15:01:32 +0100

    Updated "1ec253ab-c220-4d63-b2de-5b40e0af9293"

--- assets/queries/terraform/aws/s3_bucket_without_restriction_of_public_bucket/test/negative.tf
+++ assets/queries/terraform/aws/s3_bucket_without_restriction_of_public_bucket/test/negative.tf
@@ -8,5 +8,5 @@ resource "aws_s3_bucket_public_access_block" "negative2" {
   block_public_acls   = true
   block_public_policy = true
 
-  restrict_public_buckets = "test"
+  restrict_public_buckets = true
 }
