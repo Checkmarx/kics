453782f8eeea73a888f9cca7da6e1e4e22925bef
rafaela-soares <rafaela.soares@checkmarx.com>
2021-08-12 13:56:32 +0100

    added "S3 Bucket with Unsecured CORS Rule" query

---
+++ assets/queries/terraform/aws/s3_bucket_with_unsecured_cors_rule/test/positive2.tf
@@ -0,0 +1,22 @@
+
+resource "aws_s3_bucket" "positive2" {
+  bucket = "my-tf-test-bucket"
+  acl    = "public-read"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+
+  versioning {
+    enabled = false
+  }
+
+  cors_rule {
+    allowed_headers = ["*"]
+    allowed_methods = ["GET", "PUT", "POST", "DELETE", "HEAD"]
+    allowed_origins = ["*"]
+    expose_headers  = ["ETag"]
+    max_age_seconds = 3000
+  }
+}
