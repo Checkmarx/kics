eb15ac72673b8aa77bdb430eb413794adaf8868e
Rafaela Soares <rafaela.soares@checkmarx.com>
2021-08-13 12:26:44 +0100

    feat(query): added "S3 Bucket with Unsecured CORS Rule" query for Ansible, CloudFormation and Terraform #4030

---
+++ assets/queries/terraform/aws/s3_bucket_with_unsecured_cors_rule/test/negative.tf
@@ -0,0 +1,11 @@
+resource "aws_s3_bucket" "negative1" {
+  bucket = "s3-website-test.hashicorp.com"
+  acl    = "public-read"
+
+  cors_rule {
+    allowed_methods = ["PUT", "POST"]
+    allowed_origins = ["https://s3-website-test.hashicorp.com"]
+    expose_headers  = ["ETag"]
+    max_age_seconds = 3000
+  }
+}
