f4e313c9e5e2faf7a0308e38aa77d61e561f64b2
Rogério Peixoto <rogerio.peixoto@checkmarx.com>
2021-08-25 12:47:35 +0100

    fix(quer): s3 mfa_delete closes #3753

--- assets/queries/terraform/aws/s3_bucket_without_enabled_mfa_delete/test/positive3.tf
+++ assets/queries/terraform/aws/s3_bucket_without_enabled_mfa_delete/test/positive3.tf
@@ -1,5 +1,5 @@
-//comment
-resource "aws_s3_bucket" "negative1" {
+
+resource "aws_s3_bucket" "positive3" {
   bucket = "my-tf-test-bucket"
   acl    = "private"
 
@@ -10,6 +10,6 @@ resource "aws_s3_bucket" "negative1" {
 
   versioning {
     enabled = true
-    mfa_delete = true
+    mfa_delete = false
   }
 }
