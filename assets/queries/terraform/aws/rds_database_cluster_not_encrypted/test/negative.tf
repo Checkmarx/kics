aaab265c9f4217c97ce3124858f4a05911e3aaab
tiagocruz20 <tiago.cruz@checkmarx.com>
2021-09-07 10:11:32 +0100

    requested changes

--- assets/queries/terraform/aws/rds_database_cluster_not_encrypted/test/negative.tf
+++ assets/queries/terraform/aws/rds_database_cluster_not_encrypted/test/negative.tf
@@ -1,5 +1,5 @@
 resource "aws_db_cluster_snapshot" "negative" {
-  db_cluster_identifier          = aws_rds_cluster.example.id
+  db_cluster_identifier          = "development_cluster"
   db_cluster_snapshot_identifier = "resourcetestsnapshot1234"
   storage_encrypted = true
 }
