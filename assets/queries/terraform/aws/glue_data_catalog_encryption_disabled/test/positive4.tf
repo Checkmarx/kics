88463dd2cc477d7801a2fe9f61270078da5d4181
Rafaela Soares <rafaela.soares@checkmarx.com>
2021-09-09 10:13:25 +0100

    feat(query): added "Glue Data Catalog Encryption Disabled" for Terraform (#4157)

---
+++ assets/queries/terraform/aws/glue_data_catalog_encryption_disabled/test/positive4.tf
@@ -0,0 +1,12 @@
+resource "aws_glue_data_catalog_encryption_settings" "positive4" {
+  data_catalog_encryption_settings {
+    connection_password_encryption {
+      aws_kms_key_id                       = aws_kms_key.test.arn
+      return_connection_password_encrypted = true
+    }
+
+    encryption_at_rest {
+      catalog_encryption_mode = "SSE-KMS"
+    }
+  }
+}
