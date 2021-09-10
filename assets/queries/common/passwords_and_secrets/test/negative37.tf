0956301a8212368faaab81684c8b65384db1a964
rafaela-soares <rafaela.soares@checkmarx.com>
2021-09-10 12:58:11 +0100

    changing generic secret

---
+++ assets/queries/common/passwords_and_secrets/test/negative37.tf
@@ -0,0 +1,11 @@
+resource "aws_lambda_function" "analysis_lambda4" {
+  # lambda have plain text secrets in environment variables
+  filename      = "resources/lambda_function_payload.zip"
+  function_name = "${local.resource_prefix.value}-analysis"
+  role          = "${aws_iam_role.iam_for_lambda.arn}"
+  handler       = "exports.test"
+
+  source_code_hash = "${filebase64sha256("resources/lambda_function_payload.zip")}"
+
+  runtime = "nodejs12.x"
+}
