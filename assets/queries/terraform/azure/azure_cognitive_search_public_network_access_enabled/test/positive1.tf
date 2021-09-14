476951d2b55181ab2fbd84235f74debce340d12b
Tiago Cruz <88029213+tiagocruz20@users.noreply.github.com>
2021-09-14 09:48:12 +0100

    feat(query): added Azure Cognitive Search Public Network Access Enabled for Terraform (#4183)

---
+++ assets/queries/terraform/azure/azure_cognitive_search_public_network_access_enabled/test/positive1.tf
@@ -0,0 +1,7 @@
+resource "azurerm_search_service" "positive1" {
+  name                = "example-search-service"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  sku                 = "standard"
+  public_network_access_enabled = true
+}
