cc6aef10ab3c3497727c45ef9cefc446202b37c1
João Reigota <joao.reigota@checkmarx.com>
2021-09-20 16:00:58 +0100

    fix linter issues

--- assets/queries/terraform/aws/missing_log_metric_filter_or_alarm_for_iam_policy_changes/test/negative1.tf
+++ assets/queries/terraform/aws/missing_log_metric_filter_or_alarm_for_iam_policy_changes/test/negative1.tf
@@ -14,7 +14,7 @@ resource "aws_cloudwatch_metric_alarm" "cis_unauthorized_api_calls_cw_alarm" {
   alarm_name                = "CIS-3.1-UnauthorizedAPICalls"
   comparison_operator       = "GreaterThanOrEqualToThreshold"
   evaluation_periods        = "1"
-  metric_name               = aws_cloudwatch_log_metric_filter.cis_unauthorized_api_calls_metric_filter.id
+  metric_name               = aws_cloudwatch_log_metric_filter.cis_missing_ima.id
   namespace                 = "CIS_Metric_Alarm_Namespace"
   period                    = "300"
   statistic                 = "Sum"
@@ -53,20 +53,6 @@ resource "aws_cloudwatch_metric_alarm" "cis_no_mfa_console_signin_cw_alarm" {
   comparison_operator       = "GreaterThanOrEqualToThreshold"
   evaluation_periods        = "1"
   metric_name               = aws_cloudwatch_log_metric_filter.cis_no_mfa_console_signin_metric_filter.id
-  namespace                 = "CIS_Metric_Alarm_Namespace"
-  period                    = "300"
-  statistic                 = "Sum"
-  threshold                 = "1"
-  alarm_description         = "Monitoring for single-factor console logins will increase visibility into accounts that are not protected by MFA."
-  alarm_actions             = [aws_sns_topic.cis_alerts_sns_topic.arn]
-  insufficient_data_actions = []
-}
-
-resource "aws_cloudwatch_metric_alarm" "cis_unauthorized_api_calls_cw_alarm" {
-  alarm_name                = "CIS-3.2-ConsoleSigninWithoutMFA"
-  comparison_operator       = "GreaterThanOrEqualToThreshold"
-  evaluation_periods        = "1"
-  metric_name               = aws_cloudwatch_log_metric_filter.cis_unauthorized_api_calls_cw_alarm.id
   namespace                 = "CIS_Metric_Alarm_Namespace"
   period                    = "300"
   statistic                 = "Sum"
--- assets/queries/terraform/aws/missing_log_metric_filter_or_alarm_for_iam_policy_changes/test/positive1.tf
+++ assets/queries/terraform/aws/missing_log_metric_filter_or_alarm_for_iam_policy_changes/test/positive1.tf
@@ -23,17 +23,3 @@ resource "aws_cloudwatch_log_metric_filter" "cis_no_mfa_console_signin_metric_fi
     value     = "1"
   }
 }
-
-resource "aws_cloudwatch_metric_alarm" "cis_no_mfa_console_signin_cw_alarm" {
-  alarm_name                = "CIS-3.2-ConsoleSigninWithoutMFA"
-  comparison_operator       = "GreaterThanOrEqualToThreshold"
-  evaluation_periods        = "1"
-  metric_name               = aws_cloudwatch_log_metric_filter.cis_no_mfa_console_signin_metric_filter.id
-  namespace                 = "CIS_Metric_Alarm_Namespace"
-  period                    = "300"
-  statistic                 = "Sum"
-  threshold                 = "1"
-  alarm_description         = "Monitoring for single-factor console logins will increase visibility into accounts that are not protected by MFA."
-  alarm_actions             = [aws_sns_topic.CIS_Alerts_SNS_Topic.arn]
-  insufficient_data_actions = []
-}
