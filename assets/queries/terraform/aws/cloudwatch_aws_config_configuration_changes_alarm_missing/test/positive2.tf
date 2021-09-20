c9ac8e951dce12ea67f17711f41ccc10c8685c2b
João Reigota <joao.reigota@checkmarx.com>
2021-09-20 15:20:49 +0100

    Added Cloudwatch AWS Config Configuration Changes Alarm Missing (4.9)

---
+++ assets/queries/terraform/aws/cloudwatch_aws_config_configuration_changes_alarm_missing/test/positive2.tf
@@ -0,0 +1,25 @@
+resource "aws_cloudwatch_log_metric_filter" "cis_no_mfa_console_signin_metric_filter" {
+  name           = "CIS-ConsoleSigninWithoutMFA"
+  pattern        = "{ ($.eventName = \"ConsoleLogin\") && ($.additionalEventData.MFAUsed != \"Yes\") }"
+  log_group_name = aws_cloudwatch_log_group.CIS_CloudWatch_LogsGroup.name
+
+  metric_transformation {
+    name      = "CIS-ConsoleSigninWithoutMFA"
+    namespace = "CIS_Metric_Alarm_Namespace"
+    value     = "1"
+  }
+}
+
+resource "aws_cloudwatch_metric_alarm" "cis_no_mfa_console_signin_cw_alarm" {
+  alarm_name                = "CIS-3.2-ConsoleSigninWithoutMFA"
+  comparison_operator       = "GreaterThanOrEqualToThreshold"
+  evaluation_periods        = "1"
+  metric_name               = aws_cloudwatch_log_metric_filter.cis_no_mfa_console_signin_metric_filter.id
+  namespace                 = "CIS_Metric_Alarm_Namespace"
+  period                    = "300"
+  statistic                 = "Sum"
+  threshold                 = "1"
+  alarm_description         = "Monitoring for single-factor console logins will increase visibility into accounts that are not protected by MFA."
+  alarm_actions             = [aws_sns_topic.CIS_Alerts_SNS_Topic.arn]
+  insufficient_data_actions = []
+}
