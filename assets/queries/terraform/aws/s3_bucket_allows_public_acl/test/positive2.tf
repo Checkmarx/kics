fcad259367a03da40afe38914d41d65b556c1ce6
João Reigota <joao.reigota@checkmarx.com>
2021-09-24 10:40:27 +0100

    feat(query): Added Support to tf modules for S3 Bucket Allows Public ACL
    Signed-off-by: João Reigota <joao.reigota@checkmarx.com>

---
+++ assets/queries/terraform/aws/s3_bucket_allows_public_acl/test/positive2.tf
@@ -0,0 +1,31 @@
+module "s3_bucket" {
+  source = "terraform-aws-modules/s3-bucket/aws"
+  version = "3.7.0"
+
+  bucket = "my-s3-bucket"
+  acl    = "private"
+  restrict_public_buckets = true
+  block_public_acls = false
+
+  versioning = {
+    enabled = true
+  }
+
+  policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Id": "MYBUCKETPOLICY",
+  "Statement": [
+    {
+      "Sid": "IPAllow",
+      "Effect": "Deny",
+      "Action": "s3:*",
+      "Resource": "arn:aws:s3:::my_tf_test_bucket/*",
+      "Condition": {
+         "IpAddress": {"aws:SourceIp": "8.8.8.8/32"}
+      }
+    }
+  ]
+}
+POLICY
+}
