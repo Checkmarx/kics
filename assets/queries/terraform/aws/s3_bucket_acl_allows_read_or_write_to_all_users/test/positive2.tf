4aad92758bfcef4e5d9dd198e5a50f8b64558aed
rafaela-soares <rafaela.soares@checkmarx.com>
2021-09-24 12:45:59 +0100

    TF module for 38c5ee0d-7f22-4260-ab72-5073048df100

--- assets/queries/terraform/aws/s3_bucket_acl_allows_read_or_write_to_all_users/test/positive2.tf
+++ assets/queries/terraform/aws/s3_bucket_acl_allows_read_or_write_to_all_users/test/positive2.tf
@@ -1,17 +1,3 @@
-resource "aws_s3_bucket" "positive1" {
-  bucket = "my-tf-test-bucket"
-  acl    = "public-read"
-
-  tags = {
-    Name        = "My bucket"
-    Environment = "Dev"
-  }
-
-  versioning {
-    enabled = true
-  }
-}
-
 resource "aws_s3_bucket" "positive2" {
   bucket = "my-tf-test-bucket"
   acl    = "public-read-write"
