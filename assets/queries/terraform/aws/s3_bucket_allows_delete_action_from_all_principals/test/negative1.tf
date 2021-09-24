d4e7f5efde3d95fadf43b9281d3ba817765e65f5
rafaela-soares <rafaela.soares@checkmarx.com>
2021-09-24 14:37:32 +0100

    TF module for ffdf4b37-7703-4dfe-a682-9d2e99bc6c09

---
+++ assets/queries/terraform/aws/s3_bucket_allows_delete_action_from_all_principals/test/negative1.tf
@@ -0,0 +1,21 @@
+resource "aws_s3_bucket_policy" "negative1" {
+  bucket = aws_s3_bucket.b.id
+
+  policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Id": "MYBUCKETPOLICY",
+  "Statement": [
+    {
+      "Sid": "IPAllow",
+      "Effect": "Deny",
+      "Action": "s3:*",
+      "Resource": "arn:aws:s3:::my_tf_test_bucket/*",
+      "Condition": {
+         "IpAddress": {"aws:SourceIp": "8.8.8.8/32"}
+      }
+    }
+  ]
+}
+POLICY
+}
