80c97e06e4201cd3a85aee472f3887df7c3d89a9
tiagocruz20 <tiago.cruz@checkmarx.com>
2021-09-24 12:25:46 +0100

    requested changes

--- assets/queries/terraform/aws/s3_bucket_with_unsecured_cors_rule/test/positive3.tf
+++ assets/queries/terraform/aws/s3_bucket_with_unsecured_cors_rule/test/positive3.tf
@@ -11,7 +11,7 @@ module "s3_bucket" {
     },
   ]
 
-    cors_rule {
+   cors_rule {
     allowed_headers = ["*"]
     allowed_methods = ["PUT", "POST"]
     allowed_origins = ["https://s3-website-test.hashicorp.com"]
