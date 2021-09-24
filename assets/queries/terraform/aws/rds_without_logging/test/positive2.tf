ce53681cef0806f66d6ae437a0364fc5422d507f
Rogério Peixoto <rogerio.peixoto@checkmarx.com>
2021-09-24 09:52:27 +0100

    addressing requested changes
    Signed-off-by: Rogério Peixoto <rogerio.peixoto@checkmarx.com>

--- assets/queries/terraform/aws/rds_without_logging/test/positive2.tf
+++ assets/queries/terraform/aws/rds_without_logging/test/positive2.tf
@@ -1,11 +1,3 @@
-resource "aws_db_instance" "positive1" {
-  allocated_storage = 5
-  engine            = "postgres"
-  instance_class    = "db.t3.small"
-  password          = "admin"
-  username          = "admin"
-}
-
 resource "aws_db_instance" "positive2" {
   allocated_storage = 5
   engine            = "postgres"
