d0c3fedea60a8ba6eaae72d3b859f725ee33465b
rafaela-soares <rafaela.soares@checkmarx.com>
2021-09-24 15:07:09 +0100

    TF module for "S3 Bucket Logging Disabled"

---
+++ assets/queries/terraform/aws/s3_bucket_logging_disabled/test/negative2.tf
@@ -0,0 +1,15 @@
+module "s3_bucket" {
+  source = "terraform-aws-modules/s3-bucket/aws"
+  version = "3.7.0"
+
+  bucket = "my-s3-bucket"
+  acl    = "private"
+
+  versioning = {
+    enabled = true
+  }
+
+  logging {
+    target_bucket = "logs"
+  }
+}
