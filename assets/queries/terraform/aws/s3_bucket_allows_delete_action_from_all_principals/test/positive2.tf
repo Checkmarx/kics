e9772dc39a6f0e692e677f6ddf404dc0a6eca7d2
Rafaela Soares <rafaela.soares@checkmarx.com>
2021-09-27 12:47:22 +0100

    feat(query): support of Terraform modules for "S3 Bucket Allows Put Action From All Principals" #4274

--- assets/queries/terraform/aws/s3_bucket_allows_delete_action_from_all_principals/test/positive2.tf
+++ assets/queries/terraform/aws/s3_bucket_allows_delete_action_from_all_principals/test/positive2.tf
@@ -1,8 +1,4 @@
-resource "aws_s3_bucket" "negative1" {
-  bucket = "my_tf_test_bucket"
-}
-
-resource "aws_s3_bucket_policy" "negative2" {
+resource "aws_s3_bucket_policy" "positive2" {
   bucket = aws_s3_bucket.b.id
 
   policy = <<POLICY
@@ -12,8 +8,11 @@ resource "aws_s3_bucket_policy" "negative2" {
   "Statement": [
     {
       "Sid": "IPAllow",
-      "Effect": "Deny",
-      "Action": "s3:*",
+      "Effect": "Allow",
+      "Principal": {
+        "AWS": "*"
+      },
+      "Action": "s3:DeleteObject",
       "Resource": "arn:aws:s3:::my_tf_test_bucket/*",
       "Condition": {
          "IpAddress": {"aws:SourceIp": "8.8.8.8/32"}
