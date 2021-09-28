0691149a8270839539b4f3df477622767e0f52a0
Rafaela Soares <rafaela.soares@checkmarx.com>
2021-09-28 18:05:48 +0100

    feat(query): support of Terraform modules for "S3 Bucket SSE Disabled" (#4281)
    Co-authored-by: Rogerio Peixoto <rogerio.peixoto@checkmarx.com>
    Co-authored-by: Felipe Avelar <felipe.avelar@checkmarx.com>

---
+++ assets/queries/terraform/aws/s3_bucket_sse_disabled/test/positive1.tf
@@ -0,0 +1,13 @@
+resource "aws_s3_bucket" "positive1" {
+  bucket = "my-tf-test-bucket"
+  acl    = "private"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+
+  versioning {
+    mfa_delete = true
+  }
+}
