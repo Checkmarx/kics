b8ce56f7ce1f8a81ddfbf914413795ce81dca0da
Tiago Cruz <88029213+tiagocruz20@users.noreply.github.com>
2021-09-28 16:15:55 +0100

    feat(query): support of Terraform modules for "Security Group With Un… (#4245)

---
+++ assets/queries/terraform/aws/security_group_with_unrestricted_access_to_ssh/test/positive1.tf
@@ -0,0 +1,24 @@
+resource "aws_security_group" "positive1" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_tls"
+  }
+}
--- assets/queries/terraform/aws/security_group_with_unrestricted_access_to_ssh/test/positive2.tf
+++ assets/queries/terraform/aws/security_group_with_unrestricted_access_to_ssh/test/positive2.tf
@@ -1,29 +1,3 @@
-resource "aws_security_group" "positive1" {
-  name        = "allow_tls"
-  description = "Allow TLS inbound traffic"
-  vpc_id      = aws_vpc.main.id
-
-  ingress {
-    description = "TLS from VPC"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "allow_tls"
-  }
-}
-
-
 resource "aws_security_group" "positive2" {
   name        = "allow_tls"
   description = "Allow TLS inbound traffic"
@@ -48,6 +22,3 @@ resource "aws_security_group" "positive2" {
     Name = "allow_tls"
   }
 }
-
-
-
