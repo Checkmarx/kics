9de663cef74cd922ee9b0fdcd9a1ade60e379545
Rafaela Soares <rafaela.soares@checkmarx.com>
2021-09-28 09:53:31 +0100

    feat(query): support of Terraform modules for "SQS Queue Exposed" #4284

---
+++ assets/queries/terraform/aws/sqs_queue_exposed/test/positive1.tf
@@ -0,0 +1,24 @@
+resource "aws_sqs_queue" "positive1" {
+  name = "examplequeue"
+
+  policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Id": "sqspolicy",
+  "Statement": [
+    {
+      "Sid": "First",
+      "Effect": "Allow",
+      "Principal": "*",
+      "Action": "sqs:SendMessage",
+      "Resource": "${aws_sqs_queue.q.arn}",
+      "Condition": {
+        "ArnEquals": {
+          "aws:SourceArn": "${aws_sns_topic.example.arn}"
+        }
+      }
+    }
+  ]
+}
+POLICY
+}
