776e16de55b43b44b1cdc767593d72fd6c7cf41a
rafaela-soares <rafaela.soares@checkmarx.com>
2021-10-06 12:07:11 +0100

    first approach of the refactoring printer package

---
+++ internal/console/printer/flags.go
@@ -0,0 +1,140 @@
+package printer
+
+import (
+	"io"
+	"os"
+	"path/filepath"
+	"strings"
+
+	consoleHelpers "github.com/Checkmarx/kics/internal/console/helpers"
+	"github.com/Checkmarx/kics/internal/constants"
+	"github.com/gookit/color"
+	"github.com/pkg/errors"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+)
+
+// NoColor - disables ASCII color codes
+func NoColor(opt interface{}, changed bool) error {
+	noColor := opt.(bool)
+	if noColor {
+		color.Disable()
+		consoleLogger.NoColor = true
+	}
+	return nil
+}
+
+// Verbose - redirects log entries to stdout
+func Verbose(opt interface{}, changed bool) error {
+	verbose := opt.(bool)
+	if verbose {
+		consoleLogger = zerolog.ConsoleWriter{Out: os.Stdout}
+		outConsoleLogger = os.Stdout
+	}
+	return nil
+}
+
+// Silent - disables stdout output
+func Silent(opt interface{}) error {
+	silent := opt.(bool)
+	if silent {
+		color.SetOutput(io.Discard)
+		os.Stdout = nil
+		log.Logger = log.Output(zerolog.MultiLevelWriter(io.Discard, outFileLogger.(io.Writer)))
+	}
+	return nil
+}
+
+// CI - enable only log messages to CLI output
+func CI(opt interface{}) error {
+	ci := opt.(bool)
+	if ci {
+		color.SetOutput(io.Discard)
+		log.Logger = log.Output(zerolog.MultiLevelWriter(outConsoleLogger, outFileLogger.(io.Writer)))
+		os.Stdout = nil
+	}
+	return nil
+}
+
+// LogFormat - configures the logs format (JSON,pretty).
+func LogFormat(logFormat string) error {
+	if logFormat == constants.LogFormatJSON {
+		log.Logger = log.Output(zerolog.MultiLevelWriter(outConsoleLogger, loggerFile.(io.Writer)))
+		outFileLogger = loggerFile
+		outConsoleLogger = os.Stdout
+	} else if logFormat == constants.LogFormatPretty {
+		fileLogger = consoleHelpers.CustomConsoleWriter(&zerolog.ConsoleWriter{Out: loggerFile.(io.Writer), NoColor: true})
+		log.Logger = log.Output(zerolog.MultiLevelWriter(consoleLogger, fileLogger))
+		outFileLogger = fileLogger
+		outConsoleLogger = zerolog.ConsoleWriter{Out: os.Stdout, NoColor: true}
+	} else {
+		return errors.New("invalid log format")
+	}
+	return nil
+}
+
+// LogPath - sets the log files location
+func LogPath(opt interface{}, changed bool) error {
+	logPath := opt.(string)
+	var err error
+	if !changed {
+		if loggerFile == nil {
+			loggerFile = io.Discard
+			return nil
+		}
+		return nil
+	}
+	if logPath == "" {
+		logPath, err = getDefaultLogPath()
+		if err != nil {
+			return err
+		}
+	} else if filepath.Dir(logPath) != "." {
+		if createErr := os.MkdirAll(filepath.Dir(logPath), os.ModePerm); createErr != nil {
+			return createErr
+		}
+	}
+
+	loggerFile, err = os.OpenFile(logPath, os.O_CREATE|os.O_WRONLY, os.ModePerm)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// LogFile - enables write to log file
+func LogFile(opt interface{}, changed bool) error {
+	logFile := opt.(bool)
+	if logFile {
+		logPath, err := getDefaultLogPath()
+		if err != nil {
+			return err
+		}
+		loggerFile, err = os.OpenFile(filepath.Clean(logPath), os.O_CREATE|os.O_WRONLY, os.ModePerm)
+		if err != nil {
+			return err
+		}
+		fileLogger = consoleHelpers.CustomConsoleWriter(&zerolog.ConsoleWriter{Out: loggerFile.(io.Writer), NoColor: true})
+	}
+	return nil
+}
+
+// LogLevel - sets log level
+func LogLevel(opt interface{}, changed bool) error {
+	logLevel := opt.(string)
+	switch strings.ToUpper(logLevel) {
+	case "TRACE":
+		zerolog.SetGlobalLevel(zerolog.TraceLevel)
+	case "DEBUG":
+		zerolog.SetGlobalLevel(zerolog.DebugLevel)
+	case "INFO":
+		zerolog.SetGlobalLevel(zerolog.InfoLevel)
+	case "WARN":
+		zerolog.SetGlobalLevel(zerolog.WarnLevel)
+	case "ERROR":
+		zerolog.SetGlobalLevel(zerolog.ErrorLevel)
+	case "FATAL":
+		zerolog.SetGlobalLevel(zerolog.FatalLevel)
+	}
+	return nil
+}
---
+++ internal/console/printer/utils.go
@@ -0,0 +1,49 @@
+package printer
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"time"
+
+	consoleFlags "github.com/Checkmarx/kics/internal/console/flags"
+	"github.com/Checkmarx/kics/internal/constants"
+	"github.com/pkg/errors"
+	"github.com/rs/zerolog/log"
+)
+
+func validateFlags() error {
+	if consoleFlags.GetBoolFlag(consoleFlags.VerboseFlag) && consoleFlags.GetBoolFlag(consoleFlags.SilentFlag) {
+		return errors.New("can't provide 'silent' and 'verbose' flags simultaneously")
+	}
+
+	if consoleFlags.GetBoolFlag(consoleFlags.VerboseFlag) && consoleFlags.GetBoolFlag(consoleFlags.CIFlag) {
+		return errors.New("can't provide 'verbose' and 'ci' flags simultaneously")
+	}
+
+	if consoleFlags.GetBoolFlag(consoleFlags.CIFlag) && consoleFlags.GetBoolFlag(consoleFlags.SilentFlag) {
+		return errors.New("can't provide 'silent' and 'ci' flags simultaneously")
+	}
+	return nil
+}
+
+func getDefaultLogPath() (string, error) {
+	currentWorkDir, err := os.Getwd()
+	if err != nil {
+		return "", err
+	}
+	return filepath.Join(currentWorkDir, constants.DefaultLogFile), nil
+}
+
+// PrintScanDuration prints the scan duration
+func PrintScanDuration(elapsed time.Duration) {
+	if consoleFlags.GetBoolFlag(consoleFlags.CIFlag) {
+		elapsedStrFormat := "Scan duration: %vms\n"
+		fmt.Printf(elapsedStrFormat, elapsed.Milliseconds())
+		log.Info().Msgf(elapsedStrFormat, elapsed.Milliseconds())
+	} else {
+		elapsedStrFormat := "Scan duration: %v\n"
+		fmt.Printf(elapsedStrFormat, elapsed)
+		log.Info().Msgf(elapsedStrFormat, elapsed)
+	}
+}
