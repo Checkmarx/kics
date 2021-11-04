25c995b8f0952af066623daa528f44bd4b767ebc
Rafaela Soares <rafaela.soares@checkmarx.com>
2021-11-04 18:18:32 +0000

    feat(query): added "App Service FTPS Enforce Disabled" for Terraform (#4451)

---
+++ assets/queries/terraform/azure/app_service_ftps_enforce_disabled/test/negative2.tf
@@ -0,0 +1,12 @@
+resource "azurerm_app_service" "negative2" {
+  name                = "example-app-service"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  app_service_plan_id = azurerm_app_service_plan.example.id
+
+  site_config {
+    dotnet_framework_version = "v4.0"
+    scm_type                 = "LocalGit"
+    ftps_state = "Disabled"
+  }
+}
