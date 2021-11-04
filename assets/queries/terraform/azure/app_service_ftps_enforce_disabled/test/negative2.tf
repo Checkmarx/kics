1c40dbfc68ec2b7bd7aa8e44a28389087a6ac9f1
rafaela-soares <rafaela.soares@checkmarx.com>
2021-11-04 11:41:47 +0000

    renaming resources

--- assets/queries/terraform/azure/app_service_ftps_enforce_disabled/test/negative2.tf
+++ assets/queries/terraform/azure/app_service_ftps_enforce_disabled/test/negative2.tf
@@ -1,4 +1,4 @@
-resource "azurerm_app_service" "negative" {
+resource "azurerm_app_service" "negative2" {
   name                = "example-app-service"
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
--- assets/queries/terraform/azure/app_service_ftps_enforce_disabled/test/positive1.tf
+++ assets/queries/terraform/azure/app_service_ftps_enforce_disabled/test/positive1.tf
@@ -1,4 +1,4 @@
-resource "azurerm_app_service" "positive3" {
+resource "azurerm_app_service" "positive1" {
   name                = "example-app-service"
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
