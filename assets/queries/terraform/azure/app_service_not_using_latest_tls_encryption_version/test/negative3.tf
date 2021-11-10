15f6d23d813c4e47e3290ee0f0ed590c7c9802b2
rafaela-soares <rafaela.soares@checkmarx.com>
2021-11-10 09:39:45 +0000

    correcting b7b9d1c7-2d3b-49b4-b867-ebbe68d0b643

--- assets/queries/terraform/azure/app_service_not_using_latest_tls_encryption_version/test/negative3.tf
+++ assets/queries/terraform/azure/app_service_not_using_latest_tls_encryption_version/test/negative3.tf
@@ -1,4 +1,4 @@
-resource "azurerm_app_service" "positive1" {
+resource "azurerm_app_service" "negative3" {
   name                = "example-app-service"
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
