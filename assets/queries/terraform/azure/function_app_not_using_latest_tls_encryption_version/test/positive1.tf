60a2c308725ac7c036225688559f6083bb3e97c3
rafaela-soares <rafaela.soares@checkmarx.com>
2021-11-10 09:40:55 +0000

    correcting sample name

--- assets/queries/terraform/azure/function_app_not_using_latest_tls_encryption_version/test/positive1.tf
+++ assets/queries/terraform/azure/function_app_not_using_latest_tls_encryption_version/test/positive1.tf
@@ -1,4 +1,4 @@
-resource "azurerm_function_app" "positive2" {
+resource "azurerm_function_app" "positive1" {
   name                       = "test-azure-functions"
   location                   = azurerm_resource_group.example.location
   resource_group_name        = azurerm_resource_group.example.name
