16da56a025debaf7ab47fcaeee1a7649928a62d0
Lucas Mendes <lucas.mendes@checkmarx.com>
2021-11-10 10:23:25 +0000

    feat: improve CIS Descriprtion Testcase

--- e2e/testcases/e2e-cli-052_scan_with_custom_cis_descriptions.go
+++ e2e/testcases/e2e-cli-052_scan_with_custom_cis_descriptions.go
@@ -10,21 +10,20 @@ func init() { //nolint
 				[]string{"scan", "--queries-path", "../assets/queries",
 					"-p", "fixtures/samples/terraform.tf", "--no-color",
 					"--include-queries", "487f4be7-3fd9-4506-a07a-eae252180c08,cfdcabb0-fc06-427c-865b-c59f13e898ce",
-					"-o", "output", "--output-name", "output/E2E_CLI_052_RESULT_CIS.json"},
+					"-o", "output", "--output-name", "output/E2E_CLI_052_RESULTS_ALL_HAVE_CIS.json"},
 
 				[]string{"scan", "--queries-path", "../assets/queries",
 					"-p", "fixtures/samples/terraform.tf", "--no-color",
-					"--include-queries", "487f4be7-3fd9-4506-a07a-eae252180c08,cfdcabb0-fc06-427c-865b-c59f13e898ce",
-					"-o", "output", "--output-name", "output/E2E_CLI_052_RESULT.json"},
+					"-o", "output", "--output-name", "output/E2E_CLI_052_RESULTS_SOME_HAVE_CIS.json"},
 			},
-			UseMock: []bool{true, false},
+			UseMock: []bool{true, true},
 			ExpectedResult: []ResultsValidation{
 				{
-					ResultsFile:    "E2E_CLI_052_RESULT_CIS",
+					ResultsFile:    "E2E_CLI_052_RESULTS_ALL_HAVE_CIS",
 					ResultsFormats: []string{"json-cis"},
 				},
 				{
-					ResultsFile:    "E2E_CLI_052_RESULT",
+					ResultsFile:    "E2E_CLI_052_RESULTS_SOME_HAVE_CIS",
 					ResultsFormats: []string{"json"},
 				},
 			},
