00fa105b4e2c377f736aa60ada34af0120dd9f11
Jean-Yves Nicolas <82532271+jynicolas@users.noreply.github.com>
2021-11-12 12:28:27 +0100

    fix(query) Wrong detection of unused security group (#4502)

---
+++ assets/queries/terraform/aws/security_groups_not_used/test/negative3.tf
@@ -0,0 +1,41 @@
+# given:
+#  - used security group
+#  - aws_instance
+# when:
+#  - used security group attached to aws_instance
+# then:
+#  - do not detect any unused security group
+
+resource "aws_security_group" "used_sg" {
+  name        = "used-sg"
+  description = "Used security group"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description      = "Some port"
+    from_port        = 43
+    to_port          = 43
+    protocol         = "tcp"
+    cidr_blocks      = [aws_vpc.main.cidr_block]
+    ipv6_cidr_blocks = [aws_vpc.main.ipv6_cidr_block]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+}
+
+resource "aws_instance" "negative3" {
+  ami = "ami-003634241a8fcdec0"
+
+  instance_type = "t2.micro"
+
+  vpc_security_group_ids = [ "${aws_security_group.used_sg.id}" ]
+  
+}
+
