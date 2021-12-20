fbf0fcb241fe56c828bb6db735458f113439b8b5
Lucas Mendes <82591370+cxlucas@users.noreply.github.com>
2021-12-20 09:13:26 +0000

    feat(e2e): added CycloneDX report schema (#4599)

--- e2e/cli_test.go
+++ e2e/cli_test.go
@@ -137,8 +137,14 @@ func checkExpectedOutput(t *testing.T, tt *testcases.TestCase, argIndex int) {
 	// Check result file (JUNIT - XML)
 	if utils.Contains(resultsFormats, "junit") {
 		filename := "junit-" + tt.Args.ExpectedResult[argIndex].ResultsFile + ".xml"
-		json := utils.XMLToJSON(t, filename)
+		json := utils.XMLToJSON(t, filename, "junit")
 		utils.JSONSchemaValidationFromData(t, json, "result-junit.json")
+	}
+	// Check result file (CYCLONEDX - XML)
+	if utils.Contains(resultsFormats, "cyclonedx") {
+		filename := "cyclonedx-" + tt.Args.ExpectedResult[argIndex].ResultsFile + ".xml"
+		json := utils.XMLToJSON(t, filename, "cyclonedx")
+		utils.JSONSchemaValidationFromData(t, json, "result-cyclonedx.json")
 	}
 }
 
