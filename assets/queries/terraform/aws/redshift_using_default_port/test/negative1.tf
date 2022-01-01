aeb1cc700b18a5db8b610f166cf6bc290e3dec2c
cosmicgirl97 <rafaelamsoaressilva@gmail.com>
2022-01-01 16:28:37 +0000

    added "Redshift Using Default Port"

---
+++ assets/queries/terraform/aws/redshift_using_default_port/test/negative1.tf
@@ -0,0 +1,10 @@
+resource "aws_redshift_cluster" "negative1" {
+  cluster_identifier    = "tf-redshift-cluster"
+  database_name         = "mydb"
+  master_username       = "foo"
+  master_password       = "Mustbe8characters"
+  node_type             = "dc1.large"
+  cluster_type          = "single-node"
+  publicly_accessible   = false
+  port                  = 1150
+}
