4c7d98d153a39d8c5c02238d9d4378fb8c9874a0
cosmicgirl97 <rafaelamsoaressilva@gmail.com>
2022-01-20 21:40:12 +0000

    added "API Gateway Without Configured Authorizer"

---
+++ assets/queries/terraform/aws/api_gateway_without_configured_authorizer/test/positive.tf
@@ -0,0 +1,10 @@
+resource "aws_api_gateway_authorizer" "demo" {
+  name                   = "demo"
+  rest_api_id            = aws_api_gateway_rest_api.demo.id
+  authorizer_uri         = aws_lambda_function.authorizer.invoke_arn
+  authorizer_credentials = aws_iam_role.invocation_role.arn
+}
+
+resource "aws_api_gateway_rest_api" "demo2" {
+  name = "auth-demo"
+}
