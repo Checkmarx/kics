cb4847736ba43bcec0ea1e18bdbed7578011fa29
joaorufi <joao.rufino@checkmarx.com>
2022-02-21 15:38:40 -0300

    InitJsonFlags and GetAllFlags test
    Signed-off-by: joaorufi <joao.rufino@checkmarx.com>

--- internal/console/flags/flags_test.go
+++ internal/console/flags/flags_test.go
@@ -6,8 +6,115 @@ import (
 	"testing"
 
 	"github.com/Checkmarx/kics/internal/constants"
+	"github.com/spf13/cobra"
 	"github.com/stretchr/testify/require"
 )
+
+func TestFlags_GetAllFlags(t *testing.T) {
+	boolFlag := true
+	flagsBoolReferences["bool"] = &boolFlag
+	intFlag := 5
+	flagsIntReferences["int"] = &intFlag
+	multiStrFlag := []string{"test"}
+	flagsMultiStrReferences["multi"] = &multiStrFlag
+	strFlag := "test"
+	flagsStrReferences["str"] = &strFlag
+
+	expectedFlags := map[string]interface{}{
+		"bool":  &boolFlag,
+		"int":   &intFlag,
+		"multi": &multiStrFlag,
+		"str":   &strFlag,
+	}
+
+	gotFlags := GetAllFlags()
+
+	require.Equal(t, expectedFlags, gotFlags)
+}
+
+func TestFlags_InitJSONFlags(t *testing.T) {
+	mockCmd := &cobra.Command{
+		Use:   "mock",
+		Short: "Mock cmd",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			return nil
+		},
+	}
+
+	tests := []struct {
+		name                    string
+		cmd                     *cobra.Command
+		flagsListContent        string
+		persintentFlag          bool
+		supportedPlatforms      []string
+		supportedCloudProviders []string
+		wantErr                 bool
+	}{
+		{
+			name: "should initialize flags without error",
+			cmd:  mockCmd,
+			flagsListContent: `{"log-level": {
+				"flagType": "str",
+				"shorthandFlag": "",
+				"defaultValue": "INFO",
+				"usage": "determines log level (${supportedLogLevels})",
+				"validation": "validateStrEnum"
+			},"preview-lines": {
+				"flagType": "int",
+				"shorthandFlag": "",
+				"defaultValue": "3",
+				"usage": "number of lines to be display in CLI results (min: 1, max: 30)"
+			},"queries-path": {
+				"flagType": "multiStr",
+				"shorthandFlag": "q",
+				"defaultValue": "./assets/queries",
+				"usage": "paths to directory with queries"
+			},"verbose": {
+				"flagType": "bool",
+				"shorthandFlag": "v",
+				"defaultValue": "false",
+				"usage": "write logs to stdout too (mutually exclusive with silent)"
+			},"disable-cis-descriptions": {
+				"flagType": "bool",
+				"shorthandFlag": "",
+				"defaultValue": "false",
+				"usage": "disable request for CIS descriptions and use default vulnerability descriptions",
+				"hidden": true,
+				"deprecated": true,
+				"deprecatedInfo": "use --disable-full-descriptions instead"
+			}}`,
+			persintentFlag:          true,
+			supportedPlatforms:      []string{"terraform"},
+			supportedCloudProviders: []string{"aws"},
+			wantErr:                 false,
+		},
+		{
+			name: "should throw error due to wrong json marshal on flagListContent",
+			cmd:  mockCmd,
+			flagsListContent: `"verbose": {
+				"flagType": "bool",
+				"shorthandFlag": "v",
+				"defaultValue": "false",
+				"usage": "write logs to stdout too (mutually exclusive with silent)"
+			}`,
+			persintentFlag:          true,
+			supportedPlatforms:      []string{"terraform"},
+			supportedCloudProviders: []string{"aws"},
+			wantErr:                 true,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			got := InitJSONFlags(test.cmd, test.flagsListContent, test.persintentFlag, test.supportedCloudProviders, test.supportedCloudProviders)
+			if !test.wantErr {
+				require.NoError(t, got)
+			} else {
+				require.Error(t, got)
+			}
+		})
+	}
+}
 
 func TestFlags_GetStrFlag(t *testing.T) {
 	tests := []struct {
