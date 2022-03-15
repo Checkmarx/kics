849023c7a13970e857b49e55f548103f747c0a15
cxMiguelSilva <100352574+cxMiguelSilva@users.noreply.github.com>
2022-03-15 17:21:15 +0000

    feat(query): Added Alicloud RDS Instance TDE Status Disabled for Terraform (#4951)
    * + RDS Instance TDE Status Disabled
    
    * typos corrections
    
    * another typo correction

---
+++ assets/queries/terraform/alicloud/rds_instance_tde_status_disable/test/positive4.tf
@@ -0,0 +1,7 @@
+resource "alicloud_db_instance" "default" {
+    engine = "SQLServer"
+    engine_version = "2016_ent_ha"
+    db_instance_class = "rds.mysql.t1.small"
+    db_instance_storage = "10"
+    parameters = []
+}
