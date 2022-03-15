9c5afb75139b8f0ac496702a7c4b38816b422ecc
cxMiguelSilva <100352574+cxMiguelSilva@users.noreply.github.com>
2022-03-15 19:23:54 +0000

    feat(query): Added Alicloud RDS Instance Retention Period Not Recommended for Terraform (#4993)
    * + RDS Instance Retention Period Not Recommended
    
    * change positive results

---
+++ assets/queries/terraform/alicloud/rds_instance_retention_not_recommended/test/positive4.tf
@@ -0,0 +1,18 @@
+resource "alicloud_db_instance" "default" {
+    engine = "MySQL"
+    engine_version = "5.6"
+    db_instance_class = "rds.mysql.t1.small"
+    db_instance_storage = "10"
+    sql_collector_status = "Enabled"
+    sql_collector_config_value = 30
+    parameters = [{
+        name = "innodb_large_prefix"
+        value = "ON"
+    },{
+        name = "connect_timeout"
+        value = "50"
+    },{
+        name = "log_connections"
+        value = "ON"
+    }]
+}
