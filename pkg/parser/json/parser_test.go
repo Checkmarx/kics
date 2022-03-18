81b388864c6d4ea96004212bcc000cdc99ceea80
Rafaela Soares <rafaela.soares@checkmarx.com>
2022-03-18 09:19:14 +0000

    update(json parser): added Kubernetes to JSON parser (#5013)

--- pkg/parser/json/parser_test.go
+++ pkg/parser/json/parser_test.go
@@ -30,7 +30,7 @@ func TestParser_SupportedExtensions(t *testing.T) {
 // TestParser_SupportedExtensions tests the functions [SupportedTypes()] and all the methods called by them
 func TestParser_SupportedTypes(t *testing.T) {
 	p := &Parser{}
-	require.Equal(t, []string{"CloudFormation", "OpenAPI", "AzureResourceManager", "Terraform"}, p.SupportedTypes())
+	require.Equal(t, []string{"CloudFormation", "OpenAPI", "AzureResourceManager", "Terraform", "Kubernetes"}, p.SupportedTypes())
 }
 
 // TestParser_Parse tests the functions [Parse()] and all the methods called by them
