e1003b573e0e46e52f1b221135a7d67eda7a9320
cxMiguelSilva <miguel.dasilva@checkmarx.com>
2022-04-26 17:33:51 +0100

    update policies to check for encrypt_type

--- assets/queries/terraform/alicloud/nas_file_system_without_kms/test/negative.tf
+++ assets/queries/terraform/alicloud/nas_file_system_without_kms/test/negative.tf
@@ -2,5 +2,6 @@ resource "alicloud_nas_file_system" "foo" {
   protocol_type = "NFS"
   storage_type  = "Performance"
   description   = "tf-testAccNasConfig"
-  encrypt_type  = "1"
+  encrypt_type  = "2"
+  kms_key_id = "1234abcd-12ab-34cd-56ef-1234567890ab"
 }
--- assets/queries/terraform/alicloud/nas_file_system_without_kms/test/negative2.tf
+++
@@ -1,7 +0,0 @@
-resource "alicloud_nas_file_system" "foo" {
-  protocol_type = "NFS"
-  storage_type  = "Performance"
-  description   = "tf-testAccNasConfig"
-  encrypt_type  = "2"
-  kms_key_id = "1234abcd-12ab-34cd-56ef-1234567890ab"
-}
