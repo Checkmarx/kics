0f8203163b7fd2ef3a02126d4b05c22965f85f85
João Daniel Rufino <97480677+joaorufi@users.noreply.github.com>
2022-04-27 13:51:42 -0300

    fix(queries): Fixed aws unique identifiers from common queries (#5236)
    * separated ids
    
    Signed-off-by: joaorufi <joao.rufino@checkmarx.com>
    
    * added positive41 sample
    
    Signed-off-by: joaorufi <joao.rufino@checkmarx.com>

---
+++ assets/queries/common/passwords_and_secrets/test/positive41.tf
@@ -0,0 +1,28 @@
+resource "aws_instance" "web_host" {
+  # ec2 have plain text secrets in user data
+  ami           = var.ami
+  instance_type = "t2.nano"
+
+  vpc_security_group_ids = [
+  "${aws_security_group.web-node.id}"]
+  subnet_id = aws_subnet.web_subnet.id
+  user_data = <<EOF
+#! /bin/bash
+sudo apt-get update
+sudo apt-get install -y apache2
+sudo systemctl start apache2
+sudo systemctl enable apache2
+export AWS_CONTEXT_CREDENTIAL=ACCAIOSFODNN7EXAMAAA
+export AWS_CERTIFICATE=ASCAIOSFODNN7EXAMAAA
+export AWS_DEFAULT_REGION=us-west-2
+echo "<h1>Deployed via Terraform</h1>" | sudo tee /var/www/html/index.html
+EOF
+  tags = merge({
+    Name = "${local.resource_prefix.value}-ec2"
+    }, {
+    git_last_modified_by = "felipe.avelar@checkmarx.com"
+    git_modifiers        = "felipe.avelar"
+    git_org              = "checkmarx"
+    git_repo             = "kics"
+  })
+}
