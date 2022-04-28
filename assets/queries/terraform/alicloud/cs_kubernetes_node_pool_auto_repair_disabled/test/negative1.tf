4b56555797dadfaecbf8330c40c5caa01835a9de
André Felicidade <andre.felicidade@checkmarx.com>
2022-04-28 12:10:04 +0100

    Query CS Kubernetes Node Pool Auto Repair Disabled for Alicloud Terraform

---
+++ assets/queries/terraform/alicloud/cs_kubernetes_node_pool_auto_repair_disabled/test/negative1.tf
@@ -0,0 +1,23 @@
+resource "alicloud_cs_kubernetes_node_pool" "default1" {
+  name                 = var.name
+  cluster_id           = alicloud_cs_managed_kubernetes.default.0.id
+  vswitch_ids          = [alicloud_vswitch.default.id]
+  instance_types       = [data.alicloud_instance_types.default.instance_types.0.id]
+  system_disk_category = "cloud_efficiency"
+  system_disk_size     = 40
+
+  # only key_name is supported in the management node pool
+  key_name = alicloud_key_pair.default.key_name
+
+  # you need to specify the number of nodes in the node pool, which can be zero
+  desired_size = 1
+
+  # management node pool configuration.
+  management {
+    auto_repair     = true
+    auto_upgrade    = true
+    surge           = 1
+    max_unavailable = 1
+  }
+
+}
