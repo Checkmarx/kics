f707cf151ea3bcb852b00630f36e0a879eb7178a
cxMiguelSilva <miguel.dasilva@checkmarx.com>
2022-04-28 12:18:19 +0100

    + Launch Template Is Not Encrypted

---
+++ assets/queries/terraform/alicloud/launch_template_is_not_encrypted/query.rego
@@ -0,0 +1,31 @@
+package Cx
+
+import data.generic.common as common_lib
+
+CxPolicy[result] {
+	resource := input.document[i].resource.alicloud_launch_template[name]
+	resource.encrypted == false
+
+	result := {
+		"documentId": input.document[i].id,
+		"searchKey": sprintf("alicloud_launch_template[%s].encrypted", [name]),
+		"issueType": "IncorrectValue",
+		"keyExpectedValue": sprintf("alicloud_launch_template[%s].encrypted is true", [name]),
+		"keyActualValue": sprintf("alicloud_launch_template[%s].encrypted is false", [name]),
+		"searchLine": common_lib.build_search_line(["resource", "alicloud_launch_template", name, "encrypted"], []),
+	}
+}
+
+CxPolicy[result] {
+	resource := input.document[i].resource.alicloud_launch_template[name]
+	not common_lib.valid_key(resource, "encrypted")
+
+	result := {
+		"documentId": input.document[i].id,
+		"searchKey": sprintf("alicloud_launch_template[%s]", [name]),
+		"issueType": "IncorrectValue",
+		"keyExpectedValue": sprintf("alicloud_launch_template[%s] is true", [name]),
+		"keyActualValue": sprintf("alicloud_launch_template[%s] is false", [name]),
+		"searchLine": common_lib.build_search_line(["resource", "alicloud_launch_template", name], []),
+	}
+}
---
+++ assets/queries/terraform/alicloud/launch_template_is_not_encrypted/test/positive1.tf
@@ -0,0 +1,57 @@
+data "alicloud_images" "images" {
+  owners = "system"
+}
+
+data "alicloud_instances" "instances" {
+}
+
+resource "alicloud_launch_template" "templatepos1" {
+  name                          = "tf-test-template"
+  description                   = "test1"
+  image_id                      = data.alicloud_images.images.images[0].id
+  host_name                     = "tf-test-host"
+  instance_charge_type          = "PrePaid"
+  instance_name                 = "tf-instance-name"
+  instance_type                 = data.alicloud_instances.instances.instances[0].instance_type
+  internet_charge_type          = "PayByBandwidth"
+  internet_max_bandwidth_in     = 5
+  internet_max_bandwidth_out    = 0
+  io_optimized                  = "none"
+  key_pair_name                 = "test-key-pair"
+  ram_role_name                 = "xxxxx"
+  network_type                  = "vpc"
+  security_enhancement_strategy = "Active"
+  spot_price_limit              = 5
+  spot_strategy                 = "SpotWithPriceLimit"
+  security_group_id             = "sg-zxcvj0lasdf102350asdf9a"
+  system_disk_category          = "cloud_ssd"
+  system_disk_description       = "test disk"
+  system_disk_name              = "hello"
+  system_disk_size              = 40
+  resource_group_id             = "rg-zkdfjahg9zxncv0"
+  userdata                      = "xxxxxxxxxxxxxx"
+  vswitch_id                    = "sw-ljkngaksdjfj0nnasdf"
+  vpc_id                        = "vpc-asdfnbg0as8dfk1nb2"
+  zone_id                       = "beijing-a"
+  encrypted                     = false
+
+  tags = {
+    tag1 = "hello"
+    tag2 = "world"
+  }
+  network_interfaces {
+    name              = "eth0"
+    description       = "hello1"
+    primary_ip        = "10.0.0.2"
+    security_group_id = "xxxx"
+    vswitch_id        = "xxxxxxx"
+  }
+  data_disks {
+    name        = "disk1"
+    description = "test1"
+  }
+  data_disks {
+    name        = "disk2"
+    description = "test2"
+  }
+}
