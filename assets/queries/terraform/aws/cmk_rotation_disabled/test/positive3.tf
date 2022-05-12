19e2855386fdc4bf0957ebf1da267ee3eb0738a6
ran.lupovich <ran.lupovich@gmail.com>
2022-05-13 01:29:28 +0300

    fix support for AWS KMS in asymmetric keys - do not support automation key rotation

--- assets/queries/terraform/aws/cmk_rotation_disabled/test/positive3.tf
+++ assets/queries/terraform/aws/cmk_rotation_disabled/test/positive3.tf
@@ -1,6 +1,6 @@
-resource "aws_kms_key" positive3 {
-  description         = "KMS key 4"
-  is_enabled          = true
+resource "aws_kms_key" "positive3" {
+  description              = "KMS key 4"
+  is_enabled               = true
   customer_master_key_spec = "SYMMETRIC_DEFAULT"
-  enable_key_rotation = false
+  enable_key_rotation      = false
 }
