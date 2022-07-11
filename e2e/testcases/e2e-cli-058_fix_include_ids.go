0c09dd877f1d79eda23341d34082b1f39ad4a441
rafaela-soares <rafaela.soares@checkmarx.com>
2022-07-11 08:28:41 +0100

    fixing E2E

--- e2e/testcases/e2e-cli-058_fix_include_ids.go
+++ e2e/testcases/e2e-cli-058_fix_include_ids.go
@@ -1,48 +1,19 @@
 package testcases
 
 import (
-	"os"
-	"path/filepath"
 	"regexp"
-
-	"github.com/Checkmarx/kics/pkg/remediation"
-	"github.com/Checkmarx/kics/pkg/utils"
-	"github.com/rs/zerolog/log"
 )
 
-// E2E-CLI-058 - Kics fix command with include-ids flag
-// should fix the recomendations pointed in include-ids flag
+// E2E-CLI-057 - Kics fix command
+// should fix all remediation found
 func init() { // nolint
-	cwd, err := os.Getwd()
-
-	if err != nil {
-		log.Error().Msgf("failed to get wd: %s", err)
-	}
-
-	tmpFolderPath := filepath.Join(cwd, "fixtures", "tmp-kics-ar")
-
-	if err := os.MkdirAll(tmpFolderPath, os.ModePerm); err != nil {
-		log.Error().Msgf("failed to mkdir: %s", err)
-	}
-
-	filePathCopyFrom := filepath.Join(cwd, "fixtures", "samples", "kics-auto-remediation", "terraform.tf")
-
-	tmpFilePath := filepath.Join(cwd, "fixtures", "tmp-kics-ar", "temporary-remediation-"+utils.NextRandom()+filepath.Ext(filePathCopyFrom))
-
-	jsonPath := tmpFolderPath
-
-	// create a temporary file with the same content as filePathCopyFrom
-	tmpFile := remediation.CreateTempFile(filePathCopyFrom, tmpFilePath)
-
-	// create JSON results with remediation
-	generateReport(tmpFile, jsonPath)
-	pathResults := filepath.Join(jsonPath, "results.json")
+	generateResults("results-fix-include-ids")
 
 	testSample := TestCase{
 		Name: "should fix the recomendations pointed in include-ids flag [E2E-CLI-058]",
 		Args: args{
 			Args: []cmdArgs{
-				[]string{"fix", "--results", pathResults,
+				[]string{"fix", "--results", "/path/e2e/fixtures/tmp-kics-ar/results-fix-include-ids.json",
 					"--include-ids", "f282fa13cf5e4ffd4bbb0ee2059f8d0240edcd2ca54b3bb71633145d961de5ce," +
 						"87abbee5d0ec977ba193371c702dca2c040ea902d2e606806a63b66119ff89bc",
 					"-v"},
