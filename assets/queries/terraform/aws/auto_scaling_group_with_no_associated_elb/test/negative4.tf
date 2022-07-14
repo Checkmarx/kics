c11194a05f0ce728e031a0a847a4cbf3e29aa1e4
cxMiguelSilva <miguel.dasilva@checkmarx.com>
2022-07-14 09:24:21 +0100

    change code samples

--- assets/queries/terraform/aws/auto_scaling_group_with_no_associated_elb/test/negative4.tf
+++ assets/queries/terraform/aws/auto_scaling_group_with_no_associated_elb/test/negative4.tf
@@ -1,11 +1,11 @@
-resource "aws_autoscaling_group" "my_asg" {
-  name_prefix          = format("%s-", var.name)
-  vpc_zone_identifier  = var.private_zone_identifiers
-  launch_configuration = aws_launch_configuration.config.name
-  target_group_arns    = var.target_group_arns
+resource "aws_autoscaling_group" "foo" {
+  name_prefix          = "bar-"
+  vpc_zone_identifier  = ["subnet-abcd1234", "subnet-1a2b3c4d"]
+  launch_configuration = aws_launch_configuration.foobar.name
+  target_group_arns    = ["bar", "baz", "qux"]
   min_size             = 1
-  max_size             = 2
-  desired_capacity     = 1
+  max_size             = 3
+  desired_capacity     = 2
   instance_refresh {
     strategy = "Rolling"
     preferences {
