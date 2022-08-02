c24cfd8a3cfed63df56349b5062e1c1e0f4117c4
Sebastian Gumprich <rndmh3ro@users.noreply.github.com>
2022-08-02 11:08:56 +0000

    fix(query): azure aks policies addon var changed (#5661)
    * fix(query): azure aks policies addon var changed
    
    * fix(query): add more checks for azure policy detection
    
    * fix(query): add more tests for azure policy detection

--- assets/queries/terraform/azure/aks_uses_azure_policies_addon_disabled/test/negative2.tf
+++ assets/queries/terraform/azure/aks_uses_azure_policies_addon_disabled/test/negative2.tf
@@ -1,8 +1,8 @@
-resource "azurerm_kubernetes_cluster" "positive3" {
+resource "azurerm_kubernetes_cluster" "negative" {
   name                = "example-aks1"
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
   dns_prefix          = "exampleaks1"
 
-  addon_profile {}
+  azure_policy_enabled = true
 }
