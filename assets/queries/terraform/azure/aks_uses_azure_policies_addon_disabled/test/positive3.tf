e27cf7720c25841086ce9c9079be520cf47bbee1
Sebastian Gumprich <sebastian.gumprich@t-systems.com>
2022-08-02 12:35:10 +0200

    fix(query): add more tests for azure policy detection

---
+++ assets/queries/terraform/azure/aks_uses_azure_policies_addon_disabled/test/positive3.tf
@@ -0,0 +1,8 @@
+resource "azurerm_kubernetes_cluster" "positive3" {
+  name                = "example-aks1"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  dns_prefix          = "exampleaks1"
+
+  addon_profile {}
+}
