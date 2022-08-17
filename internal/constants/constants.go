6e6e8fdd87f8476d2f5483518a080dacacfb7ba4
gabriel-cx <gabriel.silva@checkmarx.com>
2022-08-17 16:40:06 +0100

    update version

--- internal/constants/constants.go
+++ internal/constants/constants.go
@@ -9,7 +9,7 @@ import (
 
 var (
 	// Version - current KICS version
-	Version = "1.5.13"
+	Version = "1.5.14"
 	// SCMCommit - Source control management commit identifier
 	SCMCommit = "NOCOMMIT"
 	// SentryDSN - sentry DSN, unset for disabling
