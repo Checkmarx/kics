b6ab48448e86b978b43e7de56f334d7e53a5059c
cxMiguelSilva <miguel.dasilva@checkmarx.com>
2022-09-20 17:35:13 +0100

    add test

--- pkg/scan/post_scan_test.go
+++ pkg/scan/post_scan_test.go
@@ -1,11 +1,14 @@
 package scan
 
 import (
+	"path/filepath"
 	"testing"
 	"time"
 
 	"github.com/Checkmarx/kics/internal/tracker"
 	"github.com/Checkmarx/kics/pkg/model"
+	"github.com/Checkmarx/kics/pkg/printer"
+	"github.com/Checkmarx/kics/pkg/progress"
 	"github.com/stretchr/testify/require"
 )
 
@@ -113,6 +116,218 @@ func Test_GetSummary(t *testing.T) {
 			require.Equal(t, tt.expectedResult.Counters, v.Counters)
 			require.Equal(t, tt.expectedResult.SeveritySummary, v.SeveritySummary)
 			require.ElementsMatch(t, tt.expectedResult.ScannedPaths, tt.pathParameters.ScannedPaths)
+		})
+	}
+}
+
+func Test_PrintOuput(t *testing.T) {
+
+	tests := []struct {
+		name          string
+		outputPath    string
+		filename      string
+		body          interface{}
+		formats       []string
+		proBarBuilder progress.PbBuilder
+	}{
+		{
+			name:       "print result without output file",
+			outputPath: "",
+			filename:   "results",
+			body: model.Summary{
+				Version: "",
+				LatestVersion: model.Version{
+					Latest:           true,
+					LatestVersionTag: "",
+				},
+				Counters: model.Counters{
+					ScannedFiles:           10,
+					ScannedFilesLines:      76,
+					ParsedFiles:            10,
+					ParsedFilesLines:       76,
+					FailedToScanFiles:      0,
+					TotalQueries:           1040,
+					FailedToExecuteQueries: 0,
+					FailedSimilarityID:     0,
+				},
+				SeveritySummary: model.SeveritySummary{
+					ScanID:            "console",
+					SeverityCounters:  map[model.Severity]int{},
+					TotalCounter:      14,
+					TotalBOMResources: 0,
+				},
+				Times: model.Times{},
+				ScannedPaths: []string{
+					"./assets/queries/terraform/alicloud/action_trail_logging_all_regions_disabled",
+				},
+				Queries: model.QueryResultSlice{},
+				Bom:     model.QueryResultSlice{},
+				FilePaths: map[string]string{
+					"assets/queries/terraform/alicloud/action_trail_logging_all_regions_disabled/test/positive7.tf": "/home/miguel/cx/kics/assets/queries/terraform/alicloud/action_trail_logging_all_regions_disabled/test/positive7.tf",
+					"assets/queries/terraform/alicloud/action_trail_logging_all_regions_disabled/test/positive5.tf": "/home/miguel/cx/kics/assets/queries/terraform/alicloud/action_trail_logging_all_regions_disabled/test/positive5.tf",
+					"assets/queries/terraform/alicloud/action_trail_logging_all_regions_disabled/test/positive6.tf": "/home/miguel/cx/kics/assets/queries/terraform/alicloud/action_trail_logging_all_regions_disabled/test/positive6.tf",
+					"assets/queries/terraform/alicloud/action_trail_logging_all_regions_disabled/test/positive3.tf": "/home/miguel/cx/kics/assets/queries/terraform/alicloud/action_trail_logging_all_regions_disabled/test/positive3.tf",
+					"assets/queries/terraform/alicloud/action_trail_logging_all_regions_disabled/test/positive8.tf": "/home/miguel/cx/kics/assets/queries/terraform/alicloud/action_trail_logging_all_regions_disabled/test/positive8.tf",
+					"assets/queries/terraform/alicloud/action_trail_logging_all_regions_disabled/test/positive1.tf": "/home/miguel/cx/kics/assets/queries/terraform/alicloud/action_trail_logging_all_regions_disabled/test/positive1.tf",
+					"assets/queries/terraform/alicloud/action_trail_logging_all_regions_disabled/test/positive4.tf": "/home/miguel/cx/kics/assets/queries/terraform/alicloud/action_trail_logging_all_regions_disabled/test/positive4.tf",
+					"assets/queries/terraform/alicloud/action_trail_logging_all_regions_disabled/test/positive2.tf": "/home/miguel/cx/kics/assets/queries/terraform/alicloud/action_trail_logging_all_regions_disabled/test/positive2.tf",
+					"assets/queries/terraform/alicloud/action_trail_logging_all_regions_disabled/test/positive9.tf": "/home/miguel/cx/kics/assets/queries/terraform/alicloud/action_trail_logging_all_regions_disabled/test/positive9.tf",
+				},
+			},
+			formats: []string{
+				"json",
+			},
+			proBarBuilder: *progress.InitializePbBuilder(true, false, true),
+		},
+		{
+			name:       "print with output path",
+			outputPath: filepath.Join("..", "..", "res"),
+			filename:   "results",
+			body: model.Summary{
+				Version: "",
+				LatestVersion: model.Version{
+					Latest:           true,
+					LatestVersionTag: "",
+				},
+				Counters: model.Counters{
+					ScannedFiles:           10,
+					ScannedFilesLines:      76,
+					ParsedFiles:            10,
+					ParsedFilesLines:       76,
+					FailedToScanFiles:      0,
+					TotalQueries:           1040,
+					FailedToExecuteQueries: 0,
+					FailedSimilarityID:     0,
+				},
+				SeveritySummary: model.SeveritySummary{
+					ScanID:            "console",
+					SeverityCounters:  map[model.Severity]int{},
+					TotalCounter:      14,
+					TotalBOMResources: 0,
+				},
+				Times: model.Times{},
+				ScannedPaths: []string{
+					"./assets/queries/terraform/alicloud/action_trail_logging_all_regions_disabled",
+				},
+				Queries: model.QueryResultSlice{},
+				Bom:     model.QueryResultSlice{},
+				FilePaths: map[string]string{
+					"assets/queries/terraform/alicloud/action_trail_logging_all_regions_disabled/test/positive7.tf": "/home/miguel/cx/kics/assets/queries/terraform/alicloud/action_trail_logging_all_regions_disabled/test/positive7.tf",
+					"assets/queries/terraform/alicloud/action_trail_logging_all_regions_disabled/test/positive5.tf": "/home/miguel/cx/kics/assets/queries/terraform/alicloud/action_trail_logging_all_regions_disabled/test/positive5.tf",
+					"assets/queries/terraform/alicloud/action_trail_logging_all_regions_disabled/test/positive6.tf": "/home/miguel/cx/kics/assets/queries/terraform/alicloud/action_trail_logging_all_regions_disabled/test/positive6.tf",
+					"assets/queries/terraform/alicloud/action_trail_logging_all_regions_disabled/test/positive3.tf": "/home/miguel/cx/kics/assets/queries/terraform/alicloud/action_trail_logging_all_regions_disabled/test/positive3.tf",
+					"assets/queries/terraform/alicloud/action_trail_logging_all_regions_disabled/test/positive8.tf": "/home/miguel/cx/kics/assets/queries/terraform/alicloud/action_trail_logging_all_regions_disabled/test/positive8.tf",
+					"assets/queries/terraform/alicloud/action_trail_logging_all_regions_disabled/test/positive1.tf": "/home/miguel/cx/kics/assets/queries/terraform/alicloud/action_trail_logging_all_regions_disabled/test/positive1.tf",
+					"assets/queries/terraform/alicloud/action_trail_logging_all_regions_disabled/test/positive4.tf": "/home/miguel/cx/kics/assets/queries/terraform/alicloud/action_trail_logging_all_regions_disabled/test/positive4.tf",
+					"assets/queries/terraform/alicloud/action_trail_logging_all_regions_disabled/test/positive2.tf": "/home/miguel/cx/kics/assets/queries/terraform/alicloud/action_trail_logging_all_regions_disabled/test/positive2.tf",
+					"assets/queries/terraform/alicloud/action_trail_logging_all_regions_disabled/test/positive9.tf": "/home/miguel/cx/kics/assets/queries/terraform/alicloud/action_trail_logging_all_regions_disabled/test/positive9.tf",
+				},
+			},
+			formats: []string{
+				"json",
+			},
+			proBarBuilder: *progress.InitializePbBuilder(true, false, true),
+		},
+		{
+			name:       "print with empty formats",
+			outputPath: filepath.Join("..", "..", "res"),
+			filename:   "results",
+			body: model.Summary{
+				Version: "",
+				LatestVersion: model.Version{
+					Latest:           true,
+					LatestVersionTag: "",
+				},
+				Counters: model.Counters{
+					ScannedFiles:           10,
+					ScannedFilesLines:      76,
+					ParsedFiles:            10,
+					ParsedFilesLines:       76,
+					FailedToScanFiles:      0,
+					TotalQueries:           1040,
+					FailedToExecuteQueries: 0,
+					FailedSimilarityID:     0,
+				},
+				SeveritySummary: model.SeveritySummary{
+					ScanID:            "console",
+					SeverityCounters:  map[model.Severity]int{},
+					TotalCounter:      14,
+					TotalBOMResources: 0,
+				},
+				Times: model.Times{},
+				ScannedPaths: []string{
+					"./assets/queries/terraform/alicloud/action_trail_logging_all_regions_disabled",
+				},
+				Queries: model.QueryResultSlice{},
+				Bom:     model.QueryResultSlice{},
+				FilePaths: map[string]string{
+					"assets/queries/terraform/alicloud/action_trail_logging_all_regions_disabled/test/positive7.tf": "/home/miguel/cx/kics/assets/queries/terraform/alicloud/action_trail_logging_all_regions_disabled/test/positive7.tf",
+					"assets/queries/terraform/alicloud/action_trail_logging_all_regions_disabled/test/positive5.tf": "/home/miguel/cx/kics/assets/queries/terraform/alicloud/action_trail_logging_all_regions_disabled/test/positive5.tf",
+					"assets/queries/terraform/alicloud/action_trail_logging_all_regions_disabled/test/positive6.tf": "/home/miguel/cx/kics/assets/queries/terraform/alicloud/action_trail_logging_all_regions_disabled/test/positive6.tf",
+					"assets/queries/terraform/alicloud/action_trail_logging_all_regions_disabled/test/positive3.tf": "/home/miguel/cx/kics/assets/queries/terraform/alicloud/action_trail_logging_all_regions_disabled/test/positive3.tf",
+					"assets/queries/terraform/alicloud/action_trail_logging_all_regions_disabled/test/positive8.tf": "/home/miguel/cx/kics/assets/queries/terraform/alicloud/action_trail_logging_all_regions_disabled/test/positive8.tf",
+					"assets/queries/terraform/alicloud/action_trail_logging_all_regions_disabled/test/positive1.tf": "/home/miguel/cx/kics/assets/queries/terraform/alicloud/action_trail_logging_all_regions_disabled/test/positive1.tf",
+					"assets/queries/terraform/alicloud/action_trail_logging_all_regions_disabled/test/positive4.tf": "/home/miguel/cx/kics/assets/queries/terraform/alicloud/action_trail_logging_all_regions_disabled/test/positive4.tf",
+					"assets/queries/terraform/alicloud/action_trail_logging_all_regions_disabled/test/positive2.tf": "/home/miguel/cx/kics/assets/queries/terraform/alicloud/action_trail_logging_all_regions_disabled/test/positive2.tf",
+					"assets/queries/terraform/alicloud/action_trail_logging_all_regions_disabled/test/positive9.tf": "/home/miguel/cx/kics/assets/queries/terraform/alicloud/action_trail_logging_all_regions_disabled/test/positive9.tf",
+				},
+			},
+			formats:       []string{},
+			proBarBuilder: *progress.InitializePbBuilder(true, false, true),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			err := printOutput(tt.outputPath, tt.filename, tt.body, tt.formats, tt.proBarBuilder)
+			require.NoError(t, err)
+		})
+	}
+}
+
+func Test_resolveOutputs(t *testing.T) {
+	tests := []struct {
+		name        string
+		scanResults *Results
+		scanParams  Parameters
+		tracker     tracker.CITracker
+		sevSummary  model.SeveritySummary
+	}{
+		{
+			name:        "empty results",
+			scanResults: nil,
+			scanParams: Parameters{
+				Path: []string{
+					filepath.Join("..", "..", "lib"),
+				},
+			},
+			tracker: tracker.CITracker{
+				ExecutingQueries:   0,
+				ExecutedQueries:    0,
+				FoundFiles:         0,
+				FailedSimilarityID: 0,
+				LoadedQueries:      0,
+				ParsedFiles:        0,
+				ScanSecrets:        0,
+				ScanPaths:          0,
+				FoundCountLines:    0,
+				ParsedCountLines:   0,
+				Version: model.Version{
+					Latest:           true,
+					LatestVersionTag: "",
+				},
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			c := Client{}
+			c.Tracker = &tt.tracker
+			c.ScanParams = &tt.scanParams
+			c.ProBarBuilder = progress.InitializePbBuilder(true, false, true)
+			c.Printer = printer.NewPrinter(true)
+			err := c.postScan(tt.scanResults)
+			require.NoError(t, err)
 		})
 	}
 }
