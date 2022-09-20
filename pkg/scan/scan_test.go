c3b9dfcca05c6e5e2a8661293fd2ca85d8243ee9
cxMiguelSilva <miguel.dasilva@checkmarx.com>
2022-09-20 12:23:13 +0100

    revert filesystem go

--- pkg/scan/scan_test.go
+++ pkg/scan/scan_test.go
@@ -1,12 +1,10 @@
 package scan
 
 import (
-	"os"
 	"path/filepath"
 	"testing"
 
 	"github.com/Checkmarx/kics/assets"
-	"github.com/Checkmarx/kics/pkg/engine/provider"
 	"github.com/Checkmarx/kics/pkg/engine/source"
 	"github.com/stretchr/testify/require"
 )
@@ -121,61 +119,6 @@ func Test_CreateQueryFilter(t *testing.T) {
 			v := c.createQueryFilter()
 
 			require.Equal(t, tt.expectedOutput, *v)
-		})
-	}
-}
-
-func Test_GetFileSystemSourceProvider(t *testing.T) {
-	tests := []struct {
-		name           string
-		paths          []string
-		scanParams     Parameters
-		expectedOutput provider.FileSystemSourceProvider
-		expectedError  bool
-	}{
-		{
-			name: "get FileSystemSourceProvider",
-			scanParams: Parameters{
-				PayloadPath: "",
-				ExcludePaths: []string{
-					filepath.Join("..", "..", "assets", "queries", "terraform", "alicloud", "action_trail_logging_all_regions_disabled", "test", "positive_expected_result.json"),
-					filepath.Join("..", "..", "assets", "queries", "terraform", "alicloud", "action_trail_logging_all_regions_disabled", "metadata.json"),
-					filepath.Join("..", "..", "assets", "queries", "terraform", "alicloud", "actiontrail_trail_oss_bucket_is_publicly_accessible", "metadata.json"),
-					filepath.Join("..", "..", "assets", "queries", "terraform", "alicloud", "actiontrail_trail_oss_bucket_is_publicly_accessible", "test", "positive_expected_result.json"),
-				},
-			},
-			paths: []string{
-				filepath.Join("..", "..", "assets", "queries", "terraform", "alicloud", "action_trail_logging_all_regions_disabled"),
-				filepath.Join("..", "..", "assets", "queries", "terraform", "alicloud", "actiontrail_trail_oss_bucket_is_publicly_accessible"),
-			},
-			expectedOutput: provider.FileSystemSourceProvider{
-				Paths: []string{
-					filepath.Join("..", "..", "assets", "queries", "terraform", "alicloud", "action_trail_logging_all_regions_disabled"),
-					filepath.Join("..", "..", "assets", "queries", "terraform", "alicloud", "actiontrail_trail_oss_bucket_is_publicly_accessible"),
-				},
-				Excludes: map[string][]os.FileInfo{
-					"positive_expected_result.json": {},
-					"metadata.json":                 {},
-				},
-			},
-			expectedError: false,
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			c := &Client{}
-			c.ScanParams = &tt.scanParams
-			v, err := c.getFileSystemSourceProvider(tt.paths)
-
-			for k := range tt.expectedOutput.Excludes {
-				require.Contains(t, (*v).Excludes, k)
-			}
-			require.ElementsMatch(t, tt.expectedOutput.Paths, (*v).Paths)
-			if tt.expectedError {
-				require.Error(t, err)
-			} else {
-				require.NoError(t, err)
-			}
 		})
 	}
 }
