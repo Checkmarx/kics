c4ad3304389951462f143ab130d0195da32fc50f
cxMiguelSilva <miguel.dasilva@checkmarx.com>
2022-11-29 10:38:18 +0000

    update security query

--- assets/queries/terraform/azure/aks_disk_encryption_set_id_undefined/test/negative.tf
+++ assets/queries/terraform/azure/aks_disk_encryption_set_id_undefined/test/negative.tf
@@ -12,3 +12,18 @@ resource "azurerm_kubernetes_cluster" "negative" {
     vm_size    = "Standard_D2_v2"
   }
 }
+
+
+resource "azurerm_kubernetes_cluster" "negative" {
+  name                = "example-aks1"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  dns_prefix          = "exampleaks1"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_D2_v2"
+    os_disk_type = "Ephemeral"
+  }
+}
