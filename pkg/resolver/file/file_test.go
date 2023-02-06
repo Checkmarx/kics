a500326c0fe9b5e0f142f1a3f20904b20e5f9d6a
cxMiguelSilva <miguel.dasilva@checkmarx.com>
2023-02-06 16:24:30 +0000

    revert change

--- pkg/resolver/file/file_test.go
+++ pkg/resolver/file/file_test.go
@@ -94,9 +94,8 @@ func TestResolver_Resolve(t *testing.T) {
 			if err != nil {
 				t.Fatal(err)
 			}
-			got := r.Resolve(cont, tt.args.path, 0)
 
-			if !reflect.DeepEqual(prepareString(string(got)), prepareString(string(tt.want))) {
+			if got := r.Resolve(cont, tt.args.path, 0); !reflect.DeepEqual(prepareString(string(got)), prepareString(string(tt.want))) {
 				t.Errorf("Resolve() = %v, want = %v", prepareString(string(got)), prepareString(string(tt.want)))
 			}
 		})
