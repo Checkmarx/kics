2954957550bfe3d41186630c48f441970ba99391
cxMiguelSilva <miguel.dasilva@checkmarx.com>
2023-02-24 07:48:19 +0000

    add gke outdated Query

---
+++ assets/queries/terraform/gcp/outdated_gke_version/test/positive.tf
@@ -0,0 +1,46 @@
+#this code is a correct code for which the query should not find any result
+resource "google_container_cluster" "positive1" {
+  name               = "marcellus-wallace"
+  location           = "us-central1-a"
+  initial_node_count = 3
+
+  master_auth {
+    username = ""
+    password = ""
+
+    client_certificate_config {
+      issue_client_certificate = false
+    }
+  }
+
+  timeouts {
+    create = "30m"
+    update = "40m"
+  }
+
+  min_master_version = "1.24"
+}
+
+#this code is a correct code for which the query should not find any result
+resource "google_container_cluster" "positive2" {
+  name               = "marcellus-wallace"
+  location           = "us-central1-a"
+  initial_node_count = 3
+
+  master_auth {
+    username = ""
+    password = ""
+
+    client_certificate_config {
+      issue_client_certificate = false
+    }
+  }
+
+  timeouts {
+    create = "30m"
+    update = "40m"
+  }
+
+  
+  node_version = "1.24"
+}
