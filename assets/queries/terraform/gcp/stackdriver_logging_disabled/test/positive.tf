8a42bb9e2c16813f7f16e457fd04be4436440207
Amy Gleixner <meldaravaniel@users.noreply.github.com>
2023-03-06 09:17:01 -0800

    removes default from positive tests because it's okay now

--- assets/queries/terraform/gcp/stackdriver_logging_disabled/test/positive.tf
+++ assets/queries/terraform/gcp/stackdriver_logging_disabled/test/positive.tf
@@ -3,17 +3,6 @@ resource "google_container_cluster" "positive1" {
   name               = "marcellus-wallace"
   location           = "us-central1-a"
   initial_node_count = 3
-
-  timeouts {
-    create = "30m"
-    update = "40m"
-  }
-}
-
-resource "google_container_cluster" "positive2" {
-  name               = "marcellus-wallace"
-  location           = "us-central1-a"
-  initial_node_count = 3
   logging_service = "none"
 
   timeouts {
@@ -22,7 +11,7 @@ resource "google_container_cluster" "positive2" {
   }
 }
 
-resource "google_container_cluster" "positive3" {
+resource "google_container_cluster" "positive2" {
   name               = "marcellus-wallace"
   location           = "us-central1-a"
   initial_node_count = 3
