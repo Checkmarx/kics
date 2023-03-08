9490ace4b17e48ae4cdf90270f93cdf52bef8a4b
cxMiguelSilva <miguel.dasilva@checkmarx.com>
2023-03-08 17:52:26 +0000

    fix lint

--- pkg/utils/line_counter.go
+++ pkg/utils/line_counter.go
@@ -4,15 +4,22 @@ package utils
 import (
 	"bufio"
 	"os"
+	"path/filepath"
+
+	"github.com/rs/zerolog/log"
 )
 
 // Get file number of lines
 func LineCounter(path string) (int, error) {
-	file, err := os.Open(path)
+	file, err := os.Open(filepath.Clean(path))
 	if err != nil {
 		return 0, err
 	}
-	defer file.Close()
+	defer func() {
+		if err := f.Close(); err != nil {
+			log.Err(err).Msgf("failed to close '%s'", filepath.Clean(path))
+		}
+	}()
 
 	scanner := bufio.NewScanner(file)
 	lineCount := 0
