f9fec80a6c00c4e39e0b73905997f845b1af9050
Brian Bergstrom <brianbergstrom@flywheel.io>
2023-03-23 16:42:01 -0500

    Fix up tests.

--- assets/queries/terraform/gcp/shielded_gke_nodes_disabled/test/positive1.tf
+++ assets/queries/terraform/gcp/shielded_gke_nodes_disabled/test/positive1.tf
@@ -1,6 +1,4 @@
 resource "google_container_cluster" "unset" {
   name                  = "my-gke-cluster"
   location              = "us-central1"
-  remove_default_node_pool = true
-  initial_node_count       = 1
 }
