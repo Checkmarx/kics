f556cf7f5ae729ff1fdc4b35a11fad1b5166b4bf
cx-henriqueAlvelos <henrique.alvelos@checkmarx.com>
2023-05-03 11:47:32 +0100

    new comment

--- pkg/detector/helper.go
+++ pkg/detector/helper.go
@@ -277,6 +277,7 @@ func (d *DefaultDetectLineResponse) DetectCurrentLine(str1, str2 string, recurse
 }
 
 func checkLine(str1, str2 string, distances map[int]int, line string, i int) map[int]int {
+	// Tackle initial spaces that were inducing wrong results
 	regex := regexp.MustCompile(`^\s+`)
 	line = regex.ReplaceAllString(line, "")
 	if str1 != "" && str2 != "" && strings.Contains(line, str1) {
