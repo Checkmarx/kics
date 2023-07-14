12ec59aecf1bc4bd0cb1bcf8bd18695b530aa585
pereiramarco011 <marco.pereira@checkmarx.com>
2023-07-14 15:44:07 +0100

    fix e2e name

--- e2e/testcases/e2e-cli-064_json_file_ref.go
+++ e2e/testcases/e2e-cli-064_json_file_ref.go
@@ -1,11 +1,11 @@
 // Package testcases provides end-to-end (E2E) testing functionality for the application.
 package testcases
 
-// E2E-CLI-064 - KICS  scan json/yaml file with ## in non ref
+// E2E-CLI-064 - KICS  scan with json/yaml file with ## in non ref
 // should perform the scan successfully and return exit code 0
 func init() { //nolint
 	testSample := TestCase{
-		Name: "should perform a valid scan and get the variables using a variables path as a flag [E2E-CLI-063]",
+		Name: "should perform a valid scan with json/yaml file with ## in non ref [E2E-CLI-064]",
 		Args: args{
 			Args: []cmdArgs{
 				[]string{"scan", "-p", "\"/path/e2e/fixtures/samples/swagger\"",
