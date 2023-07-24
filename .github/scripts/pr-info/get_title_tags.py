87d428d74f51bde28626d65262ab5314fd2f7162
Millena Santos <freitasmillena27@gmail.com>
2023-07-24 17:48:04 +0100

    fixed script

--- .github/scripts/pr-info/get_title_tags.py
+++ .github/scripts/pr-info/get_title_tags.py
@@ -6,7 +6,7 @@ def yaml_to_regex(yaml_file):
     with open(yaml_file, 'r') as f:
         data = yaml.safe_load(f)
     regex = '|'.join(data)
-    print(f"Regex: ^({regex})\([a-z]+\): [a-z]")
+    print(f"^({regex})\([a-z]+\): [a-z]")
     
 if __name__ == "__main__":
     yaml_to_regex('.github/pr-title-tags.yaml')
