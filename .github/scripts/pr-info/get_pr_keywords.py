ee639a504735b97864bf6011b198dafc51beed6e
Millena Santos <freitasmillena27@gmail.com>
2023-07-24 18:25:51 +0100

    getting keywords

--- .github/scripts/pr-info/get_pr_keywords.py
+++ .github/scripts/pr-info/get_pr_keywords.py
@@ -2,14 +2,17 @@
 
 import yaml
 
+
 def load_keywords(file):
     with open(file, 'r') as stream:
         try:
             data = yaml.safe_load(stream)
+            # format to bash associative array
             formatted = ' '.join([f'["{k}"]="{v}"' for k, v in data.items()])
-            print(f"({formatted})")
+            print(f"declare -A keywords=({formatted})")
         except yaml.YAMLError as exc:
             print(exc)
+
 
 if __name__ == "__main__":
     load_keywords('.github/keywords.yaml')
