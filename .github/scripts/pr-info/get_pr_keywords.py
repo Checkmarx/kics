0389fefda0ec979468673c6e7bb43ba0638b97a0
Millena Santos <freitasmillena27@gmail.com>
2023-07-24 18:00:44 +0100

    created script for platforms and cloudproviders labels

---
+++ .github/scripts/pr-info/get_pr_keywords.py
@@ -0,0 +1,16 @@
+# gets platforms and cloud providers keywords from keywordd.yaml file and inserts into a map in shell script
+
+import yaml
+
+def load_keywords(file):
+    with open(file, 'r') as stream:
+        try:
+            data = yaml.safe_load(stream)
+            formatted = ' '.join([f'["{k}"]="{v}"' for k, v in data.items()])
+            print(formatted)
+        except yaml.YAMLError as exc:
+            print(exc)
+
+if __name__ == "__main__":
+    load_keywords('.github/keywords.yaml')
+
