a9c186f18d8f13b920d886dcd2cc0339301e7820
asofsilva <ana.silva@checkmarx.com>
2023-07-31 11:53:31 +0100

    addnewfiles

---
+++ .github/scripts/pr-info/get_title_tags.py
@@ -0,0 +1,13 @@
+# gets all tags from pr-title-tags.yaml file and inserts them into the regex
+
+import yaml
+
+def yaml_to_regex(yaml_file):
+    with open(yaml_file, 'r') as f:
+        data = yaml.safe_load(f)
+    regex = '|'.join(data)
+    print(f"^({regex})\([a-z]+\): [a-z]")
+
+
+if __name__ == "__main__":
+    yaml_to_regex('.github/pr-title-tags.yaml')
