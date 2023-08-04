da67101ec3e6d96d65e6414d5acf6939545ad06b
Dimitri GRISARD <dgrisard-externe@bedrockstreaming.com>
2023-08-04 11:44:45 +0200

    feat(terraform/aws): backup delete_after greater than cold_storage

---
+++ assets/queries/terraform/aws/backup_vault_lifecycle_delete_after_badly_setup/test/negative2.tf
@@ -0,0 +1,21 @@
+resource "aws_backup_plan" "negative2" {
+  name = "tf_example_backup_plan"
+
+  rule {
+    rule_name         = "tf_example_backup_rule"
+    target_vault_name = aws_backup_vault.test.name
+    schedule          = "cron(0 12 * * ? *)"
+
+    lifecycle {
+      cold_storage_after = 0
+      delete_after = 90
+    }
+  }
+
+  advanced_backup_setting {
+    backup_options = {
+      WindowsVSS = "enabled"
+    }
+    resource_type = "EC2"
+  }
+}
