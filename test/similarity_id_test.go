70bbb9955f2233d4fc179267ac73cb036a492ce9
Millena Santos <freitasmillena27@gmail.com>
2023-08-23 11:47:44 +0100

    added cicd to e2e tests and docs

--- test/similarity_id_test.go
+++ test/similarity_id_test.go
@@ -317,7 +317,7 @@ func createInspectorAndGetVulnerabilities(ctx context.Context, t testing.TB,
 
 	wg := &sync.WaitGroup{}
 	proBarBuilder := progress.InitializePbBuilder(true, true, true)
-	platforms := []string{"Ansible", "AzureResourceManager", "Buildah", "CloudFormation", "Dockerfile", "GRPC", "Kubernetes", "OpenAPI", "Terraform"}
+	platforms := []string{"Ansible", "AzureResourceManager", "Buildah", "CICD", "CloudFormation", "Dockerfile", "GRPC", "Kubernetes", "OpenAPI", "Terraform"}
 	progressBar := proBarBuilder.BuildCounter("Executing queries: ", inspector.LenQueriesByPlat(platforms), wg, currentQuery)
 	go progressBar.Start()
 
@@ -331,7 +331,7 @@ func createInspectorAndGetVulnerabilities(ctx context.Context, t testing.TB,
 			testParams.sampleContent(t),
 		),
 		[]string{BaseTestsScanPath},
-		[]string{"Ansible", "AzureResourceManager", "Buildah", "CloudFormation", "Dockerfile", "GRPC", "Kubernetes", "OpenAPI", "Terraform"},
+		[]string{"Ansible", "AzureResourceManager", "Buildah", "CICD", "CloudFormation", "Dockerfile", "GRPC", "Kubernetes", "OpenAPI", "Terraform"},
 		currentQuery,
 	)
 
