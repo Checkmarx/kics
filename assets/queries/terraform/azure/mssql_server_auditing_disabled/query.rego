668b1892bfbbd0e75f2c6f8569631a3f2a4cb44c
Millena Santos <freitasmillena27@gmail.com>
2023-09-13 14:12:21 +0100

    added new settings of latest update to the query

--- assets/queries/terraform/azure/mssql_server_auditing_disabled/query.rego
+++ assets/queries/terraform/azure/mssql_server_auditing_disabled/query.rego
@@ -3,14 +3,18 @@ package Cx
 import data.generic.terraform as tf_lib
 
 CxPolicy[result] {
-	resource := input.document[i].resource.azurerm_mssql_server[name]
+	resource := input.document[i].resource
+	
+	server:= resource.azurerm_mssql_server[name]
+	
+	not server.extended_auditing_policy
 
-	not resource.extended_auditing_policy
+	not resource.azurerm_mssql_server_extended_auditing_policy
 
 	result := {
 		"documentId": input.document[i].id,
 		"resourceType": "azurerm_mssql_server",
-		"resourceName": tf_lib.get_resource_name(resource, name),
+		"resourceName": tf_lib.get_resource_name(server, name),
 		"searchKey": sprintf("azurerm_mssql_server[%s]", [name]),
 		"issueType": "MissingAttribute",
 		"keyExpectedValue": sprintf("'azurerm_mssql_server.%s.extended_auditing_policy' should exist", [name]),
---
+++ assets/queries/terraform/azure/mssql_server_auditing_disabled/test/negative2.tf
@@ -0,0 +1,38 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "West Europe"
+}
+
+resource "azurerm_mssql_server" "negative1" {
+    name                         = "mssqlserver"
+    resource_group_name          = azurerm_resource_group.example.name
+    location                     = azurerm_resource_group.example.location
+    version                      = "12.0"
+    administrator_login          = "mradministrator"
+    administrator_login_password = "thisIsDog11"
+
+    extended_auditing_policy {
+       storage_endpoint           = azurerm_storage_account.example.primary_blob_endpoint
+       storage_account_access_key = azurerm_storage_account.example.primary_access_key
+       storage_account_access_key_is_secondary = true
+       retention_in_days                       = 90
+    }
+}
+
+resource "azurerm_mssql_database" "example" {
+  name      = "example-db"
+  server_id = azurerm_mssql_server.example.id
+}
+
+resource "azurerm_storage_account" "example" {
+  name                     = "examplesa"
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
