3ebc66c8b2f07c9159a81648af3becdb6ee986b6
Artur Ribeiro <artur.ribeiro@checkmarx.com>
2024-01-19 12:37:02 +0000

    fix linting e2e and naming

--- e2e/testcases/e2e-cli-079_cwe_sarif_result.go
+++ e2e/testcases/e2e-cli-079_cwe_sarif_result.go
@@ -1,21 +1,21 @@
 package testcases
 
-// E2E-CLI-077 - KICS  scan
+// E2E-CLI-079 - KICS  scan
 // should perform a scan saving the reports in sarif format, completing the relationships and the taxonomies
 func init() { //nolint
 	testSample := TestCase{
-		Name: "should perform a scan saving the reports in sarif format, completing the relationships and the taxonomies if the CWE has values [E2E-CLI-077]",
+		Name: "should perform a scan saving the reports in sarif format, completing the relationships and the taxonomies [E2E-CLI-079]",
 		Args: args{
 			Args: []cmdArgs{
 				[]string{"scan", "-o", "/path/e2e/output",
-					"--output-name", "E2E_CLI_077_RESULT",
+					"--output-name", "E2E_CLI_079_RESULT",
 					"-p", "\"/path/test/fixtures/test_sarif_cwe_report\"",
 					"--report-formats", "sarif",
 				},
 			},
 			ExpectedResult: []ResultsValidation{
 				{
-					ResultsFile:    "E2E_CLI_077_RESULT",
+					ResultsFile:    "E2E_CLI_079_RESULT",
 					ResultsFormats: []string{"sarif"},
 				},
 			},
