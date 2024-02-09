b346da9548221e4b3367d0874092a7202a6766ce
pereiramarco011 <marco.pereira@checkmarx.com>
2024-02-09 11:24:56 +0000

    add e2e and fix previous

---
+++ e2e/testcases/e2e-cli-083_parallel_scan_6_workers.go
@@ -0,0 +1,27 @@
+package testcases
+
+// E2E-CLI-083 - KICS  scan
+// should perform a scan, finishing successfully and return exit code 0
+func init() { //nolint
+	testSample := TestCase{
+		Name: "should perform a scan and finish successfully [E2E-CLI-083]",
+		Args: args{
+			Args: []cmdArgs{
+				[]string{"scan", "-o", "/path/e2e/output",
+					"--output-name", "E2E_CLI_083_RESULT",
+					"-p", "\"/path/e2e/fixtures/samples/long_terraform.tf\"",
+					"--parallel", "6",
+				},
+			},
+			ExpectedResult: []ResultsValidation{
+				{
+					ResultsFile:    "E2E_CLI_081_RESULT",
+					ResultsFormats: []string{"json"},
+				},
+			},
+		},
+		WantStatus: []int{50},
+	}
+
+	Tests = append(Tests, testSample)
+}
