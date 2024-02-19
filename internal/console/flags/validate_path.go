2052df6e8fb6bdc6fede77b37eb39b35b488c88c
JulioSCX <julio.silva@checkmarx.com>
2024-02-19 12:42:13 +0000

    lint related changes

--- internal/console/flags/validate_path.go
+++ internal/console/flags/validate_path.go
@@ -9,7 +9,7 @@ func validatePath(flagName string) error {
 	regex := regexp.MustCompile(`^(?:\.\.\\|\.\\|\.\.\/|\.\/|\\|\/)?(?:[^<>:"\/\\|?*]+[\\|\/])*[^<>:"\/\\|?*]+$|^[a-zA-Z]:[\\|\/](?:[^<>:"\/\\|?*]+[\\|\/])*[^<>:"\/\\|?*]+$`)
 
 	path := GetStrFlag(flagName)
-	isValid := regex.MatchString(path) || len(path) == 0
+	isValid := regex.MatchString(path) || path == ""
 
 	if !isValid {
 		return errors.New("the directory name contains invalid characters")
