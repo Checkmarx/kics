f9e8849a7cabe59b7ee82cde926399c3a9bed10b
cxMiguelSilva <miguel.dasilva@checkmarx.com>
2024-02-21 10:12:18 +0000

    fix 2e2

--- e2e/testcases/e2e-cli-088_new_severity_metadata_field.go
+++ e2e/testcases/e2e-cli-088_new_severity_metadata_field.go
@@ -1,10 +1,10 @@
 package testcases
 
 // E2E-CLI-088 - KICS scan with new severity metadata field
-// should perform a scan successfully giving results with new severity metadata field and return exit code 60 according to the severity
+// should perform a scan successfully giving results with new severity metadata field and return exit code according to the severity
 func init() { //nolint
-	testSample := TestCase{
-		Name: "should perform a scans successfully giving results with new severity and return exit code according to new severity [E2E-CLI-088]",
+	testSample01 := TestCase{
+		Name: "should perform a scans successfully giving results with new severity and return exit code according to new severity [E2E-CLI-08_1]",
 		Args: args{
 			Args: []cmdArgs{
 				[]string{"scan", "-o", "/path/e2e/output",
@@ -13,24 +13,77 @@ func init() { //nolint
 					"-q", "\"/path/test/fixtures/test_new_severity/info\"",
 					"--new-severities",
 				},
+			},
+			ExpectedResult: []ResultsValidation{
+				{
+					ResultsFile: "E2E_CLI_088_RESULT",
+				},
+			},
+		},
+		WantStatus: []int{20},
+	}
+	testSample02 := TestCase{
+		Name: "should perform a scans successfully giving results with new severity and return exit code according to new severity [E2E-CLI-088_2]",
+		Args: args{
+			Args: []cmdArgs{
 				[]string{"scan", "-o", "/path/e2e/output",
 					"--output-name", "E2E_CLI_088_RESULT_2",
 					"-p", "\"/path/test/fixtures/test_new_severity/test\"",
 					"-q", "\"/path/test/fixtures/test_new_severity/low\"",
 					"--new-severities",
 				},
+			},
+			ExpectedResult: []ResultsValidation{
+				{
+					ResultsFile: "E2E_CLI_088_RESULT_2",
+				},
+			},
+		},
+		WantStatus: []int{30, 40, 50, 60},
+	}
+	testSample03 := TestCase{
+		Name: "should perform a scans successfully giving results with new severity and return exit code according to new severity [E2E-CLI-088_3]",
+		Args: args{
+			Args: []cmdArgs{
 				[]string{"scan", "-o", "/path/e2e/output",
 					"--output-name", "E2E_CLI_088_RESULT_3",
 					"-p", "\"/path/test/fixtures/test_new_severity/test\"",
 					"-q", "\"/path/test/fixtures/test_new_severity/medium\"",
 					"--new-severities",
 				},
+			},
+			ExpectedResult: []ResultsValidation{
+				{
+					ResultsFile: "E2E_CLI_088_RESULT_3",
+				},
+			},
+		},
+		WantStatus: []int{40},
+	}
+	testSample04 := TestCase{
+		Name: "should perform a scans successfully giving results with new severity and return exit code according to new severity [E2E-CLI-088_4]",
+		Args: args{
+			Args: []cmdArgs{
 				[]string{"scan", "-o", "/path/e2e/output",
 					"--output-name", "E2E_CLI_088_RESULT_4",
 					"-p", "\"/path/test/fixtures/test_new_severity/test\"",
 					"-q", "\"/path/test/fixtures/test_new_severity/high\"",
 					"--new-severities",
 				},
+			},
+			ExpectedResult: []ResultsValidation{
+				{
+					ResultsFile: "E2E_CLI_088_RESULT_4",
+				},
+			},
+		},
+		WantStatus: []int{50},
+	}
+	testSample05 := TestCase{
+		Name: "should perform a scans successfully giving results with new severity and return exit code according to new severity [E2E-CLI-088_5]",
+		Args: args{
+			Args: []cmdArgs{
+
 				[]string{"scan", "-o", "/path/e2e/output",
 					"--output-name", "E2E_CLI_088_RESULT_5",
 					"-p", "\"/path/test/fixtures/test_new_severity/test\"",
@@ -39,25 +92,15 @@ func init() { //nolint
 				},
 			},
 			ExpectedResult: []ResultsValidation{
-				{
-					ResultsFile: "E2E_CLI_088_RESULT",
-				},
-				{
-					ResultsFile: "E2E_CLI_088_RESULT_2",
-				},
-				{
-					ResultsFile: "E2E_CLI_088_RESULT_3",
-				},
-				{
-					ResultsFile: "E2E_CLI_088_RESULT_4",
-				},
+
 				{
 					ResultsFile: "E2E_CLI_088_RESULT_5",
 				},
 			},
 		},
-		WantStatus: []int{20, 30, 40, 50, 60},
+		WantStatus: []int{60},
 	}
 
-	Tests = append(Tests, testSample)
+	Tests = append(Tests, testSample01, testSample02, testSample03, testSample04, testSample05)
+
 }
