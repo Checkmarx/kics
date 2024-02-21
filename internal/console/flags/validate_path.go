3951d6f91b2e910bac252dd5448d722e31280bc3
JulioSCX <julio.silva@checkmarx.com>
2024-02-21 14:56:42 +0000

    updated error message and fixed regex

--- internal/console/flags/validate_path.go
+++ internal/console/flags/validate_path.go
@@ -2,19 +2,23 @@ package flags
 
 import (
 	"errors"
+	"fmt"
 	"regexp"
 )
 
 func validatePath(flagName string) error {
-	relPath := `^(?:\.\.\\|\.\\|\.\.\/|\.\/|\\|\/)?(?:[^<>:"\/\\|?*]+[\\|\/])*[^<>:"\/\\|?*]+(\/|\\)?$`
-	absPath := `^[a-zA-Z]:[\\|\/](?:[^<>:"\/\\|?*]+[\\|\/])*[^<>:"\/\\|?*]+(?:\/|\\)?$`
+	relPath := `^(?:\.\.\\|\.\\|\.\.\/|\.\/|\\|\/)?(?:[^<>:"\/\\|?*]+[\\\/])*[^<>:"\/\\|?*]+(\/|\\)?$`
+	absPath := `^[a-zA-Z]:[\\\/](?:[^<>:"\/\\|?*]+[\\\/])*[^<>:"\/\\|?*]+(?:\/|\\)?$`
 	regex := regexp.MustCompile(relPath + `|` + absPath)
 
 	path := GetStrFlag(flagName)
 	isValid := regex.MatchString(path) || path == ""
 
+	fmt.Println(flagName, GetStrFlag(flagName))
+
 	if !isValid {
-		return errors.New("the directory name contains invalid characters")
+		errorMsg := "the directory name you provided for the " + flagName + " flag contains invalid characters"
+		return errors.New(errorMsg)
 	}
 
 	return nil
