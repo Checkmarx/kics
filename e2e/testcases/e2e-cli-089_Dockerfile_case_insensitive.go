72d749b18754949119e16f85ceb4166ab172bd0a
JoaoCxMartins <joao.martins@checkmarx.com>
2024-02-28 10:03:54 +0000

    resolve conflicts

--- e2e/testcases/e2e-cli-089_Dockerfile_case_insensitive.go
+++ e2e/testcases/e2e-cli-089_Dockerfile_case_insensitive.go
@@ -1,21 +1,21 @@
 package testcases
 
-// E2E-CLI-086 - KICS  scan
+// E2E-CLI-089 - KICS  scan
 // should perform a scan and return results in different files with name DockerFile
 func init() { //nolint
 	testSample := TestCase{
-		Name: "should perform a scan and return results in different files with name DockerFile [E2E-CLI-086]",
+		Name: "should perform a scan and return results in different files with name DockerFile [E2E-CLI-089]",
 		Args: args{
 			Args: []cmdArgs{
 				[]string{"scan", "-o", "/path/e2e/output",
-					"--output-name", "E2E_CLI_086_RESULT",
+					"--output-name", "E2E_CLI_089_RESULT",
 					"-p", "\"/path/test/fixtures/dockerfile_case_insensitive\"",
 					"-i", "77783205-c4ca-4f80-bb80-c777f267c547",
 				},
 			},
 			ExpectedResult: []ResultsValidation{
 				{
-					ResultsFile:    "E2E_CLI_086_RESULT",
+					ResultsFile:    "E2E_CLI_089_RESULT",
 					ResultsFormats: []string{"json"},
 				},
 			},
