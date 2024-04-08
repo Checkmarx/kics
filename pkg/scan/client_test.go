c20d6ae1098ed86ecdce82f75ccb9b01640aaab3
Andre Peixoto <andre.peixoto@checkmarx.com>
2024-04-08 11:14:13 +0100

    Test_PerformScan_Successful

--- pkg/scan/client_test.go
+++ pkg/scan/client_test.go
@@ -1,7 +1,9 @@
 package scan
 
 import (
+	"context"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/require"
 )
@@ -28,4 +30,19 @@ func Test_ClientError(t *testing.T) {
 
 	require.Nil(t, client)
 	require.Error(t, err)
+}
+
+func Test_PerformScan_Successful(t *testing.T) {
+
+	params := &Parameters{
+
+		PreviewLines:   3,
+		ExcludeResults: []string{},
+	}
+	client, err := NewClient(params, nil, nil)
+	require.NoError(t, err, "error creating client")
+	ctx := context.Background()
+	err = client.PerformScan(ctx)
+	require.NoError(t, err, "error during scan")
+	require.NotEqual(t, time.Time{}, client.ScanStartTime, "ScanStartTime was not set")
 }
