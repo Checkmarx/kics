23ab09c9f9d99dfe51cb61a3e2f3085e57dae191
cx-ruiaraujo <rui.araujo@checkmarx.com>
2024-05-02 21:00:13 +0100

    Testing

--- pkg/resolver/file/file.go
+++ pkg/resolver/file/file.go
@@ -281,7 +281,7 @@ func (r *Resolver) resolveYamlPath(
 		value = checkServerlessFileReference(value)
 
 		path := filepath.Join(filepath.Dir(filePath), value)
-		if ansibleVars {
+		if false {
 			exists, ansibleVarsPath := findAnsibleVarsPath(filepath.Dir(filePath), value)
 			if !exists {
 				return *v, false
