5a0a9bf6742a3bf8a37e7041a85913169a02d514
JulioSCX <julio.silva@checkmarx.com>
2024-05-13 16:58:13 +0100

    changed E2E 54

--- e2e/fixtures/samples/bom-positive.tf
+++ e2e/fixtures/samples/bom-positive.tf
@@ -1,6 +1,15 @@
-resource "aws_s3_bucket" "hoge" {
-  bucket = "hoge"
+module "s3_bucket" {
+  source = "terraform-aws-modules/s3-bucket/aws"
+  version = "3.7.0"
+
+  bucket = "my-s3-bucket"
+  acl    = "private"
+
+  versioning {
+    enabled = true
+  }
 }
+
 
 resource "aws_athena_database" "hoge" {
   name   = "database_name"
