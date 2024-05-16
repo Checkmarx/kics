2dec9cba5f2fad1085f066703e3b2c478b00b6ac
JoaoCxMartins <joao.martins@checkmarx.com>
2024-05-16 09:56:03 +0100

    96/97

--- e2e/testcases/e2e-cli-096_new_similarity_id_with_old_similarity_id.go
+++ e2e/testcases/e2e-cli-096_new_similarity_id_with_old_similarity_id.go
@@ -1,14 +1,14 @@
 package testcases
 
-// E2E-CLI-095 - KICS scan
+// E2E-CLI-096 - KICS scan
 // should perform a scan successfully giving results with similarity ids unique and the old similarity id
 func init() { //nolint
 	testSample := TestCase{
-		Name: "should perform a scan successfully giving results with similarity ids unique, showing the old similarity id [E2E-CLI-095]",
+		Name: "should perform a scan successfully giving results with similarity ids unique, showing the old similarity id [E2E-CLI-096]",
 		Args: args{
 			Args: []cmdArgs{
 				[]string{"scan", "-o", "/path/e2e/output",
-					"--output-name", "E2E_CLI_095_RESULT",
+					"--output-name", "E2E_CLI_096_RESULT",
 					"-p", "\"/path/test/fixtures/new_similarity_id\"",
 					"-i", "bb9ac4f7-e13b-423d-a010-c74a1bfbe492",
 					"--kics_compute_new_simid",
@@ -16,7 +16,7 @@ func init() { //nolint
 			},
 			ExpectedResult: []ResultsValidation{
 				{
-					ResultsFile: "E2E_CLI_095_RESULT",
+					ResultsFile: "E2E_CLI_096_RESULT",
 				},
 			},
 		},
--- e2e/testcases/e2e-cli-097_new_similarity_id_without_old_similarity_id.go
+++ e2e/testcases/e2e-cli-097_new_similarity_id_without_old_similarity_id.go
@@ -1,21 +1,21 @@
 package testcases
 
-// E2E-CLI-094 - KICS scan
+// E2E-CLI-097 - KICS scan
 // should perform a scan successfully giving results with similarity ids unique without showing the old similarity id
 func init() { //nolint
 	testSample := TestCase{
-		Name: "should perform a scan successfully giving results with similarity ids unique without showing the old similarity id [E2E-CLI-094]",
+		Name: "should perform a scan successfully giving results with similarity ids unique without showing the old similarity id [E2E-CLI-097]",
 		Args: args{
 			Args: []cmdArgs{
 				[]string{"scan", "-o", "/path/e2e/output",
-					"--output-name", "E2E_CLI_094_RESULT",
+					"--output-name", "E2E_CLI_097_RESULT",
 					"-p", "\"/path/test/fixtures/new_similarity_id\"",
 					"-i", "bb9ac4f7-e13b-423d-a010-c74a1bfbe492",
 				},
 			},
 			ExpectedResult: []ResultsValidation{
 				{
-					ResultsFile: "E2E_CLI_094_RESULT",
+					ResultsFile: "E2E_CLI_097_RESULT",
 				},
 			},
 		},
