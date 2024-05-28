f5d836241d50ed9cfbadecc1cb609b21b4bdb95e
JoaoCxMartins <joao.martins@checkmarx.com>
2024-05-28 16:36:04 +0100

    linter

--- e2e/utils/json.go
+++ e2e/utils/json.go
@@ -233,7 +233,7 @@ func setFields(t *testing.T, expect, actual []string, expectFileName, actualFile
 			"Expected Result content: 'fixtures/%s' doesn't match the Actual Result Scanned Paths content: 'output/%s'.",
 			expectFileName, actualFileName)
 
-		//compare the results
+		// compare the results
 		expectToCompare := []model.VulnerableFile{}
 		for _, v := range expectI.Queries {
 			expectToCompare = append(expectToCompare, v.Files...)
@@ -246,7 +246,7 @@ func setFields(t *testing.T, expect, actual []string, expectFileName, actualFile
 			"Expected Queries content: 'fixtures/%s' doesn't match the Actual Queries content: 'output/%s'.",
 			expectToCompare, actualToCompare)
 
-		//compare severity counters
+		// compare severity counters
 		compare := reflect.DeepEqual(expectI.SeveritySummary.SeverityCounters, actualI.SeveritySummary.SeverityCounters)
 		require.True(t, compare, "Expected Severity Counters content: 'fixtures/%s' doesn't match the Actual Severity Counters content: 'output/%s'.", //nolint:lll
 			expectI.SeveritySummary.SeverityCounters, actualI.SeveritySummary.SeverityCounters)
