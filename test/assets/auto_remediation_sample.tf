e933a6d450a5433c5dd85adb7b2a90e3aba190f0
seanyyan <seanyyan@tencent.com>
2024-06-14 18:13:01 +0800

    add

--- test/assets/auto_remediation_sample.tf
+++ test/assets/auto_remediation_sample.tf
@@ -12,7 +12,7 @@ resource "alicloud_ram_account_password_policy" "corporate2" {
 		minimum_password_length = 14
 		require_lowercase_characters = false
 		require_uppercase_characters = false
-		require_numbers              = false
+		require_numbers              = true
 		require_symbols              = false
 		hard_expiry                  = true
 		password_reuse_prevention    = 5
