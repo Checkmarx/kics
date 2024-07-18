434f986fe1cb92de07eb3644f1149ef75cc66745
cx-ruiaraujo <rui.araujo@checkmarx.com>
2024-07-18 15:17:09 +0100

    test

--- e2e/cli_test.go
+++ e2e/cli_test.go
@@ -122,11 +122,11 @@ func Test_E2E_CLI(t *testing.T) {
 	t.Cleanup(func() {
 		err := os.RemoveAll("output")
 		if err != nil {
-			t.Logf("\nError when trying to remove tests output folder\n")
+			t.Logf("\nError when trying to remove tests output folder\n " + err.Error())
 		}
 		err = os.RemoveAll("tmp-kics-ar")
 		if err != nil {
-			t.Logf("\nError when trying to remove tmp-kics-ar folder\n")
+			t.Logf("\nTest2 --> Error when trying to remove tmp-kics-ar folder\n " + err.Error())
 		}
 		t.Logf("E2E tests ::ellapsed time:: %v", time.Since(scanStartTime))
 	})
