d7874421457ede4de6bca18cbbeb26079b07f43b
bahar-shah <bahar.shah@datadoghq.com>
2024-08-12 19:51:26 -0400

    reset kics.go

--- kics.go
+++ kics.go
@@ -1,17 +1,9 @@
-package main
+package kics
 
 import (
 	"github.com/Checkmarx/kics/internal/console"
 	"github.com/Checkmarx/kics/pkg/scan"
 )
-
-func main() {
-	inputPaths := []string{
-		"/Users/bahar.shah/go/src/github.com/DataDog/innovation-week-cloud-to-tf/terraform/ami.tf",
-	}
-	outputPath := "/Users/bahar.shah/go/src/github.com/DataDog/innovation-week-cloud-to-tf"
-	ExecuteKICSScan(inputPaths, outputPath)
-}
 
 func ExecuteKICSScan(inputPaths []string, outputPath string) {
 	params := scan.GetDefaultParameters()
