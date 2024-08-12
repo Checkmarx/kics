2be9a581b86712f8e3ca857fa133aa44c4c72b10
bahar-shah <bahar.shah@datadoghq.com>
2024-08-12 15:13:12 -0400

    add params to init function

--- kics.go
+++ kics.go
@@ -5,45 +5,9 @@ import (
 	"github.com/Checkmarx/kics/pkg/scan"
 )
 
-func ExecuteKICSScan() {
-	params := &scan.Parameters{
-		CloudProvider:       []string{""},
-		DisableFullDesc:     false,
-		ExcludeCategories:   []string{},
-		ExcludeQueries:      []string{},
-		ExcludeResults:      []string{},
-		ExcludeSeverities:   []string{},
-		ExcludePaths:        []string{},
-		ExperimentalQueries: false,
-		IncludeQueries:      []string{},
-		InputData:           "",
-		OutputName:          "kics-result",
-		OutputPath:          "/Users/bahar.shah/go/src/github.com/DataDog/innovation-week-cloud-to-tf",
-		Path: []string{
-			"/Users/bahar.shah/go/src/github.com/DataDog/innovation-week-cloud-to-tf/terraform/ami.tf",
-		},
-		PayloadPath:                 "",
-		PreviewLines:                3,
-		QueriesPath:                 []string{"../../../assets/queries"},
-		LibrariesPath:               "../../../assets/libraries",
-		ReportFormats:               []string{"sarif"},
-		Platform:                    []string{""},
-		TerraformVarsPath:           "",
-		QueryExecTimeout:            60,
-		LineInfoPayload:             false,
-		DisableSecrets:              true,
-		SecretsRegexesPath:          "",
-		ChangedDefaultQueryPath:     false,
-		ChangedDefaultLibrariesPath: false,
-		ScanID:                      "console",
-		BillOfMaterials:             false,
-		ExcludeGitIgnore:            false,
-		OpenAPIResolveReferences:    false,
-		ParallelScanFlag:            0,
-		MaxFileSizeFlag:             5,
-		UseOldSeverities:            false,
-		MaxResolverDepth:            15,
-		ExcludePlatform:             []string{""},
-	}
+func ExecuteKICSScan(inputPaths []string, outputPath string) {
+	params := scan.GetDefaultParameters()
+	params.Path = inputPaths
+	params.OutputPath = outputPath
 	console.ExecuteScan(params)
 }
