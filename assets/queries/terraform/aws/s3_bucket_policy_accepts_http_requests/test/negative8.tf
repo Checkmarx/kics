13618d712421aa92579c45ef8d728e1dd14aaa55
ArturRibeiro-CX <artur.ribeiro@checkmarx.com>
2025-04-07 16:50:30 +0100

    remove unnecessary positive test and fix negative8 test to align with false positive result

--- assets/queries/terraform/aws/s3_bucket_policy_accepts_http_requests/test/negative8.tf
+++ assets/queries/terraform/aws/s3_bucket_policy_accepts_http_requests/test/negative8.tf
@@ -1,3 +1,7 @@
+locals {
+  support_site_bucket_name = "support-site-${var.stack}-${var.environment}${var.bucket_name_suffix}"
+}
+
 resource "aws_s3_bucket" "support_site_app_bucket" {
   force_destroy = var.destroyable_env
   bucket        = local.support_site_bucket_name
@@ -7,6 +11,26 @@ resource "aws_cloudfront_origin_access_identity" "support_site_origin_access_ide
 
 data "aws_iam_policy_document" "support_site_bucket_policy_document" {
   version = "2012-10-17"
+
+  statement {
+    sid       = "DenyNonSecureTransport"
+    effect    = "Deny"
+    actions   = ["s3:*"]
+    resources = [
+      "arn:aws:s3:::${local.support_site_bucket_name}/*"
+    ]
+
+    condition {
+      test     = "Bool"
+      variable = "aws:SecureTransport"
+      values   = ["false"]
+    }
+
+    principals {
+      type        = "AWS"
+      identifiers = ["*"]
+    }
+  }
 
   statement {
     sid     = "AllowCloudFrontAccess"
