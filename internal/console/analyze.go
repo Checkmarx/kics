2e781ca671aacdb2d9e9036e2a7d0f413ca3fc87
ArturRibeiro-CX <153724638+cx-artur-ribeiro@users.noreply.github.com>
2025-04-21 18:08:59 +0100

    fix lint issues

--- internal/console/analyze.go
+++ internal/console/analyze.go
@@ -117,7 +117,7 @@ func writeToFile(resultsPath string, analyzerResults model.AnalyzedPaths) error 
 		return err
 	}
 
-	f, err := os.Create(resultsPath)
+	f, err := os.Create(resultsPath) //nolint:gosec
 	if err != nil {
 		return err
 	}
