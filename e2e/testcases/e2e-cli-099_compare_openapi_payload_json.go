0a99c517b9e2d891813a0446dc6527dd9ec6af36
ArturRibeiro-CX <153724638+cx-artur-ribeiro@users.noreply.github.com>
2025-04-30 18:38:19 +0100

    add e2e that compares the payload between json and yaml files

---
+++ e2e/testcases/e2e-cli-099_compare_openapi_payload_json.go
@@ -0,0 +1,28 @@
+// Package testcases provides end-to-end (E2E) testing functionality for the application.
+package testcases
+
+// E2E-CLI-099 - KICS scan with OpenAPI reference resolution enabled on JSON and YAML files containing circular references
+// The scan should complete successfully, returning exit code 50, producing equivalent payloads for both formats.
+// The only changes should be the file extensions, file id and file path.
+func init() { //nolint
+	testSample := TestCase{
+		Name: "should perform a valid scan while evaluating if the payload generated is the same for both yaml and json files scanned [E2E-CLI-099]",
+		Args: args{
+			Args: []cmdArgs{
+				[]string{"scan", "-p", "\"/path/e2e/fixtures/samples/openAPIJsonYamlPayloadCompare/openAPIJson/openAPI.json\"",
+					"-v", "-d", "/path/e2e/output/E2E_CLI_099_JSON_PAYLOAD.json", "--enable-openapi-refs"},
+				[]string{
+					"scan", "-p", "\"/path/e2e/fixtures/samples/openAPIJsonYamlPayloadCompare/openAPIYaml/openAPI.yaml\"",
+					"-v", "-d", "/path/e2e/output/E2E_CLI_099_YAML_PAYLOAD.json", "--enable-openapi-refs",
+				},
+			},
+			ExpectedPayload: []string{
+				"E2E_CLI_099_JSON_PAYLOAD.json",
+				"E2E_CLI_099_YAML_PAYLOAD.json",
+			},
+		},
+		WantStatus: []int{50},
+	}
+
+	Tests = append(Tests, testSample)
+}
