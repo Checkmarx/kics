27292c06d93fa41ab1d9543ec74a754d61553793
ArturRibeiro-CX <153724638+cx-artur-ribeiro@users.noreply.github.com>
2025-05-13 17:42:42 +0100

    add new constant reference and refactor all calls to $ref

--- pkg/resolver/file/file.go
+++ pkg/resolver/file/file.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
+	"github.com/Checkmarx/kics/v2/internal/constants"
 	"io"
 	"os"
 	"path/filepath"
@@ -18,8 +19,6 @@ import (
 	"github.com/Checkmarx/kics/v2/pkg/model"
 	"github.com/Checkmarx/kics/v2/pkg/utils"
 )
-
-const reference = "$ref"
 
 // ResolvedFile - used for caching the already resolved files
 type ResolvedFile struct {
@@ -158,7 +157,7 @@ func (r *Resolver) handleMap(
 ) (_ any, validOpenAPISectionRef, canBeCached bool) {
 	canBeCachedAll := true
 	for k, v := range value {
-		isRef := strings.Contains(strings.ToLower(k), reference)
+		isRef := strings.Contains(strings.ToLower(k), constants.Reference)
 		val, res, itemCanBeCached := r.walk(originalFileContent, fullObject, v, path, resolvingStatus, isRef)
 
 		if !itemCanBeCached {
@@ -172,8 +171,8 @@ func (r *Resolver) handleMap(
 				valMap = make(map[string]interface{})
 			}
 			valMap["RefMetadata"] = map[string]interface{}{
-				reference: v,
-				"alone":   len(value) == 1,
+				constants.Reference: v,
+				"alone":             len(value) == 1,
 			}
 			return valMap, false, canBeCachedAll
 		}
@@ -232,7 +231,7 @@ func (r *Resolver) yamlWalk(
 		ansibleVars := false
 		for i := range value.Content {
 			if i >= 1 {
-				refBool = strings.Contains(value.Content[i-1].Value, reference)
+				refBool = strings.Contains(value.Content[i-1].Value, constants.Reference)
 				ansibleVars = strings.Contains(value.Content[i-1].Value, "include_vars")
 			}
 			resolved, ok, canBeCached := r.yamlWalk(originalFileContent, fullObject,
@@ -247,7 +246,7 @@ func (r *Resolver) yamlWalk(
 				}
 				originalValueNode := &yaml.Node{
 					Kind:  yaml.ScalarNode,
-					Value: reference,
+					Value: constants.Reference,
 				}
 				refAloneKeyNode := &yaml.Node{
 					Kind:  yaml.ScalarNode,
@@ -582,7 +581,7 @@ func checkIfCircularYaml(circularValue string, yamlSection *yaml.Node) bool {
 	}
 	for index := 0; index < len(yamlSection.Content)-1; index += 1 {
 		// if there is a reference to the same value that was resolved it is a circular definition
-		if yamlSection.Content[index].Value == reference && yamlSection.Content[index+1].Value == circularValue {
+		if yamlSection.Content[index].Value == constants.Reference && yamlSection.Content[index+1].Value == circularValue {
 			return true
 		} else if checkIfCircularYaml(circularValue, yamlSection.Content[index]) {
 			return true
@@ -618,7 +617,7 @@ func checkIfCircular(circularValue string, section interface{}, maxResolverDepth
 		if okMap {
 			for key, val := range sectionAsMap {
 				// if there is a reference to the same value that was resolved it is a circular definition
-				if key == reference && val == circularValue {
+				if key == constants.Reference && val == circularValue {
 					return true
 				} else if checkIfCircular(circularValue, val, maxResolverDepth-1) {
 					return true
