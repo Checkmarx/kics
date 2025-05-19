6727967c93a40e9b8f1416fd60a66b3ea6ad35dd
ArturRibeiro-CX <153724638+cx-artur-ribeiro@users.noreply.github.com>
2025-05-19 17:44:41 +0100

    revert addition of ignored files field

--- pkg/scan/utils_test.go
+++ pkg/scan/utils_test.go
@@ -190,7 +190,7 @@ func Test_GetTotalFiles(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 
-			v := GetTotalFiles(tt.paths)
+			v := getTotalFiles(tt.paths)
 			require.Equal(t, tt.expectedOutput, v)
 
 		})
