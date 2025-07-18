74dbc6328b241e1f9f1cd7c25369342c8025b406
ArturRibeiro-CX <153724638+cx-artur-ribeiro@users.noreply.github.com>
2025-07-18 12:16:47 +0100

    update test

--- pkg/progress/counter/counter_progress_test.go
+++ pkg/progress/counter/counter_progress_test.go
@@ -10,51 +10,45 @@ import (
 func TestCounter_Start(t *testing.T) {
 	type fields struct {
 		label string
-		total int64
+		Total int
 	}
 	tests := []struct {
-		name    string
-		counter int
-		fields  fields
-		silent  bool
+		name   string
+		fields fields
+		silent bool
 	}{
 		{
-			name:    "test_counter_progress_bar",
-			counter: 9,
+			name: "test_counter_progress_bar",
 			fields: fields{
 				label: "test",
-				total: 10,
+				Total: 10,
 			},
 			silent: false,
 		},
 		{
-			name:    "test_counter_progress_bar_silent",
-			counter: 9,
+			name: "test_counter_progress_bar_silent",
 			fields: fields{
 				label: "test",
-				total: 10,
+				Total: 10,
 			},
 			silent: true,
 		},
 	}
-
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			wg := &sync.WaitGroup{}
 			prog := make(chan int64)
 
 			wg.Add(1)
-			pb := NewProgressBar(tt.fields.label, tt.fields.total, prog, wg, tt.silent)
+			pb := NewProgressBar(tt.fields.label, int64(tt.fields.Total), prog, wg, tt.silent)
 			go pb.Start()
 
-			for i := 0; i < tt.counter; i++ {
+			for i := 0; i < tt.fields.Total; i++ {
 				prog <- 1
 			}
 
-			close(prog)
 			wg.Wait()
-
-			require.Equal(t, int64(tt.counter), pb.pBar.Current())
+			require.Equal(t, int64(tt.fields.Total), pb.pBar.Current())
 		})
 	}
 }
