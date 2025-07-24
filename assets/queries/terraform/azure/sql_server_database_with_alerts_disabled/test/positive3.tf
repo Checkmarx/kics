f399d543a309927c986dbfd06848cbdc0a3e5ac6
Andre Pereira <219305055+cx-andre-pereira@users.noreply.github.com>
2025-07-24 10:17:34 +0100

    fix for github detecting secret on test positive3 (ARM)

--- assets/queries/terraform/azure/sql_server_database_with_alerts_disabled/test/positive3.tf
+++ assets/queries/terraform/azure/sql_server_database_with_alerts_disabled/test/positive3.tf
@@ -1,15 +1,8 @@
-resource "azurerm_sql_server" "positive3" {
-    name                         = "mssqlserver"
-    resource_group_name          = azurerm_resource_group.example.name
-    location                     = azurerm_resource_group.example.location
-    version                      = "12.0"
-    administrator_login          = "mradministrator"
-    administrator_login_password = "thisIsDog11"
-
-    extended_auditing_policy {
-       storage_endpoint           = azurerm_storage_account.example.primary_blob_endpoint
-       storage_account_access_key = azurerm_storage_account.example.primary_access_key
-       storage_account_access_key_is_secondary = true
-       retention_in_days                       = 90
-    }
+resource "azurerm_mssql_server_security_alert_policy" "positive4" {
+  resource_group_name        = azurerm_resource_group.example.name
+  server_name                = azurerm_sql_server.example.name
+  storage_endpoint           = azurerm_storage_account.example.primary_blob_endpoint
+  storage_account_access_key = azurerm_storage_account.example.primary_access_key
+  retention_days = 20
+  email_account_admins = false
 }
