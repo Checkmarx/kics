64626eb28d14555ff29ce6acc4d98d06b914b76b
Ricardo Jesus <219317970+cx-ricardo-jesus@users.noreply.github.com>
2025-07-28 10:58:07 +0100

    small changes

--- assets/queries/terraform/azure/app_service_authentication_disabled/test/positive3.tf
+++ assets/queries/terraform/azure/app_service_authentication_disabled/test/positive3.tf
@@ -1,5 +1,5 @@
 resource "azurerm_linux_web_app" "positive3" {
-  name                = "example"
+  name                = "example-app-service"
   resource_group_name = azurerm_resource_group.example.name
   location            = azurerm_service_plan.example.location
   service_plan_id     = azurerm_service_plan.example.id
