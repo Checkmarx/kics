58867a2fb867b618d86768bc79b6f39641bf53b2
Andre Pereira <219305055+cx-andre-pereira@users.noreply.github.com>
2025-08-18 14:53:40 +0100

    sensitive port negative tests

--- assets/queries/terraform/aws/sensitive_port_is_exposed_to_entire_network/test/negative2.tf
+++ assets/queries/terraform/aws/sensitive_port_is_exposed_to_entire_network/test/negative2.tf
@@ -1,73 +1,59 @@
-resource "aws_security_group" "negative1" {
-  name        = "allow_tls1"
-  description = "Allow TLS inbound traffic"
+resource "aws_security_group" "base" {
+  name        = "base_sg"
+  description = "Base security group"
   vpc_id      = aws_vpc.main.id
-
-  ingress {
-    description = "TLS from VPC"
-    from_port   = 2383
-    to_port     = 2383
-    protocol    = "tcp"
-    cidr_blocks = [aws_vpc.main.cidr_block]
-  }
 }
 
-
-resource "aws_security_group" "negative2" {
-  name        = "allow_tls2"
-  description = "Allow TLS inbound traffic"
-  vpc_id      = aws_vpc.main.id
-
-  ingress {
-    description = "TLS from VPC"
-    from_port   = 2384
-    to_port     = 2386
-    protocol    = "tcp"
-    cidr_blocks = ["/0"]
-  }
+resource "aws_vpc_security_group_ingress_rule" "negative1" {
+  security_group_id = aws_security_group.base.id
+  description        = "TLS from VPC"
+  from_port          = 2383
+  to_port            = 2383
+  ip_protocol        = "tcp"
+  cidr_ipv4          = aws_vpc.main.cidr_block
 }
 
-
-resource "aws_security_group" "negative3" {
-  name        = "allow_tls3"
-  description = "Allow TLS inbound traffic"
-  vpc_id      = aws_vpc.main.id
-
-  ingress {
-    description = "TLS from VPC"
-    from_port   = 25
-    to_port     = 2500
-    protocol    = "tcp"
-    cidr_blocks = ["1.2.3.4/5"]
-  }
+resource "aws_vpc_security_group_ingress_rule" "negative2" {
+  security_group_id = aws_security_group.base.id
+  description        = "TLS from VPC"
+  from_port          = 2384
+  to_port            = 2386
+  ip_protocol        = "tcp"
+  cidr_ipv4          = "0.0.0.0/0"
 }
 
-
-resource "aws_security_group" "negative4" {
-  name        = "allow_tls4"
-  description = "Allow TLS inbound traffic"
-  vpc_id      = aws_vpc.main.id
-
-  ingress {
-    description = "TLS from VPC"
-    from_port   = 25
-    to_port     = 2500
-    protocol    = "tcp"
-    cidr_blocks = ["1.2.3.4/5"]
-  }
+resource "aws_vpc_security_group_ingress_rule" "negative3" {
+  security_group_id = aws_security_group.base.id
+  description        = "TLS from VPC"
+  from_port          = 25
+  to_port            = 2500
+  ip_protocol        = "tcp"
+  cidr_ipv4          = "1.2.3.4/5"
 }
 
+resource "aws_vpc_security_group_ingress_rule" "negative4" {
+  security_group_id = aws_security_group.base.id
+  description        = "TLS from VPC"
+  from_port          = 25
+  to_port            = 2500
+  ip_protocol        = "tcp"
+  cidr_ipv4          = "1.2.3.4/5"
+}
 
-resource "aws_security_group" "negative5" {
-  name        = "allow_tls5"
-  description = "Allow TLS inbound traffic"
-  vpc_id      = aws_vpc.main.id
+resource "aws_vpc_security_group_ingress_rule" "negative5a" {
+  security_group_id = aws_security_group.base.id
+  description        = "TLS from VPC"
+  from_port          = 25
+  to_port            = 2500
+  ip_protocol        = "udp"
+  cidr_ipv4          = "1.2.3.4/5"
+}
 
-  ingress {
-    description = "TLS from VPC"
-    from_port   = 25
-    to_port     = 2500
-    protocol    = "udp"
-    cidr_blocks = ["1.2.3.4/5","0.0.0.0/12"]
-  }
+resource "aws_vpc_security_group_ingress_rule" "negative5b" {
+  security_group_id = aws_security_group.base.id
+  description        = "TLS from VPC"
+  from_port          = 25
+  to_port            = 2500
+  ip_protocol        = "udp"
+  cidr_ipv4          = "0.0.0.0/12"
 }
--- assets/queries/terraform/aws/sensitive_port_is_exposed_to_entire_network/test/negative3.tf
+++ assets/queries/terraform/aws/sensitive_port_is_exposed_to_entire_network/test/negative3.tf
@@ -1,73 +1,65 @@
-resource "aws_security_group" "negative1" {
-  name        = "allow_tls1"
-  description = "Allow TLS inbound traffic"
+resource "aws_security_group" "base" {
+  name        = "base_sg"
+  description = "Base security group"
   vpc_id      = aws_vpc.main.id
-
-  ingress {
-    description = "TLS from VPC"
-    from_port   = 2383
-    to_port     = 2383
-    protocol    = "tcp"
-    cidr_blocks = [aws_vpc.main.cidr_block]
-  }
 }
 
-
-resource "aws_security_group" "negative2" {
-  name        = "allow_tls2"
-  description = "Allow TLS inbound traffic"
-  vpc_id      = aws_vpc.main.id
-
-  ingress {
-    description = "TLS from VPC"
-    from_port   = 2384
-    to_port     = 2386
-    protocol    = "tcp"
-    cidr_blocks = ["/0"]
-  }
+resource "aws_security_group_rule" "negative1" {
+  type              = "ingress"
+  from_port         = 2383
+  to_port           = 2383
+  protocol          = "tcp"
+  cidr_blocks       = [aws_vpc.main.cidr_block]
+  security_group_id = aws_security_group.base.id
+  description        = "TLS from VPC"
 }
 
-
-resource "aws_security_group" "negative3" {
-  name        = "allow_tls3"
-  description = "Allow TLS inbound traffic"
-  vpc_id      = aws_vpc.main.id
-
-  ingress {
-    description = "TLS from VPC"
-    from_port   = 25
-    to_port     = 2500
-    protocol    = "tcp"
-    cidr_blocks = ["1.2.3.4/5"]
-  }
+resource "aws_security_group_rule" "negative2" {
+  type              = "ingress"
+  from_port         = 2384
+  to_port           = 2386
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.base.id
+  description        = "TLS from VPC"
 }
 
-
-resource "aws_security_group" "negative4" {
-  name        = "allow_tls4"
-  description = "Allow TLS inbound traffic"
-  vpc_id      = aws_vpc.main.id
-
-  ingress {
-    description = "TLS from VPC"
-    from_port   = 25
-    to_port     = 2500
-    protocol    = "tcp"
-    cidr_blocks = ["1.2.3.4/5"]
-  }
+resource "aws_security_group_rule" "negative3" {
+  type              = "ingress"
+  from_port         = 25
+  to_port           = 2500
+  protocol          = "tcp"
+  cidr_blocks       = ["1.2.3.4/5"]
+  security_group_id = aws_security_group.base.id
+  description        = "TLS from VPC"
 }
 
+resource "aws_security_group_rule" "negative4" {
+  type              = "ingress"
+  from_port         = 25
+  to_port           = 2500
+  protocol          = "tcp"
+  cidr_blocks       = ["1.2.3.4/5"]
+  security_group_id = aws_security_group.base.id
+  description        = "TLS from VPC"
+}
 
-resource "aws_security_group" "negative5" {
-  name        = "allow_tls5"
-  description = "Allow TLS inbound traffic"
-  vpc_id      = aws_vpc.main.id
+resource "aws_security_group_rule" "negative5a" {
+  type              = "ingress"
+  from_port         = 25
+  to_port           = 2500
+  protocol          = "udp"
+  cidr_blocks       = ["1.2.3.4/5"]
+  security_group_id = aws_security_group.base.id
+  description        = "TLS from VPC"
+}
 
-  ingress {
-    description = "TLS from VPC"
-    from_port   = 25
-    to_port     = 2500
-    protocol    = "udp"
-    cidr_blocks = ["1.2.3.4/5","0.0.0.0/12"]
-  }
+resource "aws_security_group_rule" "negative5b" {
+  type              = "ingress"
+  from_port         = 25
+  to_port           = 2500
+  protocol          = "udp"
+  cidr_blocks       = ["0.0.0.0/12"]
+  security_group_id = aws_security_group.base.id
+  description        = "TLS from VPC"
 }
