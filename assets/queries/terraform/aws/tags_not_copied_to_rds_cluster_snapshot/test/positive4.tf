febdeb27e8c7afddfe0f524661a8d343ce70862a
Ricardo Jesus <219317970+cx-ricardo-jesus@users.noreply.github.com>
2025-08-19 14:10:28 +0100

    added support for aws_db_instance resource on terraform

---
+++ assets/queries/terraform/aws/tags_not_copied_to_rds_cluster_snapshot/test/positive4.tf
@@ -0,0 +1,10 @@
+resource "aws_db_instance" "example" {
+  allocated_storage      = 20
+  engine                 = "mysql"
+  engine_version         = "8.0"
+  instance_class         = "db.t3.micro"
+  identifier             = "my-rds-instance"
+  username               = "admin"
+  password               = "YourSecretPassword"
+  skip_final_snapshot    = true
+}
