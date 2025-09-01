ba353d33869eabbe39528a4cfa563af1ebfa7e5c
Ricardo Jesus <219317970+cx-ricardo-jesus@users.noreply.github.com>
2025-09-01 12:24:10 +0100

    changes on samples and the query

--- assets/queries/terraform/aws/block_device_is_not_encrypted/test/positive3.tf
+++ assets/queries/terraform/aws/block_device_is_not_encrypted/test/positive3.tf
@@ -2,13 +2,6 @@ module "asg" {
   source = "terraform-aws-modules/autoscaling/aws"
   version = "1.0.4"
 
-  # Launch configuration
-  lc_name = "example-lc"
-
-  image_id        = "ami-ebd02392"
-  instance_type   = "t2.micro"
-  security_groups = ["sg-12345678"]
-
   ebs_block_device = [ 
     {
       device_name           = "/dev/xvdz"
@@ -25,26 +18,28 @@ module "asg" {
       volume_type = "gp2"
     }
   ]
+}
 
-  # Auto scaling group
-  asg_name                  = "example-asg"
-  vpc_zone_identifier       = ["subnet-1235678", "subnet-87654321"]
-  health_check_type         = "EC2"
-  min_size                  = 0
-  max_size                  = 1
-  desired_capacity          = 1
-  wait_for_capacity_timeout = 0
+module "asg2" {
+  source  = "terraform-aws-modules/autoscaling/aws"
+  version = "6.0"
 
-  tags = [
-    {
-      key                 = "Environment"
-      value               = "dev"
-      propagate_at_launch = true
+  block_device_mappings = [
+    { 
+      ebs = { # Root volume
+        volume_size           = 50
+        volume_type           = "gp2"
+        delete_on_termination = true
+      }
     },
     {
-      key                 = "Project"
-      value               = "megasecret"
-      propagate_at_launch = true
-    },
+      device_name = "/dev/xvdz" # Additional EBS volume
+      ebs = {
+        volume_size           = 50
+        volume_type           = "gp2"
+        delete_on_termination = true
+        encrypted             = false
+      }
+    }
   ]
 }
