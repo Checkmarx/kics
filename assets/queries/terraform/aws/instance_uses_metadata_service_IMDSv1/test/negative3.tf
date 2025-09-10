554ab3d714ff255b15367509c7b1b76d488f6c14
Ricardo Jesus <219317970+cx-ricardo-jesus@users.noreply.github.com>
2025-09-10 10:31:48 +0100

    fix(queries): added suport modules/resources on instance uses metadata service IMDSv1 query for Terraform, CloudFormation and Ansible implementations (#7688)
    * added two negative samples for cloudFormation query
    * module support and new resource support for Terraform query implementation + new samples
    * more modules supported for Ansible query implementation + changed samples
    ---------
    
    Co-authored-by: Andre Pereira <219305055+cx-andre-pereira@users.noreply.github.com>
    Co-authored-by: Artur Ribeiro <153724638+cx-artur-ribeiro@users.noreply.github.com>

--- assets/queries/terraform/aws/instance_uses_metadata_service_IMDSv1/test/negative3.tf
+++ assets/queries/terraform/aws/instance_uses_metadata_service_IMDSv1/test/negative3.tf
@@ -2,20 +2,22 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "positive1" {
+resource "aws_instance" "negative3" {
   ami           = "ami-12345678"
   instance_type = "t2.micro"
 
   metadata_options {
+    http_endpoint = "disabled"
     http_tokens   = "optional"
   }
 }
 
-resource "aws_launch_configuration" "positive1" {
+resource "aws_launch_configuration" "negative3" {
   image_id      = "ami-12345678"
   instance_type = "t2.micro"
 
   metadata_options {
+    http_endpoint = "disabled"
     http_tokens   = "optional"
   }
 }
