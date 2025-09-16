dc6088a42419fc1f5372cd66ca5341d5a03b23e0
Ricardo Jesus <219317970+cx-ricardo-jesus@users.noreply.github.com>
2025-09-16 11:57:39 +0100

    Simplified logic for "app_service_authentication_disabled" query  (#7715)
    * Lowered the number of CxPolicy(s) in the query logic 
    * Added missing test for 
    Co-authored-by: Andre Pereira <219305055+cx-andre-pereira@users.noreply.github.com>

---
+++ assets/queries/terraform/azure/app_service_authentication_disabled/test/positive11.tf
@@ -0,0 +1,13 @@
+resource "azurerm_windows_web_app" "positive11" {
+  name                = "example-app-service"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_service_plan.example.location
+  service_plan_id     = azurerm_service_plan.example.id
+  auth_settings {
+    enabled = true
+  }
+  auth_settings_v2 {
+    login {}
+  }
+  site_config {}
+}
