3c57f636630706c5945e9a41f55c7a214deeab59
Thomas Plisson <thomas.plisson.external@axa.com>
2025-09-16 17:29:57 +0200

    Fix query for issue #7719: false negatives for App Service FTPS Enforce Disabled

--- assets/queries/terraform/azure/app_service_ftps_enforce_disabled/test/negative2.tf
+++ assets/queries/terraform/azure/app_service_ftps_enforce_disabled/test/negative2.tf
@@ -1,10 +1,12 @@
-resource "azurerm_linux_web_app" "negative2" {
-  name                = "negative2"
+resource "azurerm_app_service" "negative2" {
+  name                = "example-app-service"
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
-  service_plan_id     = azurerm_app_service_plan.example.id
+  app_service_plan_id = azurerm_app_service_plan.example.id
 
   site_config {
-    ftps_state = "Disabled" # Options: AllAllowed, FtpsOnly, Disabled
+    dotnet_framework_version = "v4.0"
+    scm_type                 = "LocalGit"
+    ftps_state               = "Disabled"
   }
 }
