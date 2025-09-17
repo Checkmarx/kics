010d057381f8b98276395995ef1a69d3e835623e
Andre Pereira <219305055+cx-andre-pereira@users.noreply.github.com>
2025-09-17 14:48:05 +0100

    better negative tests

--- assets/queries/terraform/aws/unknown_port_exposed_to_internet/test/negative2.tf
+++ assets/queries/terraform/aws/unknown_port_exposed_to_internet/test/negative2.tf
@@ -6,26 +6,37 @@ resource "aws_security_group" "negative2" {
 
 resource "aws_vpc_security_group_ingress_rule" "negative2-1" {
   security_group_id = aws_security_group.negative3.id
-  from_port         = 443
-  to_port           = 443
+  from_port         = 2383
+  to_port           = 2383
   ip_protocol       = "tcp"
-  description       = "TLS from VPC"
+  cidr_ipv4         = "0.0.0.0/0"
+  description       = "Remote desktop open private"
 }
+
 
 resource "aws_vpc_security_group_ingress_rule" "negative2-2" {
   security_group_id = aws_security_group.negative3.id
-  from_port         = 2383
-  to_port           = 2383
+  from_port         = 20
+  to_port           = 20
+  ip_protocol       = "tcp"
+  cidr_ipv6         = "::/0"
+  description       = "Remote desktop open private"
+}
+
+resource "aws_vpc_security_group_ingress_rule" "negative2-3" {
+  security_group_id = aws_security_group.negative3.id
+  from_port         = 0
+  to_port           = 10000
   ip_protocol       = "tcp"
   cidr_ipv4         = "192.168.0.0/24"
   description       = "Remote desktop open private"
 }
 
 
-resource "aws_vpc_security_group_ingress_rule" "negative2-3" {
+resource "aws_vpc_security_group_ingress_rule" "negative2-4" {
   security_group_id = aws_security_group.negative3.id
-  from_port         = 20
-  to_port           = 20
+  from_port         = 0
+  to_port           = 10000
   ip_protocol       = "tcp"
   cidr_ipv6         = "2001:db8:abcd:0012::/64"
   description       = "Remote desktop open private"
