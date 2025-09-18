845843d65f49b3804b6ed6f558af259e4903f719
Andre Pereira <219305055+cx-andre-pereira@users.noreply.github.com>
2025-09-18 15:38:13 +0100

    test(query): better testing for security group rules without description (#7729)
    * better testing for security group rules without description

--- assets/queries/terraform/aws/security_group_rules_without_description/test/positive4.tf
+++ assets/queries/terraform/aws/security_group_rules_without_description/test/positive4.tf
@@ -1,11 +1,4 @@
-resource "aws_security_group" "positive4" {
-  name        = "example-sg"
-  description = "Example security group"
-  vpc_id      = aws_vpc.main.id
-}
-
 resource "aws_vpc_security_group_ingress_rule" "positive4-1" {
-  security_group_id = aws_security_group.example.id
   cidr_ipv4         = "192.168.1.0/24"
   from_port         = 80
   to_port           = 80
@@ -13,7 +6,6 @@ resource "aws_vpc_security_group_ingress_rule" "positive4-1" {
 }
 
 resource "aws_vpc_security_group_egress_rule" "positive4-2" {
-  security_group_id = aws_security_group.example.id
   cidr_ipv4         = "0.0.0.0/0"
   from_port         = 0
   to_port           = 0
