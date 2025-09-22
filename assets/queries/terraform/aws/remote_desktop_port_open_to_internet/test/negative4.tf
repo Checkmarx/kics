5e59254244c088bd8df3605b1e9b40f13fd54c3c
Ricardo Jesus <219317970+cx-ricardo-jesus@users.noreply.github.com>
2025-09-22 17:34:37 +0100

    test(query): added new case for the several resources covered on the queries for terraform/aws (#7734)
    * anotattions of the query
    
    * added new tests
    
    * changes on the negative tests
    
    * update dockerfile images
    
    * added new cases on all the samples
    
    ---------
    
    Co-authored-by: ArturRibeiro-CX <153724638+cx-artur-ribeiro@users.noreply.github.com>

--- assets/queries/terraform/aws/remote_desktop_port_open_to_internet/test/negative4.tf
+++ assets/queries/terraform/aws/remote_desktop_port_open_to_internet/test/negative4.tf
@@ -47,6 +47,13 @@ module "vote_service_sg_ipv4_array" {
   ingress_with_cidr_blocks = [
     {
       description = "TLS from VPC"
+      from_port   = 3389
+      to_port     = 3389
+      protocol    = "udp"
+      cidr_blocks = ["0.0.0.0/0", "8.8.8.8/24"]
+    },
+    {
+      description = "TLS from VPC"
       from_port   = 2383
       to_port     = 2383
       protocol    = "udp"
@@ -65,6 +72,60 @@ module "vote_service_sg_ipv4_array" {
       to_port     = 22
       protocol    = "tcp"
       cidr_blocks = ["192.01.01.02/23"]
+    },
+    {
+      description = "TLS from VPC"
+      from_port   = 20
+      to_port     = 22
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  ]
+}
+
+module "vote_service_sg_ipv6_array" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "4.3.0"
+
+  name        = "user-service"
+  description = "Security group for user-service with custom ports open within VPC, and PostgreSQL publicly open"
+  vpc_id      = "vpc-12345678"
+
+  ingress_with_ipv6_cidr_blocks = [
+    {
+      description = "TLS from VPC"
+      from_port   = 3389
+      to_port     = 3389
+      protocol    = "udp"
+      ipv6_cidr_blocks = ["2001:0db8:85a3::8a2e:0370:7334/64", "::/0"]
+    },
+    {
+      description = "TLS from VPC"
+      from_port   = 28000
+      to_port     = 28001
+      protocol    = "tcp"
+      ipv6_cidr_blocks = ["10.0.0.0/16"]
+    },
+    {
+      description = "TLS from VPC"
+      from_port   = 28000
+      to_port     = 28001
+      protocol    = "tcp"
+      ipv6_cidr_blocks = ["0.0.0.0/0"]
+    },
+    {
+      description = "TLS from VPC"
+      from_port   = 20
+      to_port     = 22
+      protocol    = "tcp"
+      ipv6_cidr_blocks = ["2606:4700:3033::6815:3e3/56"]
+    },
+    {
+      description = "TLS from VPC"
+      from_port   = 20
+      to_port     = 22
+      protocol    = "tcp"
+      ipv6_cidr_blocks = ["::/0"]
     }
   ]
 }
