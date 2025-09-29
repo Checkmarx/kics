c5c02ac7f64ac33515427348cb7b7a98b9c81c1b
Andre Pereira <219305055+cx-andre-pereira@users.noreply.github.com>
2025-09-29 13:45:49 +0100

    new resources added to function_app_managed_identity_disabled

--- assets/queries/terraform/azure/function_app_managed_identity_disabled/test/positive.tf
+++ assets/queries/terraform/azure/function_app_managed_identity_disabled/test/positive.tf
@@ -1,29 +1,20 @@
-resource "azurerm_function_app" "negative1" {
+resource "azurerm_function_app" "positive1" {
   name                       = "test-azure-functions"
   location                   = azurerm_resource_group.example.location
   resource_group_name        = azurerm_resource_group.example.name
   app_service_plan_id        = azurerm_app_service_plan.example.id
-  identity {
-    type = "SystemAssigned"
-  }
 }
 
-resource "azurerm_linux_function_app" "negative2" {
+resource "azurerm_linux_function_app" "positive2" {
   name                       = "test-azure-functions"
   location                   = azurerm_resource_group.example.location
   resource_group_name        = azurerm_resource_group.example.name
   service_plan_id            = azurerm_app_service_plan.example.id
-  identity {
-    type = "SystemAssigned"
-  }
 }
 
-resource "azurerm_windows_function_app" "negative3" {
+resource "azurerm_windows_function_app" "positive3" {
   name                       = "test-azure-functions"
   location                   = azurerm_resource_group.example.location
   resource_group_name        = azurerm_resource_group.example.name
   service_plan_id            = azurerm_app_service_plan.example.id
-  identity {
-    type = "SystemAssigned"
-  }
 }
