e24a63c7a73be2982de8bb7d8059cf0d5809812f
Andre Pereira <219305055+cx-andre-pereira@users.noreply.github.com>
2025-10-19 14:50:34 +0100

    initial_implementation

---
+++ assets/queries/terraform/gcp/sql_db_instance_with_exposed_show_privileges/test/negative.tf
@@ -0,0 +1,23 @@
+resource "google_sql_database_instance" "negative_1" {
+  name             = "main-instance"
+  database_version = "POSTGRES_15"      # Is not a MYSQL instance
+  region           = "us-central1"
+
+  settings {
+    tier = "db-f1-micro"
+  }
+}
+
+resource "google_sql_database_instance" "negative_2" {
+  name             = "mysql-instance-with-flag"
+  database_version = "MYSQL_8_0"
+  region           = "us-central1"
+
+  settings {
+    tier = "db-f1-micro"
+
+    database_flags = [
+      { name = "skip_show_database", value = "on" },   # Has flag set to "on"
+    ]
+  }
+}
---
+++ assets/queries/terraform/gcp/sql_db_instance_with_exposed_show_privileges/test/positive.tf
@@ -0,0 +1,39 @@
+resource "google_sql_database_instance" "positive_1" {
+  name             = "mysql-instance-without-flag"
+  database_version = "MYSQL_8_0"
+  region           = "us-central1"
+
+  # Missing 'settings' field
+}
+
+resource "google_sql_database_instance" "positive_2" {
+  name             = "mysql-instance-without-flag"
+  database_version = "MYSQL_8_0"
+  region           = "us-central1"
+
+  settings {}  # Missing 'database_flags' field
+}
+
+resource "google_sql_database_instance" "positive_3" {
+  name             = "mysql-instance-without-flag"
+  database_version = "MYSQL_8_0"
+  region           = "us-central1"
+
+  settings {
+    database_flags = [
+      # Missing 'skip_show_database' flag
+    ]
+  }
+}
+
+resource "google_sql_database_instance" "positive_4" {
+  name             = "mysql-instance-with-flag"
+  database_version = "MYSQL_8_0"
+  region           = "us-central1"
+
+  settings {
+    database_flags = [
+      { name = "skip_show_database", value = "off" }  # Flag is not set to "on"
+    ]
+  }
+}
