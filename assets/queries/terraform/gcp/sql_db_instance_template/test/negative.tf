3e20267ce7a86680e50ea24c6646d0b3c733fe39
Andre Pereira <219305055+cx-andre-pereira@users.noreply.github.com>
2025-10-21 12:08:53 +0100

    template

---
+++ assets/queries/terraform/gcp/sql_db_instance_template/test/negative.tf
@@ -0,0 +1,23 @@
+resource "google_sql_database_instance" "negative_1" {
+  name             = "main-instance"
+  database_version = "MYSQL_8_0"      # Is not a POSTGRES instance
+  region           = "us-central1"
+
+  settings {
+    tier = "db-f1-micro"
+  }
+}
+
+resource "google_sql_database_instance" "negative_2" {
+  name             = "mysql-instance-with-flag"
+  database_version = "POSTGRES_15"
+  region           = "us-central1"
+
+  settings {
+    tier = "db-f1-micro"
+
+    database_flags = [
+      { name = "log_connections", value = "on" },   # Has flag set to "on"
+    ]
+  }
+}
