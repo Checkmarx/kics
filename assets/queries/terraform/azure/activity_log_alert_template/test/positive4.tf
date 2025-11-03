e204be48d02998c5d2c811b6a8a5ba5fe0184417
Andre Pereira <219305055+cx-andre-pereira@users.noreply.github.com>
2025-11-03 15:48:23 +0000

    initial implementation

---
+++ assets/queries/terraform/azure/activity_log_alert_template/test/positive4.tf
@@ -0,0 +1,40 @@
+
+# Query prioritizes flagging the log alert(s) that is "correct" but has filter(s) over the ones with wrong "operation_name"/"category"
+resource "azurerm_monitor_activity_log_alert" "positive4_1" {
+  name                = "example-activitylogalert"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  scopes              = [azurerm_resource_group.example.id]
+  description         = "Positive sample"
+
+  criteria {
+    resource_id    = azurerm_storage_account.to_monitor.id
+    operation_name = "Microsoft.Sql/servers/firewallRules/write"
+    category       = "Administrative"
+    caller         = "admin@contoso.com"                                          # filters by caller
+    level          = "Informational"                                              # filters by level
+    status         = "Succeeded"                                                  # filters by status
+  }
+
+  action {
+    action_group_id = azurerm_monitor_action_group.main.id
+    }
+}
+
+resource "azurerm_monitor_activity_log_alert" "positive4_2" {
+  name                = "example-activitylogalert"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  scopes              = [azurerm_resource_group.example.id]
+  description         = "Positive sample"
+
+  criteria {
+    resource_id    = azurerm_storage_account.to_monitor.id
+    operation_name = "Microsoft.Storage/storageAccounts/write"          # wrong operation name
+    category       = "Administrative"
+  }
+
+  action {
+    action_group_id = azurerm_monitor_action_group.main.id
+    }
+}
---
+++ assets/queries/terraform/azure/activity_log_alert_template/test/positive5.tf
@@ -0,0 +1,56 @@
+
+# Query prioritizes flagging the log alert(s) that is "correct" but missing the "action_group_id" field over all others
+resource "azurerm_monitor_activity_log_alert" "positive5_1" {
+  name                = "example-activitylogalert"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  scopes              = [azurerm_resource_group.example.id]
+  description         = "Positive sample"
+
+  criteria {
+    resource_id    = azurerm_storage_account.to_monitor.id
+    operation_name = "Microsoft.Sql/servers/firewallRules/write"
+    category       = "Administrative"
+  }
+
+  # Missing action block
+}
+
+resource "azurerm_monitor_activity_log_alert" "positive5_2" {
+  name                = "example-activitylogalert"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  scopes              = [azurerm_resource_group.example.id]
+  description         = "Positive sample"
+
+  criteria {
+    resource_id    = azurerm_storage_account.to_monitor.id
+    operation_name = "Microsoft.Sql/servers/firewallRules/write"
+    category       = "Administrative"
+    caller         = "admin@contoso.com"                                          # filters by caller
+    level          = "Informational"                                              # filters by level
+    status         = "Succeeded"                                                  # filters by status
+  }
+
+  action {
+    action_group_id = azurerm_monitor_action_group.main.id
+    }
+}
+
+resource "azurerm_monitor_activity_log_alert" "positive5_3" {
+  name                = "example-activitylogalert"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  scopes              = [azurerm_resource_group.example.id]
+  description         = "Positive sample"
+
+  criteria {
+    resource_id    = azurerm_storage_account.to_monitor.id
+    operation_name = "Microsoft.Storage/storageAccounts/write"          # wrong operation name
+    category       = "Administrative"
+  }
+
+  action {
+    action_group_id = azurerm_monitor_action_group.main.id
+    }
+}
