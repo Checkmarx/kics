0a3fcc5d80d6017bf7a80a4221db60325103cd5e
Andre Pereira <219305055+cx-andre-pereira@users.noreply.github.com>
2025-11-04 12:23:15 +0000

    adjusted tests for correct per project scope

--- assets/queries/terraform/azure/activity_log_alert_for_delete_sql_server_firewall_rule/test/positive5/positive5_1.tf
+++ assets/queries/terraform/azure/activity_log_alert_for_delete_sql_server_firewall_rule/test/positive5/positive5_1.tf
@@ -1,6 +1,21 @@
+# Query prioritizes flagging the log alert(s) that is "correct" but missing the "action_group_id" field over all others
+resource "azurerm_monitor_activity_log_alert" "positive5_1" {
+  name                = "example-activitylogalert"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  scopes              = [azurerm_resource_group.example.id]
+  description         = "Positive sample"
 
-# Query prioritizes flagging the log alert(s) that is "correct" but has filter(s) over the ones with wrong "operation_name"/"category"
-resource "azurerm_monitor_activity_log_alert" "positive4_1" {
+  criteria {
+    resource_id    = azurerm_storage_account.to_monitor.id
+    operation_name = "Microsoft.Sql/servers/firewallRules/delete"
+    category       = "Administrative"
+  }
+
+  # Missing action block
+}
+
+resource "azurerm_monitor_activity_log_alert" "positive5_2" {
   name                = "example-activitylogalert"
   resource_group_name = azurerm_resource_group.example.name
   location            = azurerm_resource_group.example.location
@@ -14,24 +29,6 @@ resource "azurerm_monitor_activity_log_alert" "positive4_1" {
     caller         = "admin@contoso.com"                                          # filters by caller
     level          = "Informational"                                              # filters by level
     status         = "Succeeded"                                                  # filters by status
-  }
-
-  action {
-    action_group_id = azurerm_monitor_action_group.main.id
-    }
-}
-
-resource "azurerm_monitor_activity_log_alert" "positive4_2" {
-  name                = "example-activitylogalert"
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_resource_group.example.location
-  scopes              = [azurerm_resource_group.example.id]
-  description         = "Positive sample"
-
-  criteria {
-    resource_id    = azurerm_storage_account.to_monitor.id
-    operation_name = "Microsoft.Storage/storageAccounts/write"          # wrong operation name
-    category       = "Administrative"
   }
 
   action {
