238bf1e3a268e1ef2a0b6286a16f2b77af3848dc
Andre Pereira <219305055+cx-andre-pereira@users.noreply.github.com>
2025-11-04 12:18:01 +0000

    adjusted tests for correct per project scope

--- assets/queries/terraform/azure/activity_log_alert_for_delete_security_solution/test/positive3/positive3_1.tf
+++ assets/queries/terraform/azure/activity_log_alert_for_delete_security_solution/test/positive3/positive3_1.tf
@@ -1,21 +1,16 @@
-resource "azurerm_monitor_activity_log_alert" "negative1" {
+# Case of correct "operation_name" and "category" but the "action.action_group_id" field is missing
+resource "azurerm_monitor_activity_log_alert" "positive3_1" {
   name                = "example-activitylogalert"
   resource_group_name = azurerm_resource_group.example.name
   location            = azurerm_resource_group.example.location
   scopes              = [azurerm_resource_group.example.id]
-  description         = "Negative sample"
+  description         = "Positive sample"
 
   criteria {
     resource_id    = azurerm_storage_account.to_monitor.id
-    operation_name = "operation_value"
+    operation_name = "Microsoft.Security/securitySolutions/delete"
     category       = "Administrative"
   }
 
-  action {
-    action_group_id = azurerm_monitor_action_group.main.id
-
-    webhook_properties = {
-      from = "terraform"
-    }
-  }
+  # Missing action
 }
--- assets/queries/terraform/azure/activity_log_alert_for_delete_security_solution/test/positive4/positive4_2.tf
+++ assets/queries/terraform/azure/activity_log_alert_for_delete_security_solution/test/positive4/positive4_2.tf
@@ -1,21 +1,17 @@
-resource "azurerm_monitor_activity_log_alert" "negative1" {
+resource "azurerm_monitor_activity_log_alert" "positive4_2" {
   name                = "example-activitylogalert"
   resource_group_name = azurerm_resource_group.example.name
   location            = azurerm_resource_group.example.location
   scopes              = [azurerm_resource_group.example.id]
-  description         = "Negative sample"
+  description         = "Positive sample"
 
   criteria {
     resource_id    = azurerm_storage_account.to_monitor.id
-    operation_name = "operation_value"
+    operation_name = "Microsoft.Storage/storageAccounts/write"          # wrong operation name
     category       = "Administrative"
   }
 
   action {
     action_group_id = azurerm_monitor_action_group.main.id
-
-    webhook_properties = {
-      from = "terraform"
     }
-  }
 }
