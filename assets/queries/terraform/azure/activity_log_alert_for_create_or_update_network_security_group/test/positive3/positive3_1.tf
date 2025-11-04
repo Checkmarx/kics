f65fdd36e3f91c0858d25095e4913a487b2ed233
Andre Pereira <219305055+cx-andre-pereira@users.noreply.github.com>
2025-11-04 12:04:59 +0000

    adjusted tests for correct per project scope

--- assets/queries/terraform/azure/activity_log_alert_for_create_or_update_network_security_group/test/positive3/positive3_1.tf
+++ assets/queries/terraform/azure/activity_log_alert_for_create_or_update_network_security_group/test/positive3/positive3_1.tf
@@ -14,21 +14,3 @@ resource "azurerm_monitor_activity_log_alert" "positive3_1" {
 
   # Missing action
 }
-
-resource "azurerm_monitor_activity_log_alert" "positive3_2" {
-  name                = "example-activitylogalert"
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_resource_group.example.location
-  scopes              = [azurerm_resource_group.example.id]
-  description         = "Positive sample"
-
-  criteria {
-    resource_id    = azurerm_storage_account.to_monitor.id
-    operation_name = "Microsoft.Network/networkSecurityGroups/write"
-    category       = "Administrative"
-  }
-
-  action {
-    # Missing action_group_id
-    }
-}
