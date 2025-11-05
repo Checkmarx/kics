14561988921fd975bec660add48c4a89d334ea0a
ArturRibeiro-CX <153724638+cx-artur-ribeiro@users.noreply.github.com>
2025-11-05 09:05:55 +0000

    update exit code to 40

--- e2e/testcases/e2e-cli-102_scan_zip_file_utf16.go
+++ e2e/testcases/e2e-cli-102_scan_zip_file_utf16.go
@@ -4,7 +4,7 @@ package testcases
 // and successfully complete the scan with proper encoding handling, returning exit code 50
 func init() { //nolint
 	testSample := TestCase{
-		Name: "should perform a scan on zip file with UTF-16 files and return exit code 50 [E2E-CLI-102]",
+		Name: "should perform a scan on zip file with UTF-16 files and return exit code 40 [E2E-CLI-102]",
 		Args: args{
 			Args: []cmdArgs{
 				[]string{"scan", "--payload-path", "/path/e2e/output/E2E_CLI_102_PAYLOAD",
@@ -15,7 +15,7 @@ func init() { //nolint
 				"E2E_CLI_102_PAYLOAD.json",
 			},
 		},
-		WantStatus: []int{50},
+		WantStatus: []int{40},
 	}
 
 	Tests = append(Tests, testSample)
