7e5ef9c71b441736aa74ebf90348208a10a9638c
Ricardo Jesus <219317970+cx-ricardo-jesus@users.noreply.github.com>
2025-11-12 15:05:29 +0000

    changed test name

--- assets/queries/terraform/azure/use_of_user_access_administrator_role_is_not_restricted/test/negative4.tf
+++ assets/queries/terraform/azure/use_of_user_access_administrator_role_is_not_restricted/test/negative4.tf
@@ -1,4 +1,4 @@
-resource "azurerm_role_assignment" "mg_assignment" {
+resource "azurerm_role_assignment" "negative4" {
   role_definition_id   = "b24988ac-6180-42a0-ab88-20f7382dd24c"
   scope                = "/providers/Microsoft.Management/managementGroups/contoso-root"
   principal_id         = data.azurerm_client_config.current.object_id
