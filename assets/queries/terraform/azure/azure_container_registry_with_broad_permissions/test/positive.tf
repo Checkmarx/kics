67f8e014c27bf93e7f2d3449eb64cfdaa0998580
Andre Pereira <219305055+cx-andre-pereira@users.noreply.github.com>
2025-11-25 17:58:32 +0000

    mini fix test

--- assets/queries/terraform/azure/azure_container_registry_with_broad_permissions/test/positive.tf
+++ assets/queries/terraform/azure/azure_container_registry_with_broad_permissions/test/positive.tf
@@ -8,12 +8,12 @@ resource "azurerm_container_registry" "sample" {
 
 resource "azurerm_role_assignment" "positive1" {
   principal_id         = azurerm_kubernetes_cluster.sample.object_id
-  role_definition_name = "ArcPush"
+  role_definition_name = "AcrPush"
   scope                = azurerm_container_registry.sample.id
 }
 
 resource "azurerm_role_assignment" "positive2" {
   principal_id         = azurerm_kubernetes_cluster.sample.object_id
-  role_definition_id   = "8311e382-0749-4cb8-b61a-304f252e45ec"         # id for ArcPush
+  role_definition_id   = "8311e382-0749-4cb8-b61a-304f252e45ec"         # id for AcrPush
   scope                = azurerm_container_registry.sample.id
 }
