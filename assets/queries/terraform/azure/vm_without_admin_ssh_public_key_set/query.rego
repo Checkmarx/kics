3a41ba71c6434bd0ffdc198cffddaa07e6d6197e
Andre Pereira <219305055+cx-andre-pereira@users.noreply.github.com>
2025-12-02 10:35:08 +0000

    fix attempt 1

--- assets/queries/terraform/azure/vm_without_admin_ssh_public_key_set/query.rego
+++ assets/queries/terraform/azure/vm_without_admin_ssh_public_key_set/query.rego
@@ -37,7 +37,7 @@ get_results(resource, type, name) = results {
 				"keyActualValue": sprintf("'%s[%s].admin_ssh_key[%d].public_key' is undefined or null", [type, name, index]),
 				"searchLine": common_lib.build_search_line(["resource", type, name, "admin_ssh_key", index], [])
 	}]
-	results != []
+	count(results) != 0
 } else = results {
 	not common_lib.valid_key(resource.admin_ssh_key, "public_key")
 	results := [{
--- assets/queries/terraform/azure/vm_without_admin_ssh_public_key_set/test/positive2.tf
+++ assets/queries/terraform/azure/vm_without_admin_ssh_public_key_set/test/positive2.tf
@@ -1,4 +1,4 @@
-resource "azurerm_linux_virtual_machine_scale_set" "positive2_2_1" {
+resource "azurerm_linux_virtual_machine_scale_set" "positive2_1" {
   name                = "positive2_1-machine"
   resource_group_name = azurerm_resource_group.positive2_1.name
   location            = azurerm_resource_group.positive2_1.location
@@ -27,14 +27,14 @@ resource "azurerm_linux_virtual_machine_scale_set" "positive2_2" {
   }
 }
 
-resource "azurerm_linux_virtual_machine_scale_set" "positive3_3" {
-  name                = "positive3_3-machine"
-  resource_group_name = azurerm_resource_group.positive3_3.name
-  location            = azurerm_resource_group.positive3_3.location
+resource "azurerm_linux_virtual_machine_scale_set" "positive2_3" {
+  name                = "positive2_3-machine"
+  resource_group_name = azurerm_resource_group.positive2_3.name
+  location            = azurerm_resource_group.positive2_3.location
   size                = "Standard_F2"
   admin_username      = "adminuser"
   network_interface_ids = [
-    azurerm_network_interface.positive3_3.id,
+    azurerm_network_interface.positive2_3.id,
   ]
 
   admin_ssh_key {                                     # ssh key array
