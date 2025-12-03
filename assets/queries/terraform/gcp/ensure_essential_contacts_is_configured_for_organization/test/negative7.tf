48948583c7acb0c427f0ea0c25afe68c2535e983
Vasco Oliveira <200926503+cx-vasco-oliveira@users.noreply.github.com>
2025-12-03 11:00:31 +0000

    Fixed negative7 test (#7892)

--- assets/queries/terraform/gcp/ensure_essential_contacts_is_configured_for_organization/test/negative7.tf
+++ assets/queries/terraform/gcp/ensure_essential_contacts_is_configured_for_organization/test/negative7.tf
@@ -2,7 +2,7 @@ data "google_organization" "org" {
   organization = "123456789012"
 }
 
-resource "google_essential_contacts_contact" "positive1" {
+resource "google_essential_contacts_contact" "negative7" {
   parent = data.google_organization.org.name
   email  = "foo@bar.com"
   language_tag = "en-GB"
