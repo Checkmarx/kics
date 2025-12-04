30e1078b2069e09733f39a47ffcc0f61764e720a
CXVascoOliveira <200926503+cx-vasco-oliveira@users.noreply.github.com>
2025-12-04 16:00:35 +0000

    Added tests for complete QA Accuracy Validation for this query

--- assets/queries/terraform/azure/activity_log_alert_for_service_health_not_configured/test/negative1.tf
+++ assets/queries/terraform/azure/activity_log_alert_for_service_health_not_configured/test/negative1.tf
@@ -18,9 +18,34 @@ resource "azurerm_monitor_activity_log_alert" "negative1" {
   }
 }
 
+resource "azurerm_monitor_activity_log_alert" "negative2" {
+  name                = "ServiceHealthActivityLogAlert"
+  resource_group_name = var.resource_group_name
+  scopes              = [data.azurerm_subscription.current.id]
+  description         = "Alert for Azure Service Health events"
+  enabled             = true
+
+  criteria {
+    category = "ServiceHealth"
+
+     service_health {
+      events = [
+        "Incident",
+        "Maintenance",
+        "Security",
+        "Informational"
+      ]
+    }
+  }
+
+  action {
+    action_group_id = azurerm_monitor_action_group.notify_team.id
+  }
+}
+
 data "azurerm_subscription" "current" {}
 
-resource "azurerm_monitor_activity_log_alert" "negative2" {
+resource "azurerm_monitor_activity_log_alert" "negative3" {
   name                = "ServiceHealthActivityLogAlert"
   resource_group_name = var.resource_group_name
   scopes              = [data.azurerm_subscription.secondary.id]
@@ -42,4 +67,16 @@ resource "azurerm_monitor_activity_log_alert" "negative2" {
 
 data "azurerm_subscription" "secondary" {
   subscription_id = "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
+}
+
+resource "azurerm_monitor_activity_log_alert" "negative4" {
+  name                = "ServiceHealthActivityLogAlert"
+  resource_group_name = var.resource_group_name
+  scopes = [azurerm_resource_group.example.id] # The scope needs to be a subscription
+  description         = "Alert for Azure Service Health events"
+  enabled             = true
+
+  action {
+    action_group_id = azurerm_monitor_action_group.notify_team.id
+  }
 }
