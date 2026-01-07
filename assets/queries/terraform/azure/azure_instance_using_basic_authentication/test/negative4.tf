c1c93dc181b84cec1f74778ceaa30122ae7d02f5
Andre Pereira <219305055+cx-andre-pereira@users.noreply.github.com>
2026-01-07 11:10:33 +0000

    feat(query): 6 new Beta queries and fixes for "Azure Instance Using Basic Authentication" - terraform/azure (#7868)
    * initial commit
    
    * simId file update
    
    * Query 1/6 - VM Without Admin SSH Public Key Set
    
    * Query 1/6 - tests for vm scale set
    
    * Query 2/6 - VM With Extension Operations Enabled
    
    * Query 2/6 - Added support for azurerm_windows_virtual_machine_scale_set
    
    * Query 3/6 - VM With Automatic Updates Disabled
    
    * fixed query naming to include 'Beta' and Query 4/6 - Beta - Disk Encryption On Managed Disk Disabled
    
    * Query 4/6 - completed metadata
    
    * Query 5/6 - Beta - Key Vault Without HSM Protection
    
    * Query 6/6 - Beta - VM Without Encryption At Host
    
    * mini fix expected results
    
    * Query 3/6 - removed passwords from tests and addded support for legacy azurerm_virtual_machine_scale_set for azure_instance_using_basic_authentication
    
    * Query 1/6 - minor fix to ensure results for tfplan files
    
    * fix attempt 1
    
    * fix
    
    * fix
    
    * fixed tests field identation
    
    * added support for tfplan
    
    ---------
    
    Co-authored-by: Ricardo Jesus <219317970+cx-ricardo-jesus@users.noreply.github.com>

--- assets/queries/terraform/azure/azure_instance_using_basic_authentication/test/negative4.tf
+++ assets/queries/terraform/azure/azure_instance_using_basic_authentication/test/negative4.tf
@@ -1,21 +1,17 @@
-resource "azurerm_virtual_machine" "negative1_1" {
+resource "azurerm_virtual_machine_scale_set" "negative4_1" {
   name                  = "${var.prefix}-vm"
   location              = azurerm_resource_group.main.location
   resource_group_name   = azurerm_resource_group.main.name
-  network_interface_ids = [azurerm_network_interface.main.id]
-  vm_size               = "Standard_DS1_v2"
 
   os_profile_linux_config {
     disable_password_authentication = true
   }
 }
 
-resource "azurerm_virtual_machine" "negative1_2" {
+resource "azurerm_virtual_machine_scale_set" "negative4_2" {
   name                  = "${var.prefix}-vm"
   location              = azurerm_resource_group.main.location
   resource_group_name   = azurerm_resource_group.main.name
-  network_interface_ids = [azurerm_network_interface.main.id]
-  vm_size               = "Standard_DS1_v2"
 
   # missing "os_profile_linux_config" - means it is not a linux vm
 }
