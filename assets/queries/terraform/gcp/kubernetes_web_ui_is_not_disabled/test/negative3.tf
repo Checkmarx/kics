64940cac26a4b58d34262b6f97a8ffd69350449c
Ricardo Jesus <219317970+cx-ricardo-jesus@users.noreply.github.com>
2026-01-26 14:24:39 +0000

    translated pt comments to en + changed CWE

--- assets/queries/terraform/gcp/kubernetes_web_ui_is_not_disabled/test/negative3.tf
+++ assets/queries/terraform/gcp/kubernetes_web_ui_is_not_disabled/test/negative3.tf
@@ -1,8 +1,6 @@
-# incluir versão do GKE (min_master_version) superior a 1.10
-
 resource "google_container_cluster" "negative3" {
   name               = "marcellus-wallace"
   location           = "us-central1-a"
   initial_node_count = 3
-  min_master_version = "1.11.8-gke.5"
+  min_master_version = "1.11.8-gke.5" # gke version higher than 1.10
 }
--- assets/queries/terraform/gcp/kubernetes_web_ui_is_not_disabled/test/positive2.tf
+++ assets/queries/terraform/gcp/kubernetes_web_ui_is_not_disabled/test/positive2.tf
@@ -1,9 +1,7 @@
-# incluir versão do GKE (min_master_version) inferior a 1.10
-
 resource "google_container_cluster" "positive2" {
   name               = "marcellus-wallace"
   location           = "us-central1-a"
   initial_node_count = 3
-  min_master_version = "1.8.12-gke.2"
+  min_master_version = "1.8.12-gke.2" # gke version lower than 1.10
   addons_config {}
 }
