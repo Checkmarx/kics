d8681c12183a04d24ebefaa6e220cd28a74bbfeb
Ricardo Jesus <219317970+cx-ricardo-jesus@users.noreply.github.com>
2026-02-23 17:34:03 +0000

    removed unnecessary duplicated test

--- assets/queries/terraform/gcp/logs_and_alerts_missing_project_ownership_assignment_and_changes/test/negative6.tf
+++ assets/queries/terraform/gcp/logs_and_alerts_missing_project_ownership_assignment_and_changes/test/negative6.tf
@@ -1,48 +1,4 @@
-resource "google_logging_metric" "audit_config_change_1" {
-  name        = "audit_config_change"
-  description = "Detects changes to audit configurations via SetIamPolicy"
-  filter = <<-FILTER
-    (protoPayload.serviceName="cloudresourcemanager.googleapis.com")
-    AND (ProjectOwnership OR projectOwnerInvitee)
-    OR (protoPayload.serviceData.policyDelta.bindingDeltas.action="REMOVE"
-    AND protoPayload.serviceData.policyDelta.bindingDeltas.role="roles/owner")
-    OR (protoPayload.serviceData.policyDelta.bindingDeltas.action="ADD"
-    AND protoPayload.serviceData.policyDelta.bindingDeltas.role="roles/owner")
-  FILTER
-}
-
-resource "google_logging_metric" "audit_config_change_2" {
-  name        = "audit_config_change"
-  description = "Detects changes to audit configurations via SetIamPolicy"
-  filter = <<-FILTER
-    (protoPayload.serviceName="cloudresourcemanager.googleapis.com")
-    AND (ProjectOwnership OR projectOwnerInvitee)
-    OR (protoPayload.serviceData.policyDelta.bindingDeltas.action="ADD"
-    AND protoPayload.serviceData.policyDelta.bindingDeltas.role="roles/owner")
-    OR (protoPayload.serviceData.policyDelta.bindingDeltas.action="REMOVE"
-    AND protoPayload.serviceData.policyDelta.bindingDeltas.role="roles/owner")
-  FILTER
-}
-
-
-resource "google_logging_metric" "audit_config_change_3" {
-  name        = "audit_config_change"
-  description = "Detects changes to audit configurations via SetIamPolicy"
-  filter = <<-FILTER
-         ( protoPayload.serviceName  = "cloudresourcemanager.googleapis.com" )
-        AND
-         ( ProjectOwnership   OR projectOwnerInvitee  )
-        OR   ( protoPayload.serviceData.policyDelta.bindingDeltas.action   =  "REMOVE"
-        AND   protoPayload.serviceData.policyDelta.bindingDeltas.role  =   "roles/owner" )
-        OR
-         (
-          protoPayload.serviceData.policyDelta.bindingDeltas.action =   "ADD" AND
-            protoPayload.serviceData.policyDelta.bindingDeltas.role   = "roles/owner"
-            )
-      FILTER
-}
-
-resource "google_logging_metric" "audit_config_change_4" {
+resource "google_logging_metric" "audit_config_change" {
   name        = "audit_config_change"
   description = "Detects changes to audit configurations via SetIamPolicy"
   filter = <<-FILTER
@@ -50,8 +6,23 @@ resource "google_logging_metric" "audit_config_change_4" {
   AND (ProjectOwnership OR projectOwnerInvitee)
   OR (protoPayload.serviceData.policyDelta.bindingDeltas.action="REMOVE"
       AND protoPayload.serviceData.policyDelta.bindingDeltas.role="roles/owner")
-  OR (protoPayload.serviceData.policyDelta.bindingDeltas.action="ADD"
-      AND protoPayload.serviceData.policyDelta.bindingDeltas.role="roles/owner")
+  OR NOT (NOT protoPayload.serviceData.policyDelta.bindingDeltas.action="ADD"
+      OR NOT protoPayload.serviceData.policyDelta.bindingDeltas.role="roles/owner")
   OR (protoPayload.methodName="SetIamPolicy")
 FILTER
+}
+
+resource "google_monitoring_alert_policy" "audit_config_alert" {
+  display_name = "Audit Config Change Alert"
+
+  combiner = "OR"
+
+  conditions {
+    display_name = "Audit Config Change Condition"
+    condition_threshold {
+      filter = "resource.type=\"gce_instance\" AND metric.type=\"logging.googleapis.com/user/audit_config_change\""
+    }
+  }
+
+  notification_channels = [google_monitoring_notification_channel.email.id]
 }
--- assets/queries/terraform/gcp/logs_and_alerts_missing_project_ownership_assignment_and_changes/test/negative7.tf
+++ assets/queries/terraform/gcp/logs_and_alerts_missing_project_ownership_assignment_and_changes/test/negative7.tf
@@ -2,25 +2,35 @@ resource "google_logging_metric" "audit_config_change" {
   name        = "audit_config_change"
   description = "Detects changes to audit configurations via SetIamPolicy"
   filter = <<-FILTER
-  (protoPayload.serviceName="cloudresourcemanager.googleapis.com")
-  AND (ProjectOwnership OR projectOwnerInvitee)
-  OR (protoPayload.serviceData.policyDelta.bindingDeltas.action="REMOVE"
-      AND protoPayload.serviceData.policyDelta.bindingDeltas.role="roles/owner")
-  OR NOT (NOT protoPayload.serviceData.policyDelta.bindingDeltas.action="ADD"
-      OR NOT protoPayload.serviceData.policyDelta.bindingDeltas.role="roles/owner")
-  OR (protoPayload.methodName="SetIamPolicy")
-FILTER
+    (protoPayload.serviceName="cloudresourcemanager.googleapis.com")
+    AND NOT (NOT ProjectOwnership AND NOT projectOwnerInvitee)
+    OR NOT (NOT protoPayload.serviceData.policyDelta.bindingDeltas.action="REMOVE"
+    OR NOT protoPayload.serviceData.policyDelta.bindingDeltas.role="roles/owner")
+    OR (protoPayload.serviceData.policyDelta.bindingDeltas.action="ADD"
+    AND protoPayload.serviceData.policyDelta.bindingDeltas.role="roles/owner")
+  FILTER
 }
 
 resource "google_monitoring_alert_policy" "audit_config_alert" {
-  display_name = "Audit Config Change Alert"
+  display_name = "Audit Config Change Alert (Log Match)"
 
   combiner = "OR"
 
   conditions {
-    display_name = "Audit Config Change Condition"
-    condition_threshold {
-      filter = "resource.type=\"gce_instance\" AND metric.type=\"logging.googleapis.com/user/audit_config_change\""
+    display_name = "Audit Config Change Condition"        # test for unusual spacing
+    condition_matched_log {
+      filter = <<-FILTER
+         ( protoPayload.serviceName  = "cloudresourcemanager.googleapis.com" )
+        AND
+         ( ProjectOwnership   OR projectOwnerInvitee  )
+        OR  NOT ( NOT protoPayload.serviceData.policyDelta.bindingDeltas.action   =  "REMOVE"
+        OR NOT protoPayload.serviceData.policyDelta.bindingDeltas.role  =   "roles/owner" )
+        OR
+         (
+          protoPayload.serviceData.policyDelta.bindingDeltas.action =   "ADD" AND
+            protoPayload.serviceData.policyDelta.bindingDeltas.role   = "roles/owner"
+            )
+      FILTER
     }
   }
 
