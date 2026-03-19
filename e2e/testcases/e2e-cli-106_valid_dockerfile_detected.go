051e791e6a175edea66516dffa82fbce71a75dd1
Andre Pereira <219305055+cx-andre-pereira@users.noreply.github.com>
2026-03-19 16:33:28 +0000

    Changed E2E to 106 to fix merge conflict

--- e2e/testcases/e2e-cli-106_valid_dockerfile_detected.go
+++ e2e/testcases/e2e-cli-106_valid_dockerfile_detected.go
@@ -1,27 +1,27 @@
 package testcases
 
-// E2E-CLI-105 - KICS  scan
+// E2E-CLI-106 - KICS  scan
 // should perform the scan successfully detect all valid dockerfile documents and return result 50
 func init() { //nolint
 	testSample := TestCase{
-		Name: "should perform a valid scan with all dockerfile documents parsed [E2E-CLI-105]",
+		Name: "should perform a valid scan with all dockerfile documents parsed [E2E-CLI-106]",
 		Args: args{
 			Args: []cmdArgs{
 				[]string{"scan", "-o", "/path/e2e/output",
-					"--output-name", "E2E_CLI_105_RESULT",
+					"--output-name", "E2E_CLI_106_RESULT",
 					"-p", "/path/test/fixtures/dockerfile",
 					"-p", "/path/test/fixtures/negative_dockerfile",
-					"--payload-path", "/path/e2e/output/E2E_CLI_105_PAYLOAD.json",
+					"--payload-path", "/path/e2e/output/E2E_CLI_106_PAYLOAD.json",
 				},
 			},
 			ExpectedResult: []ResultsValidation{
 				{
-					ResultsFile:    "E2E_CLI_105_RESULT",
+					ResultsFile:    "E2E_CLI_106_RESULT",
 					ResultsFormats: []string{"json"},
 				},
 			},
 			ExpectedPayload: []string{
-				"E2E_CLI_105_PAYLOAD.json",
+				"E2E_CLI_106_PAYLOAD.json",
 			},
 		},
 		WantStatus: []int{50},
