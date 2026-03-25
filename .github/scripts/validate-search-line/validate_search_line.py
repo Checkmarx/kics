c6236588fd5a1866555a77a9c69d72034724645f
Ricardo Jesus <219317970+cx-ricardo-jesus@users.noreply.github.com>
2026-03-25 19:48:42 +0000

    trying type_assert

--- .github/scripts/validate-search-line/validate_search_line.py
+++ .github/scripts/validate-search-line/validate_search_line.py
@@ -5,6 +5,7 @@ import os
 import subprocess
 import sys
 from pathlib import Path
+from pymarshal.json import unmarshal_json
 from results_models import ScanResults
 
 REPO_ROOT = Path(__file__).resolve().parents[3]
