a430b8f2d912ee424f91c0f8b3b0c9fa049d880f
balaakasam <thripuraavula@gmail.com>
2026-03-29 14:09:28 -0400

    feat(query): add IAM Role Without Permission Boundary negative test case

---
+++ assets/queries/terraform/aws/iam_role_without_permission_boundary/test/negative1.tf
@@ -0,0 +1,16 @@
+# This should NOT trigger the query - permissions_boundary is defined
+resource "aws_iam_role" "negative1" {
+  name = "negative_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      }
+    ]
+  })
