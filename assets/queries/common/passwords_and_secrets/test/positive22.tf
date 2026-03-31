f54afd396bf167ef83768bcaf04ca8c5be075593
Andre Pereira <219305055+cx-andre-pereira@users.noreply.github.com>
2026-03-31 09:56:29 +0100

    Revert change to results query name in expected results and fix positive22 false positive

--- assets/queries/common/passwords_and_secrets/test/positive22.tf
+++ assets/queries/common/passwords_and_secrets/test/positive22.tf
@@ -1,4 +1,4 @@
-# "Heroku API Key" - 9a3650af-5b88-48cd-ab89-cd77fd0b633f  positive-test
+# "Her oku API Key"(extra space to prevent false positive) - 9a3650af-5b88-48cd-ab89-cd77fd0b633f  positive-test
 provider "heroku" {
   email   = "ops@company.com"
   api_key = "C71AAAAE-1D1D-1D1D-1D1D-1D1D1D1D1D1D"  # positive1
