f8f00248000ff9da0d51117e452ec77acd61539c
Andre Pereira <219305055+cx-andre-pereira@users.noreply.github.com>
2026-04-07 23:51:56 +0100

    Best of both worlds solution, fallback to the implementation that changes searchKey but now only changes if warranted, simplified distinction between identical FROM statements in the payload with a Endline-1 in brackets, SimId will vary if lines shift (exclusively on duplicate FROM statements that flag)

--- pkg/detector/docker/docker_detect.go
+++ pkg/detector/docker/docker_detect.go
@@ -47,7 +47,7 @@ func (d DetectKindLine) DetectLine(file *model.FileMetadata, searchKey string,
 		sKey = strings.ReplaceAll(sKey, str[0], `{{`+strconv.Itoa(idx)+`}}`)
 	}
 
-	extractedString[0][1], _, _ = strings.Cut(extractedString[0][1], "-kics-id-")
+	extractedString[0][1], _, _ = strings.Cut(extractedString[0][1], "(")
 
 	unchangedText := make([]string, len(*file.LinesOriginalData))
 	copy(unchangedText, *file.LinesOriginalData)
@@ -80,8 +80,8 @@ func (d DetectKindLine) DetectLine(file *model.FileMetadata, searchKey string,
 }
 
 func extractLineHint(value string) (trimmedValue string, endLine int) {
-	idx := strings.LastIndex(value, "--")
-	if n, err := strconv.Atoi(value[idx+len("--"):]); err == nil {
+	idx := strings.LastIndex(value, "#")
+	if n, err := strconv.Atoi(value[idx+len("#"):]); err == nil {
 		return value[:idx], n
 	}
 	return value, 0
