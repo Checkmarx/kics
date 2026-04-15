a1409ecff7ff2e37f399de3057064ed30c83a269
Antero Silva <antero.silva@checkmarx.com>
2026-04-15 22:34:12 +0100

    chore: remove azure_app_service_http_logs_disabled (duplicate of PR #7991)

--- assets/queries/terraform/azure/azure_app_service_http_logs_disabled/test/positive1.tf
+++
@@ -1,8 +0,0 @@
-resource "azurerm_linux_web_app" "fail_no_logs" {
-  name                = "app-fail-1"
-  resource_group_name = "rg"
-  location            = "West Europe"
-  service_plan_id     = "plan-id"
-
-  site_config {}
-}
