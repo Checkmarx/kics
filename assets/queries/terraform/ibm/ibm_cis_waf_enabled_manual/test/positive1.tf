54a994d25c46b7ea5a98cf8f90890fe8edb2807c
Antero Silva <antero.silva@checkmarx.com>
2026-04-15 22:18:28 +0100

    chore: translate Spanish comments to English in positive1.tf

--- assets/queries/terraform/ibm/ibm_cis_waf_enabled_manual/test/positive1.tf
+++ assets/queries/terraform/ibm/ibm_cis_waf_enabled_manual/test/positive1.tf
@@ -2,6 +2,6 @@ resource "ibm_cis_domain_settings" "settings_off" {
   cis_id    = "crn:v1:bluemix:public:internet-svcs:..."
   domain_id = "example-id"
   
-  # FALLO: WAF desactivado
+  # FAIL: WAF desactivado
   waf = "off"
 }
