5b59388621c1f673e6855ef3c082d76bf27b6bda
Antero Silva <antero.silva@checkmarx.com>
2026-04-15 22:22:37 +0100

    chore: translate Spanish comments to English in positive2.tf

--- assets/queries/terraform/oci/oci_instance_transit_encryption/test/positive2.tf
+++ assets/queries/terraform/oci/oci_instance_transit_encryption/test/positive2.tf
@@ -7,7 +7,7 @@ resource "oci_core_instance" "instance_missing_attribute" {
   compartment_id      = "ocid1.compartment..."
   shape               = "VM.Standard.E4.Flex"
 
-  # El bloque existe, pero falta el atributo de encriptación
+  # The block Exists, but Missing The attribute de encriptación
   launch_options {
     boot_volume_type = "PARAVIRTUALIZED"
     network_type     = "PARAVIRTUALIZED"
