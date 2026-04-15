92a2b3335fdb0f4b65214ce3873549913c848a37
Antero Silva <antero.silva@checkmarx.com>
2026-04-15 22:09:34 +0100

    chore: translate Spanish comments to English in positive2.tf

--- assets/queries/terraform/azure/azure_storage_blob_logging_disabled/test/positive2.tf
+++ assets/queries/terraform/azure/azure_storage_blob_logging_disabled/test/positive2.tf
@@ -2,5 +2,5 @@ resource "azurerm_monitor_diagnostic_setting" "fail_no_logs" {
   name               = "empty-diag"
   target_resource_id = "${azurerm_storage_account.example.id}/blobServices/default"
   storage_account_id = "id"
-  # enabled_log bloque ausente
+  # enabled_log bloque missing
 }
