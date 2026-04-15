315993c90046be9cab2178925f9f4be29d254b7a
Antero Silva <antero.silva@checkmarx.com>
2026-04-15 21:18:44 +0100

    fix(queries): remove gcp_gke_security_posture_manual from PR

--- assets/queries/terraform/gcp/gcp_gke_security_posture_manual/test/positive2.tf
+++
@@ -1,7 +0,0 @@
-resource "google_container_cluster" "fail_mode_disabled" {
-  name = "cluster-posture-off"
-  
-  security_posture_config {
-    mode = "DISABLED" # FALLO: Debe ser BASIC o ENTERPRISE
-  }
-}
