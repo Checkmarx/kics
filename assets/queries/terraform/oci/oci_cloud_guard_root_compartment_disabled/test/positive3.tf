f46707e32650ad9151601c90f51b5da52e28c4ae
Antero Silva <antero.silva@checkmarx.com>
2026-04-15 22:21:35 +0100

    chore: translate Spanish comments to English in positive3.tf

--- assets/queries/terraform/oci/oci_cloud_guard_root_compartment_disabled/test/positive3.tf
+++ assets/queries/terraform/oci/oci_cloud_guard_root_compartment_disabled/test/positive3.tf
@@ -3,7 +3,7 @@ provider "oci" {
 }
 
 resource "oci_cloud_guard_configuration" "cg_child" {
-  # FALLO: Apunta a un compartimento hijo, no al tenancy
+  # FAIL: Apunta a un compartimento hijo, no al tenancy
   compartment_id   = var.child_compartment_id
   reporting_region = "us-ashburn-1"
   status           = "ENABLED"
