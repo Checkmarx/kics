d492cb07f9376609d22a20c4ca613024d24d0cbf
Antero Silva <antero.silva@checkmarx.com>
2026-04-15 22:22:07 +0100

    chore: translate Spanish comments to English in positive3.tf

--- assets/queries/terraform/oci/oci_iam_password_policy_length/test/positive3.tf
+++ assets/queries/terraform/oci/oci_iam_password_policy_length/test/positive3.tf
@@ -1,4 +1,4 @@
 resource "oci_identity_authentication_policy" "missing_block" {
   compartment_id = "ocid1.tenancy..."
-  # FALLO: No hay bloque password_policy
+  # FAIL: No hay bloque password_policy
 }
