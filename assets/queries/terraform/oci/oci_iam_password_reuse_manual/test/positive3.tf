cb8775df1efbb7dea3ca5bbe489f855647b1fc07
Antero Silva <antero.silva@checkmarx.com>
2026-04-15 22:22:15 +0100

    chore: translate Spanish comments to English in positive3.tf

--- assets/queries/terraform/oci/oci_iam_password_reuse_manual/test/positive3.tf
+++ assets/queries/terraform/oci/oci_iam_password_reuse_manual/test/positive3.tf
@@ -1,7 +1,7 @@
 resource "oci_identity_authentication_policy" "legacy_policy" {
   compartment_id = "ocid1.tenancy..."
   
-  # FALLO: Recurso Legacy requiere chequeo manual
+  # FAIL: resource Legacy requiere chequeo manual
   password_policy {
     minimum_password_length = 14
   }
