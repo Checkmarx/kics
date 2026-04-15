2f9c2cf0fa0fa249de3d072572ba4d1fec202a32
Antero Silva <antero.silva@checkmarx.com>
2026-04-15 22:22:06 +0100

    chore: translate Spanish comments to English in positive2.tf

--- assets/queries/terraform/oci/oci_iam_password_policy_length/test/positive2.tf
+++ assets/queries/terraform/oci/oci_iam_password_policy_length/test/positive2.tf
@@ -2,7 +2,7 @@ resource "oci_identity_authentication_policy" "missing_attr" {
   compartment_id = "ocid1.tenancy..."
   
   password_policy {
-    # FALLO: Falta minimum_password_length
+    # FAIL: Missing minimum_password_length
     is_lowercase_characters_required = true
   }
 }
