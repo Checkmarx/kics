37d12f16383c2157bdc13d49bd8edc329b42ef65
Antero Silva <antero.silva@checkmarx.com>
2026-04-15 22:22:50 +0100

    chore: translate Spanish comments to English in positive2.tf

--- assets/queries/terraform/oci/oci_notification_topic_without_subscription/test/positive2.tf
+++ assets/queries/terraform/oci/oci_notification_topic_without_subscription/test/positive2.tf
@@ -2,7 +2,7 @@ provider "oci" {
   region = "us-ashburn-1"
 }
 
-# Caso: Tópico existe pero no tiene suscripción que lo apunte
+# Caso: Tópico Exists but no tiene suscripción que lo apunte
 resource "oci_ons_notification_topic" "orphan_topic" {
   compartment_id = "ocid1.compartment..."
   name           = "orphan-topic"
