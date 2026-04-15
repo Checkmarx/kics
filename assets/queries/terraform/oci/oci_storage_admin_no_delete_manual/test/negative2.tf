78646b0f01d1c615b0f21c91cc63b9dc916fff61
Antero Silva <antero.silva@checkmarx.com>
2026-04-15 22:23:22 +0100

    chore: translate Spanish comments to English in negative2.tf

--- assets/queries/terraform/oci/oci_storage_admin_no_delete_manual/test/negative2.tf
+++ assets/queries/terraform/oci/oci_storage_admin_no_delete_manual/test/negative2.tf
@@ -6,7 +6,7 @@ resource "oci_identity_policy" "false_positive_check" {
   name           = "ManagerCheck"
   compartment_id = "ocid1.tenancy..."
 
-  # CORRECTO: Aunque el grupo se llama Managers, el verbo es read. El regex \bmanage\b evita el fallo.
+  # PASS: Aunque el grupo se llama Managers, el verbo es read. El regex \bmanage\b evita el fallo.
   statements = [
     "Allow group ObjectManagers to read object-family in tenancy"
   ]
