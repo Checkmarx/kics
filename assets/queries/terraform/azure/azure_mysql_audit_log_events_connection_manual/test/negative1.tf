b549e0e37fd113baa37aa2f9191fd3f5f74721e0
Antero Silva <antero.silva@checkmarx.com>
2026-04-15 23:21:31 +0100

    chore: remove azure_mysql_audit_log_events_connection_manual (always-fire manual check — cannot verify audit config in IaC)

--- assets/queries/terraform/azure/azure_mysql_audit_log_events_connection_manual/test/negative1.tf
+++
@@ -1,7 +0,0 @@
-resource "azurerm_storage_account" "no_mysql_here" {
-  name                     = "storageaccountneg1"
-  resource_group_name      = "rg-test"
-  location                 = "West Europe"
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-}
