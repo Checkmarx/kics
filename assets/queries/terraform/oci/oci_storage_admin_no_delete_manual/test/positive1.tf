291471d418e8f642acf62cd8872106e864200e66
Antero Silva <antero.silva@checkmarx.com>
2026-04-15 22:23:23 +0100

    chore: translate Spanish comments to English in positive1.tf

--- assets/queries/terraform/oci/oci_storage_admin_no_delete_manual/test/positive1.tf
+++ assets/queries/terraform/oci/oci_storage_admin_no_delete_manual/test/positive1.tf
@@ -6,7 +6,7 @@ resource "oci_identity_policy" "unsafe_object_admin" {
   name           = "UnsafeObjectAdmin"
   compartment_id = "ocid1.tenancy..."
   
-  # FALLO: Otorga manage (incluye delete) en object-family
+  # FAIL: Otorga manage (incluye delete) en object-family
   statements = [
     "Allow group StorageAdmins to manage object-family in tenancy"
   ]
