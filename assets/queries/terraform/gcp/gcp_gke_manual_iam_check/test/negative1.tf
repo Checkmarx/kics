b3c6c2b15acb159c890a46129bd8c06d8ed7cfa6
Antero Silva <antero.silva@checkmarx.com>
2026-04-15 21:18:14 +0100

    fix(queries): remove gcp_gke_manual_iam_check from PR

--- assets/queries/terraform/gcp/gcp_gke_manual_iam_check/test/negative1.tf
+++
@@ -1,5 +0,0 @@
-# En este caso negativo, no hay SA personalizada (se encargaría la regla de SA por defecto)
-resource "google_container_cluster" "no_custom_sa" {
-  name = "default-sa-cluster"
-  # No define node_config.service_account
-}
