553333e2d3b2b7ea0cae07d8df96cfb6322acfc8
Antero Silva <antero.silva@checkmarx.com>
2026-04-15 23:12:22 +0100

    fix: replace azurerm_mssql_server with azurerm_mysql_flexible_server in positive test

--- assets/queries/terraform/azure/azure_mysql_audit_log_events_connection_manual/test/positive1.tf
+++ assets/queries/terraform/azure/azure_mysql_audit_log_events_connection_manual/test/positive1.tf
@@ -1,8 +1,10 @@
-resource "azurerm_mssql_server" "fail_single" {
-  name                = "mysql-server-connection-fail"
-  resource_group_name = "rg-test"
-  location            = "West Europe"
+resource "azurerm_mysql_flexible_server" "fail" {
+  name                   = "mysql-server-connection-fail"
+  resource_group_name    = "rg-test"
+  location               = "West Europe"
   administrator_login    = "mysqladmin"
   administrator_login_password = "Password1234!"
-  version             = "5.7"
+  sku_name               = "GP_Standard_D2ds_v4"
+  version                = "8.0.21"
+  # FAIL: No paired azurerm_mysql_flexible_server_configuration for audit_log_events
 }
