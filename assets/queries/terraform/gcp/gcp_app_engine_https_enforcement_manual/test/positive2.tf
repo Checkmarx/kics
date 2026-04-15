ab0df21b25a04b9d5b362f8b1011704f8a745fde
Antero Silva <antero.silva@checkmarx.com>
2026-04-15 22:14:55 +0100

    chore: translate Spanish comments to English in positive2.tf

--- assets/queries/terraform/gcp/gcp_app_engine_https_enforcement_manual/test/positive2.tf
+++ assets/queries/terraform/gcp/gcp_app_engine_https_enforcement_manual/test/positive2.tf
@@ -2,5 +2,5 @@ resource "google_app_engine_standard_app_version" "app_no_handlers" {
   service    = "backend"
   version_id = "v1"
   runtime    = "go119"
-  # FALLO: Falta el bloque handlers, requiere revisión de app.yaml
+  # FAIL: Missing The block handlers, requiere revisión de app.yaml
 }
