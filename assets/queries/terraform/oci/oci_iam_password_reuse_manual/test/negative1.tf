378c51e8614fd0ea239ce69d033fbaf779a43b6d
Antero Silva <antero.silva@checkmarx.com>
2026-04-15 22:22:11 +0100

    chore: translate Spanish comments to English in negative1.tf

--- assets/queries/terraform/oci/oci_iam_password_reuse_manual/test/negative1.tf
+++ assets/queries/terraform/oci/oci_iam_password_reuse_manual/test/negative1.tf
@@ -2,7 +2,7 @@ resource "oci_identity_domains_password_policy" "secure_policy" {
   idcs_endpoint = "https://idcs-..."
   name          = "SecurePolicy"
   
-  # CORRECTO: >= 24
+  # PASS: >= 24
   num_passwords_in_history = 24
   
   schemas = ["urn:ietf:params:scim:schemas:oracle:idcs:extension:passwordState:User"]
