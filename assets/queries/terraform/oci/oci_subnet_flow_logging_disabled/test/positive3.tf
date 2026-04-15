ab942210f4f7345a338c51b970876404d83b6e08
Antero Silva <antero.silva@checkmarx.com>
2026-04-15 22:23:31 +0100

    chore: translate Spanish comments to English in positive3.tf

--- assets/queries/terraform/oci/oci_subnet_flow_logging_disabled/test/positive3.tf
+++ assets/queries/terraform/oci/oci_subnet_flow_logging_disabled/test/positive3.tf
@@ -2,7 +2,7 @@ resource "oci_logging_log" "wrong_type_log" {
   display_name = "wrong_type"
   log_group_id = "ocid1.loggroup.oc1..cccc"
   
-  # FALLO: Debería ser SERVICE
+  # FAIL: Debería ser SERVICE
   log_type     = "CUSTOM"
   is_enabled   = true
 
