5964b6012639f814d6bac75304cc229c0fdf48ef
Antero Silva <antero.silva@checkmarx.com>
2026-04-15 21:19:08 +0100

    fix(queries): remove azure_recovery_services_vault_cmk_encryption_disabled from PR

--- assets/queries/terraform/azure/azure_recovery_services_vault_cmk_encryption_disabled/test/positive1.tf
+++
@@ -1,7 +0,0 @@
-resource "azurerm_recovery_services_vault" "fail" {
-  name                = "vault-fail"
-  location            = "West Europe"
-  resource_group_name = "rg-test"
-  sku                 = "Standard"
-  # Falla por ausencia de bloque encryption
-}
