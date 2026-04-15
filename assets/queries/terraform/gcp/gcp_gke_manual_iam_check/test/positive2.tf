de2d4291ac20f35770d5a8569056011c1246c97b
Antero Silva <antero.silva@checkmarx.com>
2026-04-15 21:18:16 +0100

    fix(queries): remove gcp_gke_manual_iam_check from PR

--- assets/queries/terraform/gcp/gcp_gke_manual_iam_check/test/positive2.tf
+++
@@ -1,9 +0,0 @@
-resource "google_container_node_pool" "audit_pool" {
-  name    = "custom-sa-pool"
-  cluster = "my-cluster"
-
-  node_config {
-    # INFO: Esta cuenta requiere revisión manual de IAM
-    service_account = "dedicated-pool-sa@project.iam.gserviceaccount.com"
-  }
-}
