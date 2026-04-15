5661a312c3955e54c05fc5d917c34103cdfefe08
Antero Silva <antero.silva@checkmarx.com>
2026-04-15 21:17:59 +0100

    fix(tests): update test/positive2.tf to use platform_config for oci_compute_secure_boot_disabled

--- assets/queries/terraform/oci/oci_compute_secure_boot_disabled/test/positive2.tf
+++ assets/queries/terraform/oci/oci_compute_secure_boot_disabled/test/positive2.tf
@@ -3,8 +3,8 @@ resource "oci_core_instance" "positive2" {
   compartment_id      = "ocid1.compartment..."
   shape               = "VM.Standard2.1"
 
-  shape_config {
-    # FALLO: Falta is_secure_boot_enabled (Caso 2)
-    ocpus = 1
+  platform_config {
+    # FAIL: Missing is_secure_boot_enabled (Case 2)
+    type = "AMD_VM"
   }
 }
