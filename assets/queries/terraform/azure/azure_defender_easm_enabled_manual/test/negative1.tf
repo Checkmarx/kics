44bc51831c4b4d54a64cadc78a43678d3136d1d2
Antero Silva <antero.silva@checkmarx.com>
2026-04-15 21:17:42 +0100

    fix(queries): remove azure_defender_easm_enabled_manual from PR

--- assets/queries/terraform/azure/azure_defender_easm_enabled_manual/test/negative1.tf
+++
@@ -1,7 +0,0 @@
-resource "azurerm_storage_account" "no_rg_here" {
-  name                     = "storageaccountneg1"
-  resource_group_name      = "rg-existing"
-  location                 = "East US"
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-}
