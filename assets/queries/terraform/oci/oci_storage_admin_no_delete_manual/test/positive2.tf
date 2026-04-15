4276ca0960a4765bcc91d75792159fcd52d38429
Antero Silva <antero.silva@checkmarx.com>
2026-04-15 22:23:25 +0100

    chore: translate Spanish comments to English in positive2.tf

--- assets/queries/terraform/oci/oci_storage_admin_no_delete_manual/test/positive2.tf
+++ assets/queries/terraform/oci/oci_storage_admin_no_delete_manual/test/positive2.tf
@@ -6,7 +6,7 @@ resource "oci_identity_policy" "unsafe_volume_admin" {
   name           = "UnsafeVolumeAdmin"
   compartment_id = "ocid1.tenancy..."
 
-  # FALLO: Otorga manage (incluye delete) en volume-family
+  # FAIL: Otorga manage (incluye delete) en volume-family
   statements = [
     "Allow group BackupAdmins to MANAGE volume-family in tenancy"
   ]
