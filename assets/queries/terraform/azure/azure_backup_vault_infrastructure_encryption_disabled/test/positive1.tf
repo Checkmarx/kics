aebeaf29fd5b2629a49ed2a286877ccc623b65ff
Antero Silva <antero.silva@checkmarx.com>
2026-04-15 22:08:55 +0100

    chore: translate Spanish comments to English in positive1.tf

--- assets/queries/terraform/azure/azure_backup_vault_infrastructure_encryption_disabled/test/positive1.tf
+++ assets/queries/terraform/azure/azure_backup_vault_infrastructure_encryption_disabled/test/positive1.tf
@@ -4,5 +4,5 @@ resource "azurerm_data_protection_backup_vault" "fail" {
   location            = "West Europe"
   datastore_type      = "VaultStore"
   redundancy          = "LocallyRedundant"
-  # FALLO: No tiene bloque identity
+  # FAIL: No tiene bloque identity
 }
