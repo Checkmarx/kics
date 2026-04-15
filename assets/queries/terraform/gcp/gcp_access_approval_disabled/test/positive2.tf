109f23cafe80c319dbf1ab448b27f7cb7bc6d826
Antero Silva <antero.silva@checkmarx.com>
2026-04-15 22:14:41 +0100

    chore: translate Spanish comments to English in positive2.tf

--- assets/queries/terraform/gcp/gcp_access_approval_disabled/test/positive2.tf
+++ assets/queries/terraform/gcp/gcp_access_approval_disabled/test/positive2.tf
@@ -1,4 +1,4 @@
 resource "google_access_approval_project_settings" "empty_settings" {
   project_id = "some-project-id"
-  # FALLO: No tiene enrolled_services
+  # FAIL: No tiene enrolled_services
 }
