3ec88ceaead6ab0282e72468f459ef03ed673b67
Antero Silva <antero.silva@checkmarx.com>
2026-04-15 21:19:38 +0100

    fix(tests): update test/positive3.tf to use platform_config for oci_compute_secure_boot_disabled

--- assets/queries/terraform/oci/oci_compute_secure_boot_disabled/test/positive3.tf
+++ assets/queries/terraform/oci/oci_compute_secure_boot_disabled/test/positive3.tf
@@ -3,8 +3,9 @@ resource "oci_core_instance" "positive3" {
   compartment_id      = "ocid1.compartment..."
   shape               = "VM.Standard2.1"
 
-  shape_config {
-    # FALLO: Configurado a false (Caso 3)
+  platform_config {
+    # FAIL: Set to false (Case 3)
+    type                   = "AMD_VM"
     is_secure_boot_enabled = false
   }
 }
