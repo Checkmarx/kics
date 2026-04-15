d06659e9afa53ade489d18de2474075048b594f9
Antero Silva <antero.silva@checkmarx.com>
2026-04-15 22:21:40 +0100

    chore: translate Spanish comments to English in positive1.tf

--- assets/queries/terraform/oci/oci_compute_legacy_metadata_enabled/test/positive1.tf
+++ assets/queries/terraform/oci/oci_compute_legacy_metadata_enabled/test/positive1.tf
@@ -2,5 +2,5 @@ resource "oci_core_instance" "positive1" {
   availability_domain = "AD-1"
   compartment_id      = "ocid1.compartment..."
   shape               = "VM.Standard2.1"
-  # FALLO: Falta agent_config (Caso 1)
+  # FAIL: Missing agent_config (Caso 1)
 }
